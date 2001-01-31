084b9095cd47686dda82636fc45d58aa97b2b61f
Peter Mount <peter@retep.org.uk>
2001-01-31 09:23:45 +0000

    Wed Jan 31 08:46:00 GMT 2001 peter@retep.org.uk         - Some minor additions to Statement to make our own extensions more           portable.         - Statement.close() will now call ResultSet.close() rather than just           dissasociating with it.

--- example/basic.java
+++ example/basic.java
@@ -86,7 +86,7 @@ public class basic
     // This shows how to get the oid of a just inserted row
     // updated for 7.1
     st.executeUpdate("insert into basic values (4,1)");
-    int insertedOID = ((org.postgresql.jdbc2.Statement)st).getInsertedOID();
+    int insertedOID = ((org.postgresql.Statement)st).getInsertedOID();
     System.out.println("Inserted row with oid "+insertedOID);
 
     // Now change the value of b from 1 to 8
