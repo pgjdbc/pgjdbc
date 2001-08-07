ae9dafcc8b83d63be777a3a08d23c47fcb9c5857
Bruce Momjian <bruce@momjian.us>
2001-08-07 17:45:29 +0000

    I think you replaced too many things with put(...
    Here is a context diff from latest cvs
    
    And I see why you couldn't apply the last diff, the setCatalog diff has
    been backed out, that was causing the compile problem in the first
    place.
    
    This following one needs to be applied to allow the current cvs to
    compile
    
    Dave Cramer

--- org/postgresql/Connection.java
+++ org/postgresql/Connection.java
@@ -120,8 +120,8 @@ public abstract class Connection
     this_driver = d;
     this_url = url;
     PG_DATABASE = database;
-    PG_PASSWORD = info.put("password");
-    PG_USER = info.put("user");
+    PG_PASSWORD = info.getProperty("password");
+    PG_USER = info.getProperty("user");
     PG_PORT = port;
     PG_HOST = host;
     PG_STATUS = CONNECTION_BAD;
