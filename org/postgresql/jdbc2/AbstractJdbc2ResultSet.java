394e554d2152c13ea8ec31e228b136716c6943d0
Dave Cramer <davec@fastcrypt.com>
2002-09-01 23:40:36 +0000

    patch to allow insertRow on an empty resultSet

--- org/postgresql/jdbc2/AbstractJdbc2ResultSet.java
+++ org/postgresql/jdbc2/AbstractJdbc2ResultSet.java
@@ -191,10 +191,12 @@ public abstract class AbstractJdbc2ResultSet extends org.postgresql.jdbc1.Abstra
   }
 
 
-  public boolean first() throws SQLException {
+  public boolean first() throws SQLException
+  {
     if (rows.size() <= 0)
       return false;
 
+    onInsertRow = false;
     current_row = 0;
     this_row = (byte[][]) rows.elementAt(current_row);
 
@@ -449,8 +451,10 @@ public abstract class AbstractJdbc2ResultSet extends org.postgresql.jdbc1.Abstra
   }
 
 
-  public synchronized void cancelRowUpdates() throws SQLException {
-    if (doingUpdates) {
+  public synchronized void cancelRowUpdates() throws SQLException
+  {
+    if (doingUpdates)
+    {
       doingUpdates = false;
 
       clearRowBuffer();
@@ -458,7 +462,8 @@ public abstract class AbstractJdbc2ResultSet extends org.postgresql.jdbc1.Abstra
   }
 
 
-  public synchronized void deleteRow() throws SQLException {
+  public synchronized void deleteRow() throws SQLException
+  {
     if ( !isUpdateable() ) {
       throw new PSQLException( "postgresql.updateable.notupdateable" );
     }
@@ -593,12 +598,15 @@ public abstract class AbstractJdbc2ResultSet extends org.postgresql.jdbc1.Abstra
   }
 
 
-  public synchronized void  moveToInsertRow() throws SQLException {
-    if (!updateable) {
+  public synchronized void  moveToInsertRow() throws SQLException
+  {
+    if ( !isUpdateable() )
+    {
       throw new PSQLException( "postgresql.updateable.notupdateable" );
     }
 
-    if (insertStatement != null) {
+    if (insertStatement != null)
+    {
       insertStatement = null;
     }
 
@@ -612,7 +620,8 @@ public abstract class AbstractJdbc2ResultSet extends org.postgresql.jdbc1.Abstra
   }
 
 
-  private synchronized void clearRowBuffer() throws SQLException {
+  private synchronized void clearRowBuffer() throws SQLException
+  {
     // rowBuffer is the temporary storage for the row
     rowBuffer = new byte[fields.length][];
 
@@ -1180,7 +1189,8 @@ public abstract class AbstractJdbc2ResultSet extends org.postgresql.jdbc1.Abstra
    * Is this ResultSet updateable?
    */
 
-  boolean isUpdateable() throws SQLException {
+  boolean isUpdateable() throws SQLException
+  {
 
     if (updateable) return true;
 
