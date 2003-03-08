470727fcd1a7847e9485fb06456f1cd3cef53baa
Barry Lind <barry@xythos.com>
2003-03-08 06:06:55 +0000

    Applied patch from Paul Sorenson to correctly handle schema names in updateable result sets. Applied patch from Rich Cullingford to fix a NPE in the absolute() method of result set. Applied patch from Tarjei Skorgenes to fix a NPE when logging is enabled.
     Modified Files:
     	jdbc/org/postgresql/core/BaseResultSet.java
     	jdbc/org/postgresql/jdbc1/AbstractJdbc1ResultSet.java
     	jdbc/org/postgresql/jdbc2/Array.java
     	jdbc/org/postgresql/util/PSQLException.java

--- org/postgresql/core/BaseResultSet.java
+++ org/postgresql/core/BaseResultSet.java
@@ -16,16 +16,19 @@ package org.postgresql.core;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Vector;
 
 public interface BaseResultSet
 {
+
     public BaseStatement getPGStatement();
 
 	public void append(BaseResultSet r);
 	public void close() throws SQLException;
 	public int getColumnCount();
 	public String getCursorName() throws SQLException;
+	public SimpleDateFormat getDateFormat();
 	public String getFixedString(int col) throws SQLException;
 	public long getLastOID();
 	public ResultSetMetaData getMetaData() throws SQLException;
@@ -35,6 +38,8 @@ public interface BaseResultSet
 	public String getStatusString();
 	public String getString(int columnIndex) throws SQLException;
 	public StringBuffer getStringBuffer();
+	public SimpleDateFormat getTimestampFormat();
+	public SimpleDateFormat getTimestampTZFormat();
 	public int getTupleCount();
 	public boolean next() throws SQLException;
 	public boolean reallyResultSet();
