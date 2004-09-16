9fa7b5901a2759dd45ddf536daf545b5556b498b
Kris Jurka <books@ejurka.com>
2004-09-16 22:32:15 +0000

    Add a new file to hold the build number to avoid filling the Driver.java.in commit log with just build number changes.
    Also add a utility program to print out the driver version and where
    it was loaded from.

---
+++ org/postgresql/util/PSQLDriverVersion.java
@@ -0,0 +1,24 @@
+package org.postgresql.util;
+
+import org.postgresql.Driver;
+
+/**
+ * This class holds the current build number and a utility program to print
+ * it and the file it came from.  The primary purpose of this is to keep
+ * from filling the cvs history of Driver.java.in with commits simply
+ * changing the build number.  The utility program can also be helpful for
+ * people to determine what version they really have and resolve problems
+ * with old and unknown versions located somewhere in the classpath.
+ */
+public class PSQLDriverVersion {
+
+	public static int buildNumber = 306;
+
+	public static void main(String args[]) {
+		Driver driver = new Driver();
+		java.net.URL url = driver.getClass().getResource("/org/postgresql/Driver.class");
+		System.out.println(driver.getVersion());
+		System.out.println("Found in: " + url);
+	}
+
+}
