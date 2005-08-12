b97bc3f469f384e2bfd427f584d21953f2286d6d
Kris Jurka <books@ejurka.com>
2005-08-12 18:09:02 +0000

    Make callable statements work again for JDK 1.5 builds.  Any code int the jdbc3/Jdbc3 classes also needs to get into the corresponding jdbc3g/Jdbc3g class.

--- org/postgresql/jdbc3g/Jdbc3gCallableStatement.java
+++ org/postgresql/jdbc3g/Jdbc3gCallableStatement.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/jdbc3g/Jdbc3gCallableStatement.java,v 1.3 2004/11/09 08:51:22 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc3g/Jdbc3gCallableStatement.java,v 1.4 2005/01/11 08:25:47 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -17,6 +17,10 @@ class Jdbc3gCallableStatement extends Jdbc3gPreparedStatement implements Callabl
     Jdbc3gCallableStatement(Jdbc3gConnection connection, String sql, int rsType, int rsConcurrency, int rsHoldability) throws SQLException
     {
         super(connection, sql, true, rsType, rsConcurrency, rsHoldability);
+        if ( !connection.haveMinimumServerVersion("8.1") || connection.getProtocolVersion() == 2)
+        {
+            adjustIndex = true;
+        }
     }
 
     public Object getObject(int i, Map < String, Class < ? >> map) throws SQLException
