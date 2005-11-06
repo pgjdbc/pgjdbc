5f21e4fa065eeb8719ff8f0ce593c9337f9d0335
Kris Jurka <books@ejurka.com>
2005-11-06 06:41:45 +0000

    Release build 314.

--- org/postgresql/util/PSQLDriverVersion.java
+++ org/postgresql/util/PSQLDriverVersion.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.8.2.3 2005/07/24 11:05:11 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.8.2.4 2005/10/07 06:08:40 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -21,7 +21,7 @@ import org.postgresql.Driver;
  */
 public class PSQLDriverVersion {
 
-    public static int buildNumber = 313;
+    public static int buildNumber = 314;
 
     public static void main(String args[]) {
         java.net.URL url = Driver.class.getResource("/org/postgresql/Driver.class");
