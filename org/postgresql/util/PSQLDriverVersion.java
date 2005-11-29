2ce90ba7c44868efc5810157bf322dc2799e69d6
Kris Jurka <books@ejurka.com>
2005-11-29 08:07:36 +0000

    Actually the build number changes to the new version upon release and this was already changed when I branched off 8.1, so this is still build 500.  Sorry for the noise.

--- org/postgresql/util/PSQLDriverVersion.java
+++ org/postgresql/util/PSQLDriverVersion.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.14 2005/11/23 18:47:36 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.15 2005/11/29 08:01:22 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -21,7 +21,7 @@ import org.postgresql.Driver;
  */
 public class PSQLDriverVersion {
 
-    public static int buildNumber = 501;
+    public static int buildNumber = 500;
 
     public static void main(String args[]) {
         java.net.URL url = Driver.class.getResource("/org/postgresql/Driver.class");
