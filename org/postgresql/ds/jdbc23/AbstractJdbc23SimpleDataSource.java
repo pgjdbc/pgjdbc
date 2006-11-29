2e146b900f08b7f96316147e69ceb2e69efb79a2
Kris Jurka <books@ejurka.com>
2006-11-29 04:03:48 +0000

    Missed adding these files in previous commit to build datasource code under JDK1.6.

---
+++ org/postgresql/ds/jdbc23/AbstractJdbc23SimpleDataSource.java
@@ -0,0 +1,47 @@
+/*-------------------------------------------------------------------------
+*
+* Copyright (c) 2004-2005, PostgreSQL Global Development Group
+*
+* IDENTIFICATION
+*   $PostgreSQL$
+*
+*-------------------------------------------------------------------------
+*/
+package org.postgresql.ds.jdbc23;
+
+import javax.sql.DataSource;
+import java.io.Serializable;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+
+import org.postgresql.ds.common.*;
+
+/**
+ * Simple DataSource which does not perform connection pooling.  In order to use
+ * the DataSource, you must set the property databaseName. The settings for
+ * serverName, portNumber, user, and password are optional.  Note: these properties
+ * are declared in the superclass.
+ *
+ * @author Aaron Mulder (ammulder@chariotsolutions.com)
+ */
+public class AbstractJdbc23SimpleDataSource extends BaseDataSource implements Serializable
+{
+    /**
+     * Gets a description of this DataSource.
+     */
+    public String getDescription()
+    {
+        return "Non-Pooling DataSource from " + org.postgresql.Driver.getVersion();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException
+    {
+        writeBaseObject(out);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+    {
+        readBaseObject(in);
+    }
+}
