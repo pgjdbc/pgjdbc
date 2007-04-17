a071b25c4f0dc7cb2ce549c18ed475566395a9b1
Kris Jurka <books@ejurka.com>
2007-04-17 05:23:33 +0000

    Implement ResultSet.updateArray by simply mapping it to updateObject.

--- org/postgresql/jdbc3/AbstractJdbc3ResultSet.java
+++ org/postgresql/jdbc3/AbstractJdbc3ResultSet.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/jdbc3/AbstractJdbc3ResultSet.java,v 1.12 2005/02/15 08:56:26 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc3/AbstractJdbc3ResultSet.java,v 1.13 2005/06/08 01:44:02 oliver Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -192,7 +192,7 @@ public abstract class AbstractJdbc3ResultSet extends org.postgresql.jdbc2.Abstra
      */
     public void updateArray(int columnIndex, java.sql.Array x) throws SQLException
     {
-        throw org.postgresql.Driver.notImplemented(this.getClass(), "updateArray(int,Array)");
+        updateObject(columnIndex, x);
     }
 
     /**
@@ -209,7 +209,7 @@ public abstract class AbstractJdbc3ResultSet extends org.postgresql.jdbc2.Abstra
      */
     public void updateArray(String columnName, java.sql.Array x) throws SQLException
     {
-        throw org.postgresql.Driver.notImplemented(this.getClass(), "updateArray(String,Array)");
+        updateArray(findColumn(columnName), x);
     }
 
 }
