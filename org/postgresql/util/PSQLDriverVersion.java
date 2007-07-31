a72e305c0133dd8b962ab06cb991b44927317dd9
Kris Jurka <books@ejurka.com>
2007-07-31 06:07:02 +0000

    Prepare for release of 8.0-320.

--- org/postgresql/util/PSQLDriverVersion.java
+++ org/postgresql/util/PSQLDriverVersion.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.8.2.9 2006/12/01 11:54:54 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/util/PSQLDriverVersion.java,v 1.8.2.10 2007/04/18 08:13:26 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -21,7 +21,7 @@ import org.postgresql.Driver;
  */
 public class PSQLDriverVersion {
 
-    public static int buildNumber = 319;
+    public final static int buildNumber = 320;
 
     public static void main(String args[]) {
         java.net.URL url = Driver.class.getResource("/org/postgresql/Driver.class");
