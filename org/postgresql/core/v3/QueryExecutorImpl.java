41193c3f013196e9e9dc3dd5c7eaa82c7ee9f7e7
Kris Jurka <books@ejurka.com>
2007-09-24 12:34:10 +0000

    When doing batch execution we can have multiple Parse and DescribeStatement messages on the wire at the same time.  When we finally get around to collecting the DescribeStatement results we must check whether they still apply to the currently parsed query. Otherwise we'll overwrite our type information with stale data that will cause failures down the line.
    With debugging assistance from Eric Faulhaber.

--- org/postgresql/core/v3/QueryExecutorImpl.java
+++ org/postgresql/core/v3/QueryExecutorImpl.java
@@ -4,7 +4,7 @@
 * Copyright (c) 2004, Open Cloud Limited.
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/core/v3/QueryExecutorImpl.java,v 1.34 2007/02/28 06:11:00 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/core/v3/QueryExecutorImpl.java,v 1.35 2007/07/27 10:15:29 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -919,7 +919,7 @@ public class QueryExecutorImpl implements QueryExecutor {
             pgStream.Send(encodedStatementName);    // Statement name
         pgStream.SendChar(0);                       // end message
 
-        pendingDescribeStatementQueue.add(new Object[]{query, params, new Boolean(describeOnly)});
+        pendingDescribeStatementQueue.add(new Object[]{query, params, new Boolean(describeOnly), query.getStatementName()});
     }
 
     private void sendExecute(Query query, Portal portal, int limit) throws IOException {
@@ -1193,13 +1193,23 @@ public class QueryExecutorImpl implements QueryExecutor {
                     SimpleQuery query = (SimpleQuery)describeData[0];
                     SimpleParameterList params = (SimpleParameterList)describeData[1];
                     boolean describeOnly = ((Boolean)describeData[2]).booleanValue();
+                    String origStatementName = (String)describeData[3];
 
                     int numParams = pgStream.ReceiveInteger2();
+
                     for (int i=1; i<=numParams; i++) {
                         int typeOid = pgStream.ReceiveInteger4();
                         params.setResolvedType(i, typeOid);
                     }
-                    query.setStatementTypes((int[])params.getTypeOIDs().clone());
+
+                    // Since we can issue multiple Parse and DescribeStatement
+                    // messages in a single network trip, we need to make
+                    // sure the describe results we requested are still
+                    // applicable to the latest parsed query.
+                    //
+                    if ((origStatementName == null && query.getStatementName() == null) || (origStatementName != null && origStatementName.equals(query.getStatementName()))) {
+                        query.setStatementTypes((int[])params.getTypeOIDs().clone());
+                    }
 
                     if (describeOnly)
                         doneAfterRowDescNoData = true;
