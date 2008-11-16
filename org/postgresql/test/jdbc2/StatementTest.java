e280999e8391be2fea7aa106148ffd275ec49e73
Kris Jurka <books@ejurka.com>
2008-11-16 12:14:28 +0000

    Support a parameter (?) for the amount part of the timestampadd function.  Previously we only supported literals.
    Christophe Janton

--- org/postgresql/test/jdbc2/StatementTest.java
+++ org/postgresql/test/jdbc2/StatementTest.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/test/jdbc2/StatementTest.java,v 1.23 2006/11/02 15:31:14 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/test/jdbc2/StatementTest.java,v 1.24 2006/12/01 08:53:46 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -288,6 +288,22 @@ public class StatementTest extends TestCase
         assertEquals("bc",rs.getString(6));
         assertEquals("ABCD",rs.getString(7));
     }
+
+    public void testDateFuncWithParam() throws SQLException
+    {
+        // Prior to 8.0 there is not an interval + timestamp operator,
+        // so timestampadd does not work.
+        //
+        if (!TestUtil.haveMinimumServerVersion(con, "8.0"))
+            return;
+
+        PreparedStatement ps = con.prepareStatement("SELECT {fn timestampadd(SQL_TSI_QUARTER, ? ,{fn now()})}, {fn timestampadd(SQL_TSI_MONTH, ?, {fn now()})} ");
+        ps.setInt(1, 4);
+        ps.setInt(2, 12);
+        ResultSet rs = ps.executeQuery();
+        assertTrue(rs.next());
+        assertEquals(rs.getTimestamp(1), rs.getTimestamp(2));
+    }
     
     public void testDateFunctions() throws SQLException
     {
@@ -304,7 +320,7 @@ public class StatementTest extends TestCase
         // ensure sunday =>1 and monday =>2
         assertEquals(2,rs.getInt(5));
 
-	// Prior to 8.0 there is not an interval + timestamp operator,
+        // Prior to 8.0 there is not an interval + timestamp operator,
         // so timestampadd does not work.
         //
         if (!TestUtil.haveMinimumServerVersion(con, "8.0"))
