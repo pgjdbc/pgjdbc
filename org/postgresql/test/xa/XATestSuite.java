c5167530d6b815d9538254ef03dd87c4f8497f97
Kris Jurka <books@ejurka.com>
2009-06-20 14:47:56 +0000

    Disable the XA tests if the server doesn't allow any prepared transactions, which is the default starting with the 8.4 release.

--- org/postgresql/test/xa/XATestSuite.java
+++ org/postgresql/test/xa/XATestSuite.java
@@ -1,6 +1,8 @@
 package org.postgresql.test.xa;
 
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 import junit.framework.TestSuite;
 
@@ -16,7 +18,17 @@ public class XATestSuite extends TestSuite {
         {
             if (TestUtil.haveMinimumServerVersion(connection, "8.1"))
             {
-                suite.addTestSuite(XADataSourceTest.class);
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("SHOW max_prepared_transactions");
+                rs.next();
+                int mpt = rs.getInt(1);
+                if (mpt > 0) {
+                    suite.addTestSuite(XADataSourceTest.class);
+                } else {
+                    System.out.println("Skipping XA tests because max_prepared_transactions = 0.");
+                }
+                rs.close();
+                stmt.close();
             }
         }
         finally
