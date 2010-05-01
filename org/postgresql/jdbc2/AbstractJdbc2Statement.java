fbeba014d7eb7468b0e5cb3a2dc3ed97e02d5dba
Kris Jurka <books@ejurka.com>
2010-05-01 16:08:09 +0000

    When setNull is called with a TIME or TIMESTAMP type we cannot pass that type information on to the backend because we really don't know whether it is with or without a time zone.  For a NULL value it doesn't matter, but we can't establish a type because a later call with a non-null value using the same PreparedStatement can potentially end up using a specific type that is incorrect.
    Per example from Martti Jeenicke

--- org/postgresql/jdbc2/AbstractJdbc2Statement.java
+++ org/postgresql/jdbc2/AbstractJdbc2Statement.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2005, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2Statement.java,v 1.96.2.8 2009/05/27 23:55:35 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/jdbc2/AbstractJdbc2Statement.java,v 1.96.2.9 2009/09/26 15:21:40 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -1068,10 +1068,8 @@ public abstract class AbstractJdbc2Statement implements BaseStatement
             oid = Oid.DATE;
             break;
         case Types.TIME:
-            oid = Oid.TIME;
-            break;
         case Types.TIMESTAMP:
-            oid = Oid.TIMESTAMPTZ;
+            oid = Oid.UNSPECIFIED;
             break;
         case Types.BIT:
             oid = Oid.BOOL;
