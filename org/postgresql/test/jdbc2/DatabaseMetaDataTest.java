486bb84fad49f9fb3020e4003043c70e3ad34524
Kris Jurka <books@ejurka.com>
2010-12-22 16:53:46 +0000

    In DatabaseMetaData.getSchemas, return the user's own temp schemas, but no others.  Previously it wasn't returning the users own temp schema, but was showing all toast temp schemas.
    Per report from Thomas Kellerer.

--- org/postgresql/test/jdbc2/DatabaseMetaDataTest.java
+++ org/postgresql/test/jdbc2/DatabaseMetaDataTest.java
@@ -3,7 +3,7 @@
 * Copyright (c) 2004-2008, PostgreSQL Global Development Group
 *
 * IDENTIFICATION
-*   $PostgreSQL: pgjdbc/org/postgresql/test/jdbc2/DatabaseMetaDataTest.java,v 1.48 2010/10/16 00:38:31 jurka Exp $
+*   $PostgreSQL: pgjdbc/org/postgresql/test/jdbc2/DatabaseMetaDataTest.java,v 1.49 2010/10/23 07:30:47 jurka Exp $
 *
 *-------------------------------------------------------------------------
 */
@@ -714,9 +714,6 @@ public class DatabaseMetaDataTest extends TestCase
 
     public void testSchemas() throws Exception
     {
-        if (!TestUtil.haveMinimumServerVersion(con, "7.3"))
-            return ;
-
         DatabaseMetaData dbmd = con.getMetaData();
         assertNotNull(dbmd);
 
