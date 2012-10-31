1cd438695dc35cf4c1453e5d225f698c28d7389e
Dave Cramer <davecramer@gmail.com>
2012-10-31 14:40:22 -0400

    incremented build number to 1001

--- org/postgresql/util/PSQLDriverVersion.java
+++ org/postgresql/util/PSQLDriverVersion.java
@@ -1,6 +1,6 @@
 /*-------------------------------------------------------------------------
 *
-* Copyright (c) 2004-2011, PostgreSQL Global Development Group
+* Copyright (c) 2004-2012, PostgreSQL Global Development Group
 *
 *
 *-------------------------------------------------------------------------
@@ -19,7 +19,7 @@ import org.postgresql.Driver;
  */
 public class PSQLDriverVersion {
 
-    public final static int buildNumber = 1000;
+    public final static int buildNumber = 1001;
 
     public static void main(String args[]) {
         java.net.URL url = Driver.class.getResource("/org/postgresql/Driver.class");
