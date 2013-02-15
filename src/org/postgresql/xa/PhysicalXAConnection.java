b586fd30e7893f03bc6fda37e3e7061a6545ebdd
Adam J Gray <adam.j.gray@gmail.com>
2013-02-15 16:27:47 -0500

    Added some documentation

--- src/org/postgresql/xa/PhysicalXAConnection.java
+++ src/org/postgresql/xa/PhysicalXAConnection.java
@@ -24,6 +24,16 @@ class PhysicalXAConnection {
     private int backendPid;
     private boolean originalAutoCommit;
 
+    /**
+     * Construct a PhysicalXAConnection.  After construction, this connection will have no associated xid,
+     * will be set to not suspended, and will store the original auto commit value in order to restore it after a
+     * transaction is complete if one is started.  Original auto-commit will default to true if there is a problem
+     * retrieving its current value.
+     *
+     * @param physicalConn The BaseConnection to wrap
+     * @param user The username credential for the connection
+     * @param password The password for the connection
+     */
     PhysicalXAConnection(final BaseConnection physicalConn, final String user, final String password) {
         this.connection = physicalConn;
         this.user = user;
@@ -38,14 +48,34 @@ class PhysicalXAConnection {
         }
     }
 
+    /**
+     * @return The wrapped BaseConnection
+     */
     BaseConnection getConnection() {
         return connection;
     }
 
+    /**
+     * @return The transaction id associated to this connection
+     */
     Xid getAssociatedXid() {
         return associatedXid;
     }
 
+    /**
+     * Sets the transaction id associated to this connection.
+     *
+     * If the provided transaction id is null and there is no currently associated transaction id, restore the
+     * auto-commit state.
+     *
+     * If the provided transaction id is not null, and there is no currently associated transaction id, cache the
+     * auto-commit state.
+     *
+     * If all goes well, the transaction id will be associated to this connection and the connection will be set to
+     * not suspended.
+     *
+     * @param xid the transaction id to associate to this connection
+     */
     void setAssociatedXid(final Xid xid) {
         try {
             if (associatedXid != null && xid == null) { // restore the autocommit state.
@@ -82,6 +112,11 @@ class PhysicalXAConnection {
         return password;
     }
 
+    /**
+     * @param obj The object to compare
+     * @return false if the provided object is null, not the same class, or does not have the same backend PID
+     *  true otherwise.
+     */
     @Override
     public boolean equals(final Object obj) {
         if (obj == null || getClass() != obj.getClass() || this.backendPid != ((PhysicalXAConnection)obj).backendPid) {
