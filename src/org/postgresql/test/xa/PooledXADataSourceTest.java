10a13c337218fd90956c421517aabd902e78149e
Adam J Gray <adam.j.gray@gmail.com>
2013-02-28 16:30:11 -0500

    Added threaded pool test that exhausts the pool.

--- src/org/postgresql/test/xa/PooledXADataSourceTest.java
+++ src/org/postgresql/test/xa/PooledXADataSourceTest.java
@@ -13,6 +13,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author agray@polarislabs.com
@@ -90,5 +91,78 @@ public class PooledXADataSourceTest extends TestCase {
         xaRes.commit(xid, true);
 
         pool.add(xaconn);
+    }
+
+    /**
+     * Does a one-phase commit test on every connection in the pool plus one.
+     * Each connection, once used will be put at the back of the list.
+     * As such, the first connection will be reused once while the other two
+     * will be used only once each.
+     *
+     * @throws Exception
+     */
+    public void testPoolUsage() throws Exception {
+        for (int i = 0; i < poolsize + 1; i++) {
+            XAConnection xaconn = pool.get(0);
+            pool.remove(xaconn);
+            XAResource xaRes = xaconn.getXAResource();
+            Connection conn = xaconn.getConnection();
+
+            Xid xid = new XADataSourceTest.CustomXid(1);
+            xaRes.start(xid, XAResource.TMNOFLAGS);
+            conn.createStatement().executeQuery("SELECT * FROM testxa1");
+            xaRes.end(xid, XAResource.TMSUCCESS);
+            xaRes.commit(xid, true);
+
+            pool.add(pool.size(), xaconn);
+        }
+    }
+
+    public void testPoolUsageThreaded() throws Exception {
+
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch endLatch = new CountDownLatch(3);
+
+        new Thread(new OnePhaseThread(startLatch, endLatch)).start();
+        new Thread(new OnePhaseThread(startLatch, endLatch)).start();
+        new Thread(new OnePhaseThread(startLatch, endLatch)).start();
+
+        startLatch.countDown();
+        endLatch.await();
+    }
+
+    class OnePhaseThread implements Runnable {
+
+        private CountDownLatch startLatch;
+        private CountDownLatch endLatch;
+
+        OnePhaseThread(final CountDownLatch startLatch, final CountDownLatch endLatch) {
+            this.startLatch = startLatch;
+            this.endLatch = endLatch;
+        }
+
+        @Override
+        public void run() {
+            try {
+                startLatch.await();
+                XAConnection xaconn = pool.get(0);
+                pool.remove(xaconn);
+                XAResource xaRes = xaconn.getXAResource();
+                Connection conn = xaconn.getConnection();
+
+                Xid xid = new XADataSourceTest.CustomXid(1);
+                xaRes.start(xid, XAResource.TMNOFLAGS);
+                conn.createStatement().executeQuery("SELECT * FROM testxa1");
+                xaRes.end(xid, XAResource.TMSUCCESS);
+                xaRes.commit(xid, true);
+
+                pool.add(pool.size(), xaconn);
+
+                endLatch.countDown();
+            } catch (Throwable t) {
+                t.printStackTrace();
+                fail();
+            }
+        }
     }
 }
