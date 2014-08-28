0164171523d104a06a6ae89ccd4d11f417d28739
nicolas-f <github@nettrader.fr>
2014-08-28 16:28:49 +0200

    Handle null value

--- org/postgresql/util/PGobject.java
+++ org/postgresql/util/PGobject.java
@@ -8,6 +8,7 @@
 package org.postgresql.util;
 
 import java.io.Serializable;
+import java.lang.Object;
 import java.lang.Override;
 import java.sql.SQLException;
 
@@ -108,10 +109,10 @@ public class PGobject implements Serializable, Cloneable
 
     /**
      * Compute hash. As equals() use only value. Return the same hash for the same value.
-     * @return Value hashcode
+     * @return Value hashcode, 0 if value is null {@link java.util.Objects#hashCode(Object)}
      */
     @Override
     public int hashCode() {
-        return getValue().hashCode();
+        return getValue() != null ? getValue().hashCode() : 0;
     }
 }
