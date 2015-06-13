f470f055fe3dadc6bc94429969119de896269e94
Stephen Nelson <stephen@eccostudio.com>
2015-06-13 15:00:27 +0100

    chore: migrate the build to Maven
    Use Maven as the build tool to reduce complexity of the build process. Separate source and test into
    separate directories according to Maven conventions.
    
    closes #322

---
+++ src/main/java-templates/org/postgresql/ssl/MakeSSL.java
@@ -0,0 +1,18 @@
+/*-------------------------------------------------------------------------
+*
+* Copyright (c) 2004-2014, PostgreSQL Global Development Group
+*
+*
+*-------------------------------------------------------------------------
+*/
+package org.postgresql.ssl;
+
+
+
+public class MakeSSL extends ${template.make.ssl.class}
+{
+	
+}
+
+
+
