16c27b9e31546f4ebdc23deeb0178ffce9cc2537
Christian Ullrich <chris@chrullrich.net>
2016-05-10 02:50:05 +0200

    test: add basic tests for SSPI authentication
    closes #557

---
+++ pgjdbc/src/test/java/org/postgresql/test/sspi/SSPITest.java
@@ -0,0 +1,71 @@
+package org.postgresql.test.sspi;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeThat;
+
+import org.postgresql.test.TestUtil;
+import org.postgresql.util.PSQLException;
+import org.postgresql.util.PSQLState;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Properties;
+
+/*
+ * These tests require a working SSPI authentication setup
+ * in the database server that allows the executing user
+ * to authenticate as the "sspiusername" in the build
+ * configuration.
+ */
+public class SSPITest {
+
+  /*
+   * SSPI only exists on Windows.
+   */
+  @BeforeClass
+  public static void checkPlatform() {
+    assumeThat("SSPI not supported on this platform",
+               System.getProperty("os.name").toLowerCase(),
+               containsString("windows"));
+  }
+
+  /*
+   * Tests that SSPI login succeeds and a query can be run.
+   */
+  @Test
+  public void testAuthorized() throws Exception {
+    Properties props = new Properties();
+    props.setProperty("username", TestUtil.getSSPIUser());
+    Connection con = TestUtil.openDB(props);
+
+    Statement stmt = con.createStatement();
+    stmt.executeQuery("SELECT 1");
+
+    TestUtil.closeDB(con);
+  }
+
+  /*
+   * Tests that SSPI login fails with an unknown/unauthorized
+   * user name.
+   */
+  @Test
+  public void testUnauthorized() throws Exception {
+    Properties props = new Properties();
+    props.setProperty("username", "invalid" + TestUtil.getSSPIUser());
+
+    try {
+      Connection con = TestUtil.openDB(props);
+      TestUtil.closeDB(con);
+      fail("Expected a PSQLException");
+    } catch (PSQLException e) {
+      assertThat(e.getSQLState(), is(PSQLState.INVALID_AUTHORIZATION_SPECIFICATION.getState()));
+    }
+  }
+
+}
---
+++ pgjdbc/src/test/java/org/postgresql/test/sspi/SSPITestSuite.java
@@ -0,0 +1,13 @@
+package org.postgresql.test.sspi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/*
+ * Executes all known tests for SSPI.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ SSPITest.class })
+public class SSPITestSuite {
+  // Empty.
+}
