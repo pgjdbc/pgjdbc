7a9b20db12f7cd0dac72c5ea95aa0ccc95c58c2e
Minglei Tu <tmlneu@gmail.com>
2016-05-23 07:10:40 +0800

    add tests to main test suite

--- pgjdbc/src/test/java/org/postgresql/test/jdbc42/Jdbc42TestSuite.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc42/Jdbc42TestSuite.java
@@ -8,6 +8,7 @@
 
 package org.postgresql.test.jdbc42;
 
+import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -20,5 +21,17 @@ import org.junit.runners.Suite.SuiteClasses;
     GetObject310BinaryTest.class,
     SetObject310Test.class})
 public class Jdbc42TestSuite {
+
+  /*
+   * The main entry point for JUnit
+   */
+  public static TestSuite suite() throws Exception {
+    Class.forName("org.postgresql.Driver");
+    TestSuite suite = new TestSuite();
+
+    suite.addTestSuite(PreparedStatementTest.class);
+
+    return suite;
+  }
 
 }
