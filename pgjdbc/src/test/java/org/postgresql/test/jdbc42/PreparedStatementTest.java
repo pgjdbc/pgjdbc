bbff3be013a672c54d57711004bd6349e0244d6d
Minglei Tu <tmlneu@gmail.com>
2016-05-23 07:27:59 +0800

    follow `single assertion per test method ` rule

--- pgjdbc/src/test/java/org/postgresql/test/jdbc42/PreparedStatementTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc42/PreparedStatementTest.java
@@ -24,23 +24,39 @@ public class PreparedStatementTest extends BaseTest {
 
   protected void setUp() throws Exception {
     super.setUp();
-    TestUtil.createTable(con, "datetable", "tstz timestamptz, ttz timetz");
+    TestUtil.createTable(con, "timestamptztable", "tstz timestamptz");
+    TestUtil.createTable(con, "timetztable", "ttz timetz");
   }
 
   protected void tearDown() throws SQLException {
-    TestUtil.dropTable(con, "datetable");
+    TestUtil.dropTable(con, "timestamptztable");
+    TestUtil.dropTable(con, "timetztable");
     super.tearDown();
   }
 
-  public void testSetNull() throws SQLException {
-    PreparedStatement pstmt = con.prepareStatement("INSERT INTO datetable (tstz, ttz) VALUES (?, ?)");
+  public void testTimestampTzSetNull() throws SQLException {
+    PreparedStatement pstmt = con.prepareStatement("INSERT INTO timestamptztable (tstz) VALUES (?)");
 
+    // valid: fully qualified type to setNull()
     pstmt.setNull(1, Types.TIMESTAMP_WITH_TIMEZONE);
-    pstmt.setNull(2, Types.TIME_WITH_TIMEZONE);
     pstmt.executeUpdate();
 
+    // valid: fully qualified type to setObject()
     pstmt.setObject(1, null, Types.TIMESTAMP_WITH_TIMEZONE);
-    pstmt.setObject(2, null, Types.TIME_WITH_TIMEZONE);
+    pstmt.executeUpdate();
+
+    pstmt.close();
+  }
+
+  public void testTimeTzSetNull() throws SQLException {
+    PreparedStatement pstmt = con.prepareStatement("INSERT INTO timetztable (ttz) VALUES (?)");
+
+    // valid: fully qualified type to setNull()
+    pstmt.setNull(1, Types.TIME_WITH_TIMEZONE);
+    pstmt.executeUpdate();
+
+    // valid: fully qualified type to setObject()
+    pstmt.setObject(1, null, Types.TIME_WITH_TIMEZONE);
     pstmt.executeUpdate();
 
     pstmt.close();
