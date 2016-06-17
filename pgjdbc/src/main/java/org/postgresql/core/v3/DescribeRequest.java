5669e3469586b1df6b6c81a99f4f26bbac8f5906
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2016-06-17 12:19:43 +0300

    fix: incorrect binary data format in bind parameter X when using batch execution
    Some batch executions might result in "incorrect binary data format" if using non-exact data types (e.g. date) and bind different data types for the same bind position across executions.
    
    Reported here: https://www.postgresql.org/message-id/681cd4467be74455a6e599b3838164f5%40EX2013-DB01.adesso.local

--- pgjdbc/src/main/java/org/postgresql/core/v3/DescribeRequest.java
+++ pgjdbc/src/main/java/org/postgresql/core/v3/DescribeRequest.java
@@ -17,11 +17,13 @@ class DescribeRequest {
   public final SimpleQuery query;
   public final SimpleParameterList parameterList;
   public final boolean describeOnly;
+  public final String statementName;
 
   public DescribeRequest(SimpleQuery query, SimpleParameterList parameterList,
-      boolean describeOnly) {
+      boolean describeOnly, String statementName) {
     this.query = query;
     this.parameterList = parameterList;
     this.describeOnly = describeOnly;
+    this.statementName = statementName;
   }
 }
