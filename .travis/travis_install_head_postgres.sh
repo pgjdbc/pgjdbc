23abc6ce56b18956ada1031e595fc73ca7a41d56
Vladimir Gordiychuk <folyga@gmail.com>
2016-08-21 21:00:24 +0300

    bug: travis test not use HEAD postgresql
    Previous script have next problems:
    1) On travis exists another version postgresql that not shotdown.
    2) pg_ctl by default doesn't want when server start
    3) Not compile contrib that require for test for example hstor
    4) Tests use one of previous version postgresql
    
    #561
    #613

--- .travis/travis_install_head_postgres.sh
+++ .travis/travis_install_head_postgres.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 set -x -e
 
+PG_DATADIR="/etc/postgresql/${PG_VERSION}/main"
+
+sudo service postgresql stop
+sudo apt-get remove postgresql libpq-dev libpq5 postgresql-client-common postgresql-common -qq --purge
+
 git clone --depth=1 https://github.com/postgres/postgres.git
 cd postgres
 
-PREFIX=$HOME/pg_head
+# Build PostgreSQL from source
+sudo ./configure && sudo make && sudo make install
+sudo ln -s /usr/local/pgsql/bin/psql /usr/bin/psql
+# Build contrib from source
+cd contrib
+sudo make all && sudo make install
 
-./configure --prefix=$PREFIX
+#Post compile actions
+LD_LIBRARY_PATH=/usr/local/pgsql/lib
+export LD_LIBRARY_PATH
+sudo /sbin/ldconfig /usr/local/pgsql/lib
 
-# Build PostgreSQL from source and start the DB
-make && make install && $PREFIX/bin/pg_ctl -D $PREFIX/data initdb && $PREFIX/bin/pg_ctl -D $PREFIX/data -l postgres.log start
+sudo mkdir -p ${PG_DATADIR}
+sudo chmod 777 ${PG_DATADIR}
+sudo chown -R postgres:postgres ${PG_DATADIR}
+
+sudo su postgres -c "/usr/local/pgsql/bin/pg_ctl -D ${PG_DATADIR} -U postgres initdb"
+#Start head postgres
+sudo su postgres -c "/usr/local/pgsql/bin/pg_ctl -D ${PG_DATADIR} -w -t 300 -o '-p 5432' -l /tmp/postgres.log start"
+sudo tail /tmp/postgres.log
