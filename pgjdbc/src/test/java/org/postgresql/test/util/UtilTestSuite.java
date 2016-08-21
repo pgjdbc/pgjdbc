3001ec55cbc27dc08f3d1af3d8d31848df234d71
Vladimir Gordiychuk <folyga@gmail.com>
2016-08-21 22:22:27 +0300

    bug: fail during parsin 10devel version
    Number of HEAD postgresql is 10devel
    https://github.com/postgres/postgres/commit/ca9112a424ff68ec4f2ef67b47122f7d61412964
    but ServerVersion parse it as 0.
    
    This commit fix few tests that base on version for #630

---
+++ pgjdbc/src/test/java/org/postgresql/test/util/UtilTestSuite.java
@@ -0,0 +1,10 @@
+package org.postgresql.test.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ServerVersionTest.class})
+public class UtilTestSuite {
+}
