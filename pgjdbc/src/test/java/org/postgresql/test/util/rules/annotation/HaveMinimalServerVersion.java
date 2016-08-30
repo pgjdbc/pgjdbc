b8c30753334c352f5b52cd12546269c4b4f5aa0d
Vladimir Gordiychuk <folyga@gmail.com>
2016-08-31 00:11:39 +0300

    test: Add ability ignore test if sever version too old
    Add HaveMinimalServerVersion annotation with ServerVersionRule that allow
    via annotations ignore test if current version too old.
    
    Example
    @HaveMinimalServerVersion("8.4")
    public class CopyAPITest {
        @Rule
        private ServerVersionRule versionRule = new ServerVersionRule();
    
        @Test
        public void testCopyFromFile() throws Exception {
            // test copy api introduce in 8.4 version
        }
    }
    
    public class LogicalReplicationTest {
        @Rule
        private ServerVersionRule versionRule = new ServerVersionRule();
    
        @Test
        @HaveMinimalServerVersion("9.4")
        public void testStartLogicalReplication() throws Exception {
            // test logical replication introduced in 9.4
        }
    }

---
+++ pgjdbc/src/test/java/org/postgresql/test/util/rules/annotation/HaveMinimalServerVersion.java
@@ -0,0 +1,21 @@
+package org.postgresql.test.util.rules.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation use to ignore test if the current server version less than specified version.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {METHOD, TYPE})
+public @interface HaveMinimalServerVersion {
+  /**
+   * @return not null sever version in form x.y.z like 9.4, 9.5.3, etc.
+   * @see org.postgresql.core.ServerVersion
+   */
+  String value();
+}
