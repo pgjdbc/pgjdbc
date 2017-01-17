b55beaac84efc8912bbef6b0550b3b69a3822b8d
Jorge Solorzano <jorsol@gmail.com>
2017-01-17 13:30:21 -0600

    test: fix test replication on PG_HEAD

--- .travis/travis_configure_replication.sh
+++ .travis/travis_configure_replication.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -x -e
 
+set_conf_property() {
+    local key=${1}
+    local value=${2}
+
+    sudo sed -i -e "s/^#\?${key}.*/${key} = ${value}/g" ${PG_DATADIR}/postgresql.conf
+}
+
 if [ "${REPLICATION}" = "Y" ]
 then
     if [ "${PG_VERSION}" = "HEAD" ]
@@ -10,15 +17,15 @@ then
 
     if (( $(echo "${PG_VERSION} >= 9.1" | bc -l)))
     then
-        sudo sed -i -e 's/#max_wal_senders = 0/max_wal_senders = 4/g' ${PG_DATADIR}/postgresql.conf
-        sudo sed -i -e 's/#wal_keep_segments = 0/wal_keep_segments = 4/g' ${PG_DATADIR}/postgresql.conf
-        sudo sed -i -e 's/#wal_sender_timeout = .*/wal_sender_timeout = 2000/g' ${PG_DATADIR}/postgresql.conf
+        set_conf_property "max_wal_senders" "10"
+        set_conf_property "wal_keep_segments" "10"
+        set_conf_property "wal_sender_timeout" "5s"
         sudo sed -i -e 's/^#local\s\+replication\s\+postgres\s\+\(.*\)/local replication all \1/g' ${PG_DATADIR}/pg_hba.conf
         sudo sed -i -e 's/^#host\s\+replication\s\+postgres\s\+\(.*\)\s\+\(.*\)/host replication all \1 \2/g' ${PG_DATADIR}/pg_hba.conf
         if (( $(echo "${PG_VERSION} >= 9.4" | bc -l)))
         then
-            sudo sed -i -e 's/#wal_level = minimal/wal_level = logical/g' ${PG_DATADIR}/postgresql.conf
-            sudo sed -i -e 's/#max_replication_slots = 0/max_replication_slots = 4/g' ${PG_DATADIR}/postgresql.conf
+            set_conf_property "wal_level" "logical"
+            set_conf_property "max_replication_slots" "10"
         fi
     fi
 fi
