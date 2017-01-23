4aa53b7e43886d4036373cd36c11b5b82591e064
Alex <alexey.elin@list.ru>
2017-01-23 20:42:40 +0600

    refactor: migrate to Junit4
    fix compilation
    
    https://github.com/pgjdbc/pgjdbc/issues/205

--- pgjdbc/src/test/java/org/postgresql/test/jdbc42/GetObject310BinaryTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc42/GetObject310BinaryTest.java
@@ -9,10 +9,6 @@ import java.util.Properties;
 
 public class GetObject310BinaryTest extends GetObject310Test {
 
-  public GetObject310BinaryTest(String name) {
-    super(name);
-  }
-
   @Override
   protected void updateProperties(Properties props) {
     forceBinary(props);
