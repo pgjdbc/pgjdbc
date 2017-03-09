480b0cf1ffe266e41e259f1f7a016d3ea681cc3a
Joe Kutner <jpkutner@gmail.com>
2017-03-09 11:05:48 -0600

    Only resolve hostname if not using a SOCKS proxy (PR #774)

--- pgjdbc/src/main/java/org/postgresql/core/PGStream.java
+++ pgjdbc/src/main/java/org/postgresql/core/PGStream.java
@@ -59,7 +59,13 @@ public class PGStream {
 
     Socket socket = socketFactory.createSocket();
     if (!socket.isConnected()) {
-      socket.connect(new InetSocketAddress(hostSpec.getHost(), hostSpec.getPort()), timeout);
+      // When using a SOCKS proxy, the host might not be resolvable locally,
+      // thus we defer resolution until the traffic reaches the proxy. If there
+      // is no proxy, we must resolve the host to an IP to connect the socket.
+      InetSocketAddress address = System.getProperty("socksProxyHost") == null
+          ? new InetSocketAddress(hostSpec.getHost(), hostSpec.getPort())
+          : InetSocketAddress.createUnresolved(hostSpec.getHost(), hostSpec.getPort());
+      socket.connect(address, timeout);
     }
     changeSocket(socket);
     setEncoding(Encoding.getJVMEncoding("UTF-8"));
