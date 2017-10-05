d3a06d64d97710be22fac69590f9eedfaa94d104
Dave Cramer <davecramer@gmail.com>
2017-10-05 17:13:23 -0400

    added method in setObject to deal with ZonedDateTime added very preliminary test for same

--- pgjdbc/src/test/java/org/postgresql/test/jdbc42/SimpleJdbc42Test.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc42/SimpleJdbc42Test.java
@@ -11,6 +11,10 @@ import org.postgresql.test.jdbc2.BaseTest4;
 
 import org.junit.Test;
 
+import java.sql.PreparedStatement;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 /**
  * Most basic test to check that the right package is compiled
  */
@@ -22,5 +26,11 @@ public class SimpleJdbc42Test extends BaseTest4 {
   @Test
   public void testSupportsRefCursors() throws Exception {
     assertTrue(con.getMetaData().supportsRefCursors());
+  }
+
+  @Test
+  public void testZonedDateTime() throws Exception {
+    PreparedStatement preparedStatement = con.prepareStatement("insert into test values (?)");
+    preparedStatement.setObject(1, ZonedDateTime.now(ZoneOffset.UTC));
   }
 }
