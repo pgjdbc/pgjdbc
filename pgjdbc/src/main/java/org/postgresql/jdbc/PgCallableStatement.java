0c8bd4698a89611855896bf4ec0a8249d990688f
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2017-11-26 17:06:10 +0300

    fix: synchronize Statement#result field access to make #close() more thread-safe

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgCallableStatement.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgCallableStatement.java
@@ -87,7 +87,11 @@ class PgCallableStatement extends PgPreparedStatement implements CallableStateme
           PSQLState.NO_DATA);
     }
 
-    ResultSet rs = result.getResultSet();
+    ResultSet rs;
+    synchronized (this) {
+      checkClosed();
+      rs = result.getResultSet();
+    }
     if (!rs.next()) {
       throw new PSQLException(GT.tr("A CallableStatement was executed with nothing returned."),
           PSQLState.NO_DATA);
@@ -146,7 +150,9 @@ class PgCallableStatement extends PgPreparedStatement implements CallableStateme
 
     }
     rs.close();
-    result = null;
+    synchronized (this) {
+      result = null;
+    }
     return false;
   }
 
