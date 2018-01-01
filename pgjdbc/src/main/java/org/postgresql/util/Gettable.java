70d746660ac06f258b0c37ff8e797c42d25d6719
Marc Slemko <marc.slemko@socrata.com>
2017-12-31 16:20:29 -0800

    fix: allow disabling field metadata cache
    Clients accessing very dynamic schemas can have issues with the
    field metadata cache getting stale.  This change allows configuring the
    databaseMetadataCacheFields property to 0 to disable the cache and
    avoid these issues where necessary.  This behaviour was already
    documented, however did not actually work as the codepath assumed
    it could retrieve the fields from the cache.

---
+++ pgjdbc/src/main/java/org/postgresql/util/Gettable.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2018, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.util;
+
+public interface Gettable<K,V> {
+  V get(K key);
+}
