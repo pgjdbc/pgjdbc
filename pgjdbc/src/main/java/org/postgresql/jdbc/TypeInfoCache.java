c821f7d29bcb7d6c12583519f91f7c52052f757f
Dave Cramer <davecramer@gmail.com>
2018-01-10 12:00:24 -0500

    return timestamp with timezone in java 8

--- pgjdbc/src/main/java/org/postgresql/jdbc/TypeInfoCache.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/TypeInfoCache.java
@@ -85,7 +85,7 @@ public class TypeInfoCache implements TypeInfo {
       {"time", Oid.TIME, Types.TIME, "java.sql.Time", Oid.TIME_ARRAY},
       {"timetz", Oid.TIMETZ, Types.TIME, "java.sql.Time", Oid.TIMETZ_ARRAY},
       {"timestamp", Oid.TIMESTAMP, Types.TIMESTAMP, "java.sql.Timestamp", Oid.TIMESTAMP_ARRAY},
-      {"timestamptz", Oid.TIMESTAMPTZ, Types.TIMESTAMP, "java.sql.Timestamp",
+      {"timestamptz", Oid.TIMESTAMPTZ, Types.TIMESTAMP_WITH_TIMEZONE, "java.sql.Timestamp",
           Oid.TIMESTAMPTZ_ARRAY},
       //#if mvn.project.property.postgresql.jdbc.spec >= "JDBC4.2"
       {"refcursor", Oid.REF_CURSOR, Types.REF_CURSOR, "java.sql.ResultSet", Oid.REF_CURSOR_ARRAY},
