429753d86a814fdb07b0a444f5c9cd638155ef48
Dave Cramer <davecramer@gmail.com>
2018-01-10 12:20:09 -0500

    fix checkstyle, add conditional for java 8 in TypeCache

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
@@ -202,10 +202,10 @@ public class PgResultSet implements ResultSet, org.postgresql.PGRefCursorResultS
         return getTime(columnIndex);
       case Types.TIMESTAMP:
         return getTimestamp(columnIndex, null);
-//#if mvn.project.property.postgresql.jdbc.spec >= "JDBC4.2
+      //#if mvn.project.property.postgresql.jdbc.spec >= "JDBC4.2
       case Types.TIMESTAMP_WITH_TIMEZONE:
         return  getTimestamp(columnIndex, null);
-//#endif
+      //#endif
       case Types.BINARY:
       case Types.VARBINARY:
       case Types.LONGVARBINARY:
