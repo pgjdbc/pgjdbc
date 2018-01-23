57a903d82eac9ec19134f44dee0593aabf2cc75d
Alexander Kjäll <alexander.kjall@di.no>
2018-01-23 21:07:03 +0100

    wrote a negative test, that verifies that an exception is thrown if you try to use the wrong data type

--- pgjdbc/src/test/java/org/postgresql/test/jdbc42/UpdateObject310Test.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc42/UpdateObject310Test.java
@@ -6,8 +6,10 @@
 package org.postgresql.test.jdbc42;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.postgresql.test.TestUtil;
+import org.postgresql.util.PSQLException;
 
 import org.junit.After;
 import org.junit.Before;
@@ -36,9 +38,20 @@ public class UpdateObject310Test {
   @Parameterized.Parameters
   public static Collection<Object[]> data() {
     return Arrays.asList(new Object[][]{
-        {LocalDateTime.now().truncatedTo(ChronoUnit.MICROS), "timestamp_without_time_zone_column", "'2003-01-01'::TIMESTAMP"},
-        {LocalDate.now(), "date_column", "'2003-01-01'::DATE"},
-        {LocalTime.now().truncatedTo(ChronoUnit.MICROS), "time_without_time_zone_column", "'07:23'::TIME WITHOUT TIME ZONE"}
+        {LocalDateTime.now().truncatedTo(ChronoUnit.MICROS), "timestamp_without_time_zone_column",
+            "'2003-01-01'::TIMESTAMP",
+            "ERROR: column \"timestamp_without_time_zone_column\" is of type timestamp without time zone but expression is of type integer\n"
+                + "  Hint: You will need to rewrite or cast the expression.\n"
+                + "  Position: 59"},
+        {LocalDate.now(), "date_column", "'2003-01-01'::DATE",
+            "ERROR: column \"date_column\" is of type date but expression is of type integer\n"
+                + "  Hint: You will need to rewrite or cast the expression.\n"
+                + "  Position: 36"},
+        {LocalTime.now().truncatedTo(ChronoUnit.MICROS), "time_without_time_zone_column",
+            "'07:23'::TIME WITHOUT TIME ZONE",
+            "ERROR: column \"time_without_time_zone_column\" is of type time without time zone but expression is of type integer\n"
+                + "  Hint: You will need to rewrite or cast the expression.\n"
+                + "  Position: 54"}
     });
   }
 
@@ -61,15 +74,17 @@ public class UpdateObject310Test {
   private Object updateDate;
   private String columnName;
   private String originalValue;
+  private String errorMessage;
 
-  public UpdateObject310Test(Object updateDate, String columnName, String originalValue) {
+  public UpdateObject310Test(Object updateDate, String columnName, String originalValue, String errorMessage) {
     this.updateDate = updateDate;
     this.columnName = columnName;
     this.originalValue = originalValue;
+    this.errorMessage = errorMessage;
   }
 
   /**
-   * Test the behavior of setObject for timestamp columns.
+   * Test the behavior of updateObject for timestamp columns.
    */
   @Test
   public void test() throws SQLException {
@@ -87,6 +102,33 @@ public class UpdateObject310Test {
       rs = s.executeQuery(TestUtil.selectSQL("table1", columnName));
       rs.next();
       assertEquals(updateDate, rs.getObject(1, updateDate.getClass()));
+    } finally {
+      if (rs != null) {
+        rs.close();
+      }
+      s.close();
+    }
+  }
+
+  /**
+   * Test the behavior of updateObject for timestamp columns.
+   */
+  @Test
+  public void testWrongClass() throws SQLException {
+    Statement s = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+    ResultSet rs = null;
+
+    try {
+      s.executeUpdate("INSERT INTO table1(" + columnName + ") VALUES(" + originalValue + ")");
+      rs = s.executeQuery(TestUtil.selectSQL("table1", "table1_id, " + columnName));
+      rs.next();
+      rs.updateObject(2, 23);
+      rs.updateRow();
+      rs.close();
+
+      fail("should have thrown an Exception");
+    } catch (PSQLException e) {
+      assertEquals(errorMessage, e.getMessage());
     } finally {
       if (rs != null) {
         rs.close();
