5e9d678d4158c68e5f705a150a6580d1b62415dd
Dave Cramer <davecramer@gmail.com>
2018-01-23 08:46:14 -0500

    No need to check for HEAD in TEST_CLIENTS

--- .travis/travis_build.sh
+++ .travis/travis_build.sh
@@ -72,13 +72,10 @@ then
   sbt test
 
   cd ..
-  if [[ "${PG_VERSION}" == "HEAD" ]];
-  then
-    # test to make sure the jar is complete
-    git clone --depth=10 https://github.com/clojure/java.jdbc.git
-    cd java.jdbc
-    TEST_DBS=postgres TEST_POSTGRES_USER=test TEST_POSTGRES_DBNAME=test mvn test -Djava.jdbc.test.pgjdbc.version=$PROJECT_VERSION
-  fi
+  # test to make sure the jar is complete
+  git clone --depth=10 https://github.com/clojure/java.jdbc.git
+  cd java.jdbc
+  TEST_DBS=postgres TEST_POSTGRES_USER=test TEST_POSTGRES_DBNAME=test mvn test -Djava.jdbc.test.pgjdbc.version=$PROJECT_VERSION
 fi
 
 if [[ "${COVERAGE}" == "Y" ]];
