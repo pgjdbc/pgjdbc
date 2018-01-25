b4267daea2022102402afa3faf29900d50889195
Dave Cramer <davecramer@gmail.com>
2018-01-24 19:30:20 -0500

    allow postgres to connect, fix configure

--- .travis/travis_configure_ssl.sh
+++ .travis/travis_configure_ssl.sh
@@ -5,12 +5,12 @@ set_conf_property() {
     local key=${1}
     local value=${2}
 
-    sudo sed -i -e "s/^#\?${key}.*/${key} = ${value}/g" /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+    sudo sed -i -e "s/^#\?${key}.*/${key} = ${value}/" /etc/postgresql/${PG_VERSION}/main/postgresql.conf
 }
 
 enable_ssl_property() {
     local property=${1}
-    sed  -i -e "s/^#${property}\(.*\)/${property}\1 /" ssltest.properties
+    sed  -i -e "s/^#${property}\(.*\)/${property}\1/" ssltest.properties
 }
 
 if [ -z "$PG_VERSION" ]
@@ -18,10 +18,9 @@ then
     echo "env PG_VERSION is not defined";
 
 else
-set_conf_property "ssl" "on"
-set_conf_property "ssl_cert_file" "server.crt"
-set_conf_property "ssl_key_file" "server.key"
-set_conf_property "ssl_ca_file" "root.crt"
+set_conf_property "ssl_cert_file" "'server.crt'"
+set_conf_property "ssl_key_file" "'server.key'"
+set_conf_property "ssl_ca_file" "'root.crt'"
 
 enable_ssl_property "sslhostnossl9"
 enable_ssl_property "sslhostgh9"
@@ -33,12 +32,16 @@ enable_ssl_property "sslhostsslcertbh9"
 enable_ssl_property "sslcertgh9"
 enable_ssl_property "sslcertbh9"
 
+PG_DATA_DIR="/var/lib/postgresql/${PG_VERSION}/main"
 sudo cp certdir/server/pg_hba.conf "/etc/postgresql/${PG_VERSION}/main/pg_hba.conf"
-sudo cp certdir/server/root.crt "${PG_DATADIR}"
-sudo chmod 0600 "${PG_DATADIR}/root.crt"
-sudo cp certdir/server/server.crt "${PG_DATADIR}"
-sudo chmod 0600 "${PG_DATADIR}/server.crt"
-sudo cp certdir/server/server.key "${PG_DATADIR}"
-sudo chmod 0600 "${PG_DATADIR}/server.key"
+sudo cp certdir/server/root.crt "${PG_DATA_DIR}"
+sudo chmod 0600 "${PG_DATA_DIR}/root.crt"
+sudo chown postgres:postgres "${PG_DATA_DIR}/root.crt"
+sudo cp certdir/server/server.crt "${PG_DATA_DIR}"
+sudo chmod 0600 "${PG_DATA_DIR}/server.crt"
+sudo chown postgres:postgres "${PG_DATA_DIR}/server.crt"
+sudo cp certdir/server/server.key "${PG_DATA_DIR}"
+sudo chmod 0600 "${PG_DATA_DIR}/server.key"
+sudo chown postgres:postgres "${PG_DATA_DIR}/server.key"
 
 fi
