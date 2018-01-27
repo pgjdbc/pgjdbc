d9e7ea7cdf048db646f8bb7f592338a79413eb73
Dave Cramer <davecramer@gmail.com>
2018-01-27 13:49:44 -0500

    include the bash configuration files

---
+++ .travis/travis_ssl_users.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+create_databases() {
+    for db in hostdb hostssldb hostnossldb certdb hostsslcertdb; do
+        createdb -U postgres $db
+        psql -U postgres $db -c "create extension sslinfo"
+    done
+}
+
+create_databases
