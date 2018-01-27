e5c782db7b1e0893514173159e64be3ff327e933
Dave Cramer <davecramer@gmail.com>
2018-01-27 09:42:07 -0500

    need to quote entire string

--- .travis/travis_configure_ssl.sh
+++ .travis/travis_configure_ssl.sh
@@ -5,7 +5,7 @@ set_conf_property() {
     local key=${1}
     local value=${2}
 
-    sudo sed -i -e "s/^#\?${key}.*/${key} = \/etc\/postgresql\/${PG_VERSION}\/main\/${value}/" /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+    sudo sed -i -e "s/^#\?${key}.*/${key} = '\/etc\/postgresql\/${PG_VERSION}\/main\/${value}'/" /etc/postgresql/${PG_VERSION}/main/postgresql.conf
 }
 
 enable_ssl_property() {
@@ -18,9 +18,9 @@ then
     echo "env PG_VERSION is not defined";
 
 else
-set_conf_property "ssl_cert_file" "'server.crt'"
-set_conf_property "ssl_key_file" "'server.key'"
-set_conf_property "ssl_ca_file" "'root.crt'"
+set_conf_property "ssl_cert_file" "server.crt"
+set_conf_property "ssl_key_file" "server.key"
+set_conf_property "ssl_ca_file" "root.crt"
 
 enable_ssl_property "sslhostnossl9"
 enable_ssl_property "sslhostgh9"
