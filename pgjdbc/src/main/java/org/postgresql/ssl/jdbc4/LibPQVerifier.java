3548509b21c618ce60eebf3ceb1519cf0e2f4102
DRayX <7531689+DRayX@users.noreply.github.com>
2018-03-14 11:04:06 -0700

    Add copyright to fix style error

--- pgjdbc/src/main/java/org/postgresql/ssl/jdbc4/LibPQVerifier.java
+++ pgjdbc/src/main/java/org/postgresql/ssl/jdbc4/LibPQVerifier.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2004, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
 package org.postgresql.ssl.jdbc4;
 
 import java.security.cert.CertificateParsingException;
