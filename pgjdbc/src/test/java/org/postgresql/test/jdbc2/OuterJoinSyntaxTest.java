6a582fb88c70b9a1546bc2635cbb1beb970c6a2c
AlBundy33 <therealhawk@freenet.de>
2018-05-29 15:36:58 +0200

    fixed checkstyle errors

--- pgjdbc/src/test/java/org/postgresql/test/jdbc2/OuterJoinSyntaxTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc2/OuterJoinSyntaxTest.java
@@ -2,26 +2,27 @@
  * Copyright (c) 2004, PostgreSQL Global Development Group
  * See the LICENSE file in the project root for more information.
  */
+
 package org.postgresql.test.jdbc2;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import org.postgresql.test.TestUtil;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.postgresql.test.TestUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * Executes the query with the outer join syntax. The joined tables are encapsulated with parenthesis.
  * <b>Note:</b> This queries worked up to driver version 9.4.1211 (postgresql-9.4.1211.jre7.jar).
  * Encapsulation with parenthesis is used by third party like CrystalReports.
  */
-public class OuterJoinSyntaxTest
-{
-  
+public class OuterJoinSyntaxTest {
+
   /**
    * The connection to the test database.
    */
@@ -30,73 +31,59 @@ public class OuterJoinSyntaxTest
   /**
    * Prepares the test environment.
    *
-   * @throws Exception
+   * @throws Exception on error
    */
   @Before
-  public void setUp()
-      throws Exception
-  {
+  public void setUp() throws Exception {
     connection = TestUtil.openDB();
   }
 
   /**
    * Closes resources.
    *
-   * @throws Exception
+   * @throws Exception on error
    */
   @After
-  public void tearDown()
-      throws Exception
-  {
+  public void tearDown() throws Exception {
     TestUtil.closeDB(connection);
   }
-  
+
   @Test
-  public void testOuterJoinSyntaxWithSingleJoinAndWithoutOj()
-      throws Exception
-  {
+  public void testOuterJoinSyntaxWithSingleJoinAndWithoutOj() throws Exception {
     testOuterJoinSyntax(
         "select *"
         + " from (values (1, 'one'), (2, 'two')) as t1 (id, text)"
         + " left outer join (values (1, 'a'), (2, 'b')) as t2 (id, text) on (t1.id = t2.id)");
   }
-  
+
   @Test
-  public void testOuterJoinSyntaxWithMultipleJoinsAndWithoutOj()
-      throws Exception
-  {
+  public void testOuterJoinSyntaxWithMultipleJoinsAndWithoutOj() throws Exception {
     testOuterJoinSyntax(
         "select *"
         + " from (values (1, 'one'), (2, 'two')) as t1 (id, text)"
         + " left outer join (values (1, 'a'), (2, 'b')) as t2 (id, text) on (t1.id = t2.id)"
         + " left outer join (values (1, '1'), (2, '2')) as t3 (id, text) on (t2.id = t3.id)");
   }
-  
+
   @Test
-  public void testOuterJoinSyntaxWithSingleJoinAndWithOj()
-      throws Exception
-  {
+  public void testOuterJoinSyntaxWithSingleJoinAndWithOj() throws Exception {
     testOuterJoinSyntax(
         "select *"
         + " from {oj (values (1, 'one'), (2, 'two')) as t1 (id, text)"
         + " left outer join (values (1, 'a'), (2, 'b')) as t2 (id, text) on (t1.id = t2.id) }");
   }
-  
+
   @Test
-  public void testOuterJoinSyntaxWithMultipleJoinsAndWithOj()
-      throws Exception
-  {
+  public void testOuterJoinSyntaxWithMultipleJoinsAndWithOj() throws Exception {
     testOuterJoinSyntax(
         "select *"
         + " from {oj (values (1, 'one'), (2, 'two')) as t1 (id, text)"
         + " left outer join (values (1, 'a'), (2, 'b')) as t2 (id, text) on (t1.id = t2.id)"
         + " left outer join (values (1, '1'), (2, '2')) as t3 (id, text) on (t2.id = t3.id)}");
   }
-  
+
   @Test
-  public void testOuterJoinSyntaxWithMultipleJoinsAndWithOj2()
-      throws Exception
-  {
+  public void testOuterJoinSyntaxWithMultipleJoinsAndWithOj2() throws Exception {
     // multiple joins with oj and missing space character after oj
     testOuterJoinSyntax(
         "select *"
@@ -104,11 +91,9 @@ public class OuterJoinSyntaxTest
         + " left outer join (values (1, 'a'), (2, 'b')) as t2 (id, text) on (t1.id = t2.id)"
         + " left outer join (values (1, '1'), (2, '2')) as t3 (id, text) on (t2.id = t3.id)}");
   }
-  
+
   @Test
-  public void testOuterJoinSyntaxWithMultipleJoinsAndWithOj3()
-      throws Exception
-  {
+  public void testOuterJoinSyntaxWithMultipleJoinsAndWithOj3() throws Exception {
     // multiple joins with oj and missing space character after oj and some more parenthesis
     testOuterJoinSyntax(
         "select *"
@@ -121,29 +106,22 @@ public class OuterJoinSyntaxTest
    * Executes the statment.
    *
    * @param theQuery the query to execute
-   * @throws Exception
+   * @throws Exception on error
    */
-  private void testOuterJoinSyntax(final String theQuery)
-      throws Exception
-  {
+  private void testOuterJoinSyntax(final String theQuery) throws Exception {
     final Statement _st = connection.createStatement();
-    try
-    {
+    try {
       final ResultSet _rs = _st.executeQuery(theQuery);
-      try
-      {
+      try {
         int _count = 0;
-        while (_rs.next())
+        while (_rs.next()) {
           _count++;
+        }
         Assert.assertEquals(_count, 2);
-      }
-      finally
-      {
+      } finally {
         _rs.close();
       }
-    }
-    finally
-    {
+    } finally {
       _st.close();
     }
   }
