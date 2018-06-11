0d5c8162c9fd38ddebc380664b6bb33608c11d72
Anderson.Fachini <anderson.fachini@senior.com.br>
2018-06-11 12:15:09 -0300

    Adjusted styling

--- pgjdbc/src/main/java/org/postgresql/copy/CopyManager.java
+++ pgjdbc/src/main/java/org/postgresql/copy/CopyManager.java
@@ -128,7 +128,7 @@ public class CopyManager {
       while ((buf = cp.readFromCopy()) != null) {
         to.write(buf);
       }
-	  to.flush();
+      to.flush();
       return cp.getHandledRowCount();
     } catch (IOException ioEX) {
       // if not handled this way the close call will hang, at least in 8.2
