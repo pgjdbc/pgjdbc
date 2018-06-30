6b649cf0387b82670e84f789677809014f3072d4
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2018-06-30 16:13:31 +0300

    perf: reuse Matcher in interpretCommandStatus
    This slightly reduces memory allocation for DML statements
    
    see #1219

--- pgjdbc/src/main/java/org/postgresql/core/v3/QueryExecutorImpl.java
+++ pgjdbc/src/main/java/org/postgresql/core/v3/QueryExecutorImpl.java
@@ -73,7 +73,8 @@ import java.util.regex.Pattern;
 public class QueryExecutorImpl extends QueryExecutorBase {
 
   private static final Logger LOGGER = Logger.getLogger(QueryExecutorImpl.class.getName());
-  private static final Pattern COMMAND_COMPLETE_PATTERN = Pattern.compile("^([A-Za-z]++)(?: (\\d++))?+(?: (\\d++))?+$");
+  private static final Pattern COMMAND_COMPLETE_PATTERN = Pattern.compile("^([a-zA-Z][^ ]*+(?: [a-zA-Z][^ ]*+)*+)(?: (\\d++))?+(?: (\\d++))?+$");
+  private static final Matcher COMMAND_COMPLETE_MATCHER = COMMAND_COMPLETE_PATTERN.matcher("");
 
   /**
    * TimeZone of the current connection (TimeZone backend parameter)
@@ -2469,7 +2470,7 @@ public class QueryExecutorImpl extends QueryExecutorBase {
   private void interpretCommandStatus(String status, ResultHandler handler) {
     long oid = 0;
     long count = 0;
-    Matcher matcher = COMMAND_COMPLETE_PATTERN.matcher(status);
+    Matcher matcher = COMMAND_COMPLETE_MATCHER.reset(status);
     if (matcher.matches()) {
       // String command = matcher.group(1);
       String group2 = matcher.group(2);
