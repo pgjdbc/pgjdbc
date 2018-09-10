e1476864816f4072c0730a44ec79f3453eb94841
Sehrope Sarkuni <sehrope@jackdb.com>
2018-09-10 09:33:03 -0400

    refactor: Split out CRL test connection props and rename test method

--- pgjdbc/src/test/java/org/postgresql/test/ssl/CertificateRevocationListTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/ssl/CertificateRevocationListTest.java
@@ -5,45 +5,42 @@
 
 package org.postgresql.test.ssl;
 
+import java.io.File;
+import java.sql.Connection;
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.postgresql.PGProperty;
 import org.postgresql.jdbc.SslMode;
 import org.postgresql.test.TestUtil;
 import org.postgresql.util.PSQLException;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.io.File;
-import java.sql.Connection;
-
-import java.util.Properties;
-
 public class CertificateRevocationListTest {
-  @Test
-  public void testRevokedClientCert() throws Exception {
+  private static Properties getCrlProps(String crlFileName) {
     File certDirFile = TestUtil.getFile("certdir");
     String certdir = certDirFile.getAbsolutePath();
-
-    Connection con = null;
     Properties props = new Properties();
     props.put(TestUtil.SERVER_HOST_PORT_PROP, "localhost" + ":" + TestUtil.getPort());
     props.put(TestUtil.DATABASE_PROP, "hostssldb");
-
     PGProperty.SSL_MODE.set(props, SslMode.VERIFY_CA.value);
     PGProperty.SSL_CERT.set(props, certdir + "/" + "revoked.crt");
     PGProperty.SSL_KEY.set(props, certdir + "/" + "revoked.pk8");
     PGProperty.SSL_ROOT_CERT.set(props, certdir + "/" + "goodroot.crt");
-    PGProperty.SSL_CRL_FILE.set(props, certdir + "/" + "root.crl");
+    PGProperty.SSL_CRL_FILE.set(props, certdir + "/" + crlFileName);
+    return props;
+  }
 
-    try {
-      con = TestUtil.openDB(props);
+  @Test
+  public void revokedClientCert() throws Exception {
+    Properties props = getCrlProps("root.crl");
+    try (Connection con = TestUtil.openDB(props)) {
       Assert.fail("Should throw an exception");
     } catch (PSQLException ex) {
       String message = ex.getMessage();
-      if (message == null || !message.matches("^SSL certificate with serial number .* revoked.")) {
+      if (message == null || !message.matches("^SSL certificate with serial number .* revoked")) {
         Assert.fail("Expected SSL certificate revocation exception; actual message: " + message);
       }
     }
-    TestUtil.closeQuietly(con);
   }
 }
