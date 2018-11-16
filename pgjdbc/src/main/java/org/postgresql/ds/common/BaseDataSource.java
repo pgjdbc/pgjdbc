9f4bc60524465936c050cabec30214c51f50d1cc
Dave Cramer <davecramer@gmail.com>
2018-11-16 07:11:42 -0500

    make sure an invalid URL throws an exception in setUrl

--- pgjdbc/src/main/java/org/postgresql/ds/common/BaseDataSource.java
+++ pgjdbc/src/main/java/org/postgresql/ds/common/BaseDataSource.java
@@ -1109,6 +1109,7 @@ public abstract class BaseDataSource implements CommonDataSource, Referenceable 
 
     Properties p = org.postgresql.Driver.parseURL(url, null);
 
+    if (p == null ) throw new IllegalArgumentException("URL invalid " + url);
     for (PGProperty property : PGProperty.values()) {
       if (!this.properties.containsKey(property.getName())) {
         setProperty(property, property.get(p));
