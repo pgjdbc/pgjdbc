1bb927de2d7e6e294e630364e3ca52a8b14d139a
AO Industries, Inc. <support@aoindustries.com>
2018-12-25 12:24:12 -0600

    Noted this intentionally does not extend SQLData.

--- pgjdbc/src/test/java/org/postgresql/test/udt/Port.java
+++ pgjdbc/src/test/java/org/postgresql/test/udt/Port.java
@@ -5,9 +5,15 @@
 
 package org.postgresql.test.udt;
 
+import java.sql.SQLData;
+
 /**
  * A user-defined data type implemented as a domain over integer on the
  * server-side, demonstrating separation of interface from implementation.
+ * <p>
+ * This intentionally does not extend {@link SQLData}, where the implementing
+ * class {@link PortImpl} does.
+ * </p>
  */
 public interface Port extends Comparable<Port> {
 
