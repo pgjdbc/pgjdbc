d20212486b312dd1ce7d460bb4994e0185fe8086
AO Industries, Inc. <support@aoindustries.com>
2018-12-25 01:39:29 -0600

    Testing SQLData interface is only used for SQLType OTHER.

--- pgjdbc/src/test/java/org/postgresql/test/udt/CustomTypePriorityTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/udt/CustomTypePriorityTest.java
@@ -17,6 +17,7 @@ import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.Collections;
 import java.util.Map;
 
@@ -51,7 +52,7 @@ public class CustomTypePriorityTest {
   }
 
   @Test
-  public void testGetObjectPreparedStatement() throws Exception {
+  public void testSetObjectWithoutSqlType() throws Exception {
     PreparedStatement pstmt = con.prepareStatement("SELECT ? AS normal, ? AS sqldata");
     pstmt.setObject(1, new BigDecimal("1.00"));
     // If this uses SQLData, it will actually write CONSTANT_VALUE:
@@ -59,11 +60,55 @@ public class CustomTypePriorityTest {
     ResultSet result = pstmt.executeQuery();
     Assert.assertTrue(result.next());
 
+    Assert.assertEquals(new BigDecimal("1.00"), result.getBigDecimal("normal"));
+    Assert.assertEquals("SQLData should have been used here", BigDecimalSQLData.CONSTANT_VALUE, result.getBigDecimal(2));
+
+    Assert.assertEquals(new BigDecimal("1.00"), result.getObject(1));
+    Assert.assertEquals("SQLData should have been used here", BigDecimalSQLData.CONSTANT_VALUE, result.getObject("sqldata"));
+
+    Assert.assertEquals(BigDecimalSQLData.CONSTANT_VALUE, result.getObject(1, Collections.<String, Class<?>>singletonMap("numeric", BigDecimalSQLData.class)));
+    Assert.assertEquals(BigDecimalSQLData.CONSTANT_VALUE, result.getObject("sqldata", Collections.<String, Class<?>>singletonMap("numeric", BigDecimalSQLData.class)));
+
+    Assert.assertFalse(result.next());
+    pstmt.close();
+  }
+
+  @Test
+  public void testSetObjectWithSqlTypeNumeric() throws Exception {
+    PreparedStatement pstmt = con.prepareStatement("SELECT ? AS normal, ? AS sqldata");
+    pstmt.setObject(1, new BigDecimal("1.00"), Types.NUMERIC);
+    // If this uses SQLData, it will actually write CONSTANT_VALUE:
+    pstmt.setObject(2, new BigDecimalSQLData("2.00"), Types.NUMERIC);
+    ResultSet result = pstmt.executeQuery();
+    Assert.assertTrue(result.next());
+
     Assert.assertEquals(new BigDecimal("1.00"), result.getBigDecimal(1));
-    Assert.assertEquals(BigDecimalSQLData.CONSTANT_VALUE, result.getBigDecimal(2));
+    Assert.assertEquals("SQLData not have been used here", new BigDecimal("2.00"), result.getBigDecimal("sqldata"));
 
     Assert.assertEquals(new BigDecimal("1.00"), result.getObject("normal"));
-    Assert.assertEquals("SQLData should have taken priority here", BigDecimalSQLData.CONSTANT_VALUE, result.getObject("sqldata"));
+    Assert.assertEquals("SQLData not have been used here", new BigDecimal("2.00"), result.getObject(2));
+
+    Assert.assertEquals(BigDecimalSQLData.CONSTANT_VALUE, result.getObject(1, Collections.<String, Class<?>>singletonMap("numeric", BigDecimalSQLData.class)));
+    Assert.assertEquals(BigDecimalSQLData.CONSTANT_VALUE, result.getObject("sqldata", Collections.<String, Class<?>>singletonMap("numeric", BigDecimalSQLData.class)));
+
+    Assert.assertFalse(result.next());
+    pstmt.close();
+  }
+
+  @Test
+  public void testSetObjectWithSqlTypeOther() throws Exception {
+    PreparedStatement pstmt = con.prepareStatement("SELECT ? AS normal, ? AS sqldata");
+    pstmt.setObject(1, new BigDecimal("1.00"), Types.OTHER);
+    // If this uses SQLData, it will actually write CONSTANT_VALUE:
+    pstmt.setObject(2, new BigDecimalSQLData("2.00"), Types.OTHER);
+    ResultSet result = pstmt.executeQuery();
+    Assert.assertTrue(result.next());
+
+    Assert.assertEquals(new BigDecimal("1.00"), result.getBigDecimal(1));
+    Assert.assertEquals("SQLData should have been used here", BigDecimalSQLData.CONSTANT_VALUE, result.getBigDecimal(2));
+
+    Assert.assertEquals(new BigDecimal("1.00"), result.getObject("normal"));
+    Assert.assertEquals("SQLData should have been used here", BigDecimalSQLData.CONSTANT_VALUE, result.getObject("sqldata"));
 
     Assert.assertEquals(BigDecimalSQLData.CONSTANT_VALUE, result.getObject(1, Collections.<String, Class<?>>singletonMap("numeric", BigDecimalSQLData.class)));
     Assert.assertEquals(BigDecimalSQLData.CONSTANT_VALUE, result.getObject("sqldata", Collections.<String, Class<?>>singletonMap("numeric", BigDecimalSQLData.class)));
