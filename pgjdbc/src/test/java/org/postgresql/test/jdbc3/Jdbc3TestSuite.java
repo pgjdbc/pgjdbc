19aa0a335a00a0109c83b684d615c4630763c81c
Sehrope Sarkuni <sehrope@jackdb.com>
2019-04-26 11:38:55 -0400

    refactor: Sort Jdbc3TestSuite class list

--- pgjdbc/src/test/java/org/postgresql/test/jdbc3/Jdbc3TestSuite.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc3/Jdbc3TestSuite.java
@@ -13,18 +13,18 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    Jdbc3CallableStatementTest.class,
-    GeneratedKeysTest.class,
     CompositeQueryParseTest.class,
-    SqlCommandParseTest.class,
-    Jdbc3SavepointTest.class,
-    TypesTest.class,
-    ResultSetTest.class,
-    ParameterMetaDataTest.class,
-    Jdbc3BlobTest.class,
     DatabaseMetaDataTest.class,
+    GeneratedKeysTest.class,
+    Jdbc3BlobTest.class,
+    Jdbc3CallableStatementTest.class,
+    Jdbc3SavepointTest.class,
+    ParameterMetaDataTest.class,
+    ResultSetTest.class,
     SendRecvBufferSizeTest.class,
+    SqlCommandParseTest.class,
     StringTypeParameterTest.class,
+    TypesTest.class,
 })
 public class Jdbc3TestSuite {
 
