a185f0399561e5dce466795305958d0aabdbef29
Sehrope Sarkuni <sehrope@jackdb.com>
2019-04-26 15:30:44 -0400

    refactor: Sort Jdbc42TestSuite class list

--- pgjdbc/src/test/java/org/postgresql/test/jdbc42/Jdbc42TestSuite.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc42/Jdbc42TestSuite.java
@@ -11,13 +11,13 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    SimpleJdbc42Test.class,
     CustomizeDefaultFetchSizeTest.class,
-    GetObject310Test.class,
-    PreparedStatementTest.class,
-    Jdbc42CallableStatementTest.class,
     GetObject310InfinityTests.class,
+    GetObject310Test.class,
+    Jdbc42CallableStatementTest.class,
+    PreparedStatementTest.class,
     SetObject310Test.class,
+    SimpleJdbc42Test.class,
 })
 public class Jdbc42TestSuite {
 
