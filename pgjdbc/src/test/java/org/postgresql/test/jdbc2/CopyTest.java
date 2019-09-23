f0cb4de20edaed6d663f0125b6abe0915bf01d37
Igor Volkov <virtual-machinist@users.noreply.github.com>
2019-09-24 00:37:11 +0300

    Fix import order

--- pgjdbc/src/test/java/org/postgresql/test/jdbc2/CopyTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc2/CopyTest.java
@@ -6,9 +6,9 @@
 package org.postgresql.test.jdbc2;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import org.postgresql.PGConnection;
