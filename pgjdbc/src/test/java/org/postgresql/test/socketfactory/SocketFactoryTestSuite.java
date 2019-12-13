60393b1986f5e40abf88863d4760bfb93824b7b2
Dave Cramer <davecramer@gmail.com>
2019-12-13 09:29:44 -0500

    remove more currentTimeMillis in order to get tests to be more reliable

--- pgjdbc/src/test/java/org/postgresql/test/socketfactory/SocketFactoryTestSuite.java
+++ pgjdbc/src/test/java/org/postgresql/test/socketfactory/SocketFactoryTestSuite.java
@@ -44,7 +44,8 @@ public class SocketFactoryTestSuite {
   public void testDatabaseMetaData() throws Exception {
     assertNotNull("Custom socket factory not null", CustomSocketFactory.getInstance());
     assertEquals(STRING_ARGUMENT, CustomSocketFactory.getInstance().getArgument());
-    assertEquals(1, CustomSocketFactory.getInstance().getSocketCreated());
+    /* since we are enabling ssl a second connection is created */
+    assertEquals(2, CustomSocketFactory.getInstance().getSocketCreated());
   }
 
 }
