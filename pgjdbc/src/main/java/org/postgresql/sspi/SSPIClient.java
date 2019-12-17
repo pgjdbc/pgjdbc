3471b69aa5fde290256f6a145e0af6743c74201d
Dave Cramer <davecramer@gmail.com>
2019-12-17 07:02:56 -0500

    fix: issue 1482 where the port was being added to the GSSAPI service name

--- pgjdbc/src/main/java/org/postgresql/sspi/SSPIClient.java
+++ pgjdbc/src/main/java/org/postgresql/sspi/SSPIClient.java
@@ -103,8 +103,13 @@ public class SSPIClient implements ISSPIClient {
     final HostSpec hs = pgStream.getHostSpec();
 
     try {
+      /*
+      The GSSAPI implementation does not use the port in the service name.
+      Force the port number to 0
+      Fixes issue 1482
+      */
       return NTDSAPIWrapper.instance.DsMakeSpn(spnServiceClass, hs.getHost(), null,
-          (short) hs.getPort(), null);
+          (short) 0, null);
     } catch (LastErrorException ex) {
       throw new PSQLException("SSPI setup failed to determine SPN",
           PSQLState.CONNECTION_UNABLE_TO_CONNECT, ex);
