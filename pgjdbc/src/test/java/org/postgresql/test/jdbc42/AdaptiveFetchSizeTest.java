648ee666290a4ceb7b8386b6e20b02cb46900431
Adrian Kloskowski <adrian.kloskowski@heimdalldata.com>
2020-02-21 10:10:43 +0100

    feat: implementation of adaptive fetching
    Change in AdaptiveFetchSizeTest:
    - is using PreparedStatement during tests now;
    - added assume to check if preferQueryMode is not Simple;
    - new method to open connection and creating table to remove code repetitiveness.
    
    Commit made for Heimdall Data's request to implement adaptive fetching.

--- pgjdbc/src/test/java/org/postgresql/test/jdbc42/AdaptiveFetchSizeTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc42/AdaptiveFetchSizeTest.java
@@ -7,17 +7,20 @@ package org.postgresql.test.jdbc42;
 
 import static org.junit.Assert.assertEquals;
 
+import org.postgresql.PGConnection;
 import org.postgresql.PGProperty;
+import org.postgresql.jdbc.PreferQueryMode;
 import org.postgresql.test.TestUtil;
 
 import org.junit.After;
+import org.junit.Assume;
 import org.junit.Test;
 
 import java.lang.management.ManagementFactory;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 /**
@@ -26,7 +29,7 @@ import java.util.Properties;
 public class AdaptiveFetchSizeTest {
 
   private Connection connection;
-  private Statement statement;
+  private PreparedStatement statement;
   private ResultSet resultSet;
 
   private String table = "test_adaptive_fetch";
@@ -70,14 +73,13 @@ public class AdaptiveFetchSizeTest {
     int expectedSecondSize = 7;
     int expectedCounter = 50;
     int resultCounter = 0;
+
     Properties properties = new Properties();
     PGProperty.DEFAULT_ROW_FETCH_SIZE.set(properties, startFetchSize);
     PGProperty.MAX_RESULT_BUFFER.set(properties, "300");
     PGProperty.ADAPTIVE_FETCH.set(properties, true);
 
-    connection = TestUtil.openDB(properties);
-
-    TestUtil.createTable(connection, table, columns);
+    openConnectionAndCreateTable(properties);
 
     for (int i = 0; i != expectedCounter; i++) {
       if (i == 4) {
@@ -107,7 +109,6 @@ public class AdaptiveFetchSizeTest {
     assertEquals(expectedCounter, resultCounter);
   }
 
-
   /**
    * Second integration test. The main purpose of this set is to check if minimum size was used
    * during adaptive fetching. To a table are added 50 rows with sizes: 1x270B, 49x10B. First fetch
@@ -125,15 +126,14 @@ public class AdaptiveFetchSizeTest {
     int expectedSize = 10;
     int expectedCounter = 50;
     int resultCounter = 0;
+
     Properties properties = new Properties();
     PGProperty.DEFAULT_ROW_FETCH_SIZE.set(properties, startFetchSize);
     PGProperty.MAX_RESULT_BUFFER.set(properties, "300");
     PGProperty.ADAPTIVE_FETCH.set(properties, true);
     PGProperty.ADAPTIVE_FETCH_MINIMUM.set(properties, expectedSize);
 
-    connection = TestUtil.openDB(properties);
-
-    TestUtil.createTable(connection, table, columns);
+    openConnectionAndCreateTable(properties);
 
     for (int i = 0; i != expectedCounter; i++) {
       if (i == 0) {
@@ -176,15 +176,14 @@ public class AdaptiveFetchSizeTest {
     int expectedSize = 10;
     int expectedCounter = 50;
     int resultCounter = 0;
+
     Properties properties = new Properties();
     PGProperty.DEFAULT_ROW_FETCH_SIZE.set(properties, startFetchSize);
     PGProperty.MAX_RESULT_BUFFER.set(properties, "300");
     PGProperty.ADAPTIVE_FETCH.set(properties, true);
     PGProperty.ADAPTIVE_FETCH_MAXIMUM.set(properties, expectedSize);
 
-    connection = TestUtil.openDB(properties);
-
-    TestUtil.createTable(connection, table, columns);
+    openConnectionAndCreateTable(properties);
 
     for (int i = 0; i != expectedCounter; i++) {
       if (i < 4) {
@@ -225,17 +224,15 @@ public class AdaptiveFetchSizeTest {
     int startFetchSize = 4;
     int expectedCounter = 1000;
     int resultCounter = 0;
+    int expectedSize = (int) (
+        (long) (0.90 * ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax()) / 10);
+
     Properties properties = new Properties();
     PGProperty.DEFAULT_ROW_FETCH_SIZE.set(properties, startFetchSize);
     PGProperty.MAX_RESULT_BUFFER.set(properties, "90p");
     PGProperty.ADAPTIVE_FETCH.set(properties, true);
 
-    int expectedSize = (int) (
-        (long) (0.90 * ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax()) / 10);
-
-    connection = TestUtil.openDB(properties);
-
-    TestUtil.createTable(connection, table, columns);
+    openConnectionAndCreateTable(properties);
 
     for (int i = 0; i != expectedCounter; i++) {
       addStringWithSize(10);
@@ -263,12 +260,13 @@ public class AdaptiveFetchSizeTest {
   private void executeFetchingQuery() throws SQLException {
     connection.setAutoCommit(false);
 
-    statement = connection.createStatement();
-    resultSet = statement.executeQuery("SELECT * FROM " + table);
+    statement = connection.prepareStatement("SELECT * FROM " + table);
+    resultSet = statement.executeQuery();
   }
 
   /**
    * Method to insert string with given size to a table.
+   *
    * @param size desired size of a string to be inserted in the table
    */
   private void addStringWithSize(int size) throws SQLException {
@@ -280,6 +278,31 @@ public class AdaptiveFetchSizeTest {
     sb.append("'");
     String insert = TestUtil.insertSQL(table, "value", sb.toString());
     TestUtil.execute(insert, connection);
+  }
+
+  /**
+   * Method to open connection, check if fetch can be performed and create table.
+   *
+   * @param properties Properties to be used during opening connection.
+   */
+  private void openConnectionAndCreateTable(Properties properties) throws SQLException {
+    connection = TestUtil.openDB(properties);
+    //After opening connection we should check if will be possible to do a fetch
+    checkIfFetchTestCanBePerformed(connection);
+    TestUtil.createTable(connection, table, columns);
+  }
+
+  /**
+   * Method to check if a fetch can be performed - PreferQueryMode is not set to Simple.
+   *
+   * @param connection Connection to be checked.
+   */
+  private void checkIfFetchTestCanBePerformed(Connection connection) throws SQLException {
+    PGConnection pgConnection = connection.unwrap(PGConnection.class);
+    PreferQueryMode preferQueryMode =
+        pgConnection == null ? PreferQueryMode.EXTENDED : pgConnection.getPreferQueryMode();
+    Assume.assumeTrue("Fetching tests can't be performed in simple mode",
+        preferQueryMode != PreferQueryMode.SIMPLE);
   }
 
 }
