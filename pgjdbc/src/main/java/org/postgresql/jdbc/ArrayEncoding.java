af30c15b9bd18ade278d436fe9b916cff4a1d5a3
BO8979 <BO8979@W1971362.northamerica.cerner.net>
2020-02-25 08:21:34 -0600

    documentation

--- pgjdbc/src/main/java/org/postgresql/jdbc/ArrayEncoding.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/ArrayEncoding.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, PostgreSQL Global Development Group
+ * Copyright (c) 2020, PostgreSQL Global Development Group
  * See the LICENSE file in the project root for more information.
  */
 
@@ -42,6 +42,8 @@ import java.util.Map;
  * </ul>
  * </ul>
  * </p>
+ *
+ * @author Brett Okken
  */
 final class ArrayEncoding {
 
