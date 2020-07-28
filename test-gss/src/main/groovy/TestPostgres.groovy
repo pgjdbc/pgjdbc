5fead4cf018b150484f88b8a8ae38bccfa5a768a
Dave Cramer <davecramer@gmail.com>
2020-07-28 15:07:27 -0400

    remove assertNull

--- test-gss/src/main/groovy/TestPostgres.groovy
+++ test-gss/src/main/groovy/TestPostgres.groovy
@@ -70,7 +70,6 @@ class TestPostgres {
 
                 try {
                     connection = pgJDBC.tryConnect('test', 'auth-test-localhost.postgresql.example.com', postgres.getPort(), 'test1', 'secret1')
-                    Assert.assertNull('Unable to acquire connection', connection)
 
                     if (pgJDBC.select(connection, "SELECT gss_authenticated AND not encrypted from pg_stat_gssapi where pid = pg_backend_pid()")) {
                         System.err.println 'GSS authenticated and not encrypted Connection succeeded'
