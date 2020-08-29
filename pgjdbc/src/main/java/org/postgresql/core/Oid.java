37048679fb1b4342fa37334872b6b4049dad9e00
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-08-29 17:40:47 +0300

    fix: make enum, json, and jsonb array result classes to be the same as 42.2.14 and earlier
    enum[] => String[]
    jsonb[] => String[]
    json[] => PGobject[]
    
    fixes #1876

--- pgjdbc/src/main/java/org/postgresql/core/Oid.java
+++ pgjdbc/src/main/java/org/postgresql/core/Oid.java
@@ -72,6 +72,7 @@ public class Oid {
   public static final int POINT = 600;
   public static final int POINT_ARRAY = 1017;
   public static final int BOX = 603;
+  public static final int JSONB = 3802;
   public static final int JSONB_ARRAY = 3807;
   public static final int JSON = 114;
   public static final int JSON_ARRAY = 199;
