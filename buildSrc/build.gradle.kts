ff0774da883287e9f012dd09f510e062167143e3
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2020-10-17 02:30:37 +0300

    chore: make buildSrc.jar reproducible

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -33,6 +33,14 @@ tasks.withType<KotlinCompile> {
     }
 }
 
+tasks.withType<AbstractArchiveTask>().configureEach {
+    // Ensure builds are reproducible
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+    dirMode = "775".toInt(8)
+    fileMode = "664".toInt(8)
+}
+
 autostyle {
     kotlin {
         ktlint {
