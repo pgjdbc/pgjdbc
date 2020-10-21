3373a815f91f61fda3cdbb0ca8de780f05cbae07
BO8979 <BO8979@W1971362.northamerica.cerner.net>
2020-10-21 16:18:15 -0500

    feat: optimized binary numeric support
    checkstyle

--- pgjdbc/src/test/java/org/postgresql/util/BigDecimalByteConverterTest.java
+++ pgjdbc/src/test/java/org/postgresql/util/BigDecimalByteConverterTest.java
@@ -2,6 +2,7 @@
  * Copyright (c) 2020, PostgreSQL Global Development Group
  * See the LICENSE file in the project root for more information.
  */
+
 package org.postgresql.util;
 
 import static org.junit.Assert.assertEquals;
