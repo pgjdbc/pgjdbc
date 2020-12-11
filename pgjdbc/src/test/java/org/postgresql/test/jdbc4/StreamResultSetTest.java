5baa70ca7f19b3ec0142f162ef3aa31ef559db20
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2020-12-11 16:48:28 +0200

    Try to remake the test to be more stable

--- pgjdbc/src/test/java/org/postgresql/test/jdbc4/StreamResultSetTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc4/StreamResultSetTest.java
@@ -34,13 +34,13 @@ import java.util.Properties;
 public class StreamResultSetTest extends BaseTest4 {
   // backend seems to use 8kB buffer for results.
   // Use enough rows and and padding to force results into multiple packets
-  private static final int GENERATED_ROWS = 50;
-  private static final double SLEEP = 0.03;
-  private static final long MIN_EXPECTED_DURATION = (long) (GENERATED_ROWS * 0.9 * SLEEP * 1000);
+  private static final int GENERATED_ROWS = 5;
+  private static final double SLEEP = 1.0;
+  private static final long EXPECTED_DURATION = (long) (GENERATED_ROWS * SLEEP * 1000);
   private static final String SELECT_SLEEP_BETWEEN_ROWS;
 
   static {
-    char[] filler = new char[1024];
+    char[] filler = new char[8192];
     Arrays.fill(filler,'x');
     String fillerText = new String(filler);
     SELECT_SLEEP_BETWEEN_ROWS = "select *,pg_sleep(" + SLEEP + "),clock_timestamp(),'" + fillerText + "' from series";
@@ -54,8 +54,6 @@ public class StreamResultSetTest extends BaseTest4 {
 
     BaseConnection pg = con.unwrap(BaseConnection.class);
     assumeTrue(pg.streamResults());
-
-    statement = prepareTable(con);
   }
 
   private static PreparedStatement prepareTable(Connection con) throws SQLException {
@@ -69,49 +67,8 @@ public class StreamResultSetTest extends BaseTest4 {
 
   @Test
   public void testStreamedResults() throws Exception {
-    readResults();
-  }
+    statement = prepareTable(con);
 
-  @Test
-  public void statementReExecuteCleansUpResources() throws Exception {
-    ResultSet results = statement.executeQuery();
-
-    // read few first items
-    results.next();
-    results.next();
-
-    // re-execute the same statement -> should close result set
-    // query second time to make sure the connection state is still correct after partial streaming
-    readResults(true);
-
-    assertThat(results.isClosed(), is(true));
-  }
-
-  @Test
-  public void differentStatementExecuteCleansUpResources() throws Exception {
-    try (PreparedStatement statement2 = con.prepareStatement(SELECT_SLEEP_BETWEEN_ROWS)) {
-      ResultSet results = statement2.executeQuery();
-
-      // read few first items
-      results.next();
-      results.next();
-
-      // re-execute the different statement -> must not close the previous result set
-      // query second time to make sure the connection state is still correct after partial streaming
-      readResults(true);
-
-      assertThat(results.isClosed(), is(false));
-
-      results.next();
-      results.next();
-    }
-  }
-
-  private void readResults() throws SQLException {
-    readResults(false);
-  }
-
-  private void readResults(boolean waitForPrevious) throws SQLException {
     // the test query seems to work differently on older servers by not pacing the response rows
     // the streaming features works nicely though
     assumeTrue(haveMinimumServerVersion(con, v10));
@@ -120,11 +77,75 @@ public class StreamResultSetTest extends BaseTest4 {
     r.run(statement);
 
     // verify that the there was a delay between the client seeing the first and last row
-    assertThat(r.timeBetweenFirstAndLastRow, greaterThan(MIN_EXPECTED_DURATION / 2));
+    assertThat("timeBetweenFirstAndLastRow", r.timeBetweenFirstAndLastRow, greaterThan(EXPECTED_DURATION / 2));
     // verify that the first row still appeared in reasonable time
-    assertThat(r.timeToFirstRow, lessThan((waitForPrevious ? MIN_EXPECTED_DURATION : 0 ) + MIN_EXPECTED_DURATION / 2));
+    assertThat("timeToFirstRow", r.timeToFirstRow, lessThan(EXPECTED_DURATION / 2));
 
-    assertThat(r.maxDeltaBetweenServerAndLocal, lessThan(300L));
+    assertThat("maxDeltaBetweenServerAndLocal", r.maxDeltaBetweenServerAndLocal, lessThan((long) (SLEEP * 1000 * 2)));
+  }
+
+  @Test
+  public void streamingDisabledBuffersAllRows() throws Exception {
+    Properties props = new Properties();
+    updateProperties(props);
+    PGProperty.STREAM_RESULTS.set(props, false);
+    Connection noStreamConn = TestUtil.openDB(props);
+
+    ResultReader r = new ResultReader();
+    r.run(prepareTable(noStreamConn));
+
+    // verify that the rows could be processed without much delays (allow some random gc time)
+    assertThat("timeBetweenFirstAndLastRow", r.timeBetweenFirstAndLastRow, lessThan(300L));
+    // verify that the first row took at least the expected time
+    assertThat("timeToFirstRow", r.timeToFirstRow, greaterThan(EXPECTED_DURATION));
+
+    assertThat("maxDeltaBetweenServerAndLocal", r.maxDeltaBetweenServerAndLocal, greaterThan(EXPECTED_DURATION / 2));
+  }
+
+  @Test
+  public void statementReExecuteCleansUpResources() throws Exception {
+    statement = con.prepareStatement("select * from generate_series(1,10)");
+    ResultSet results = statement.executeQuery();
+
+    // read few first items to get stream initialized
+    results.next();
+    assertThat(results.getInt(1), is(1));
+    results.next();
+    assertThat(results.getInt(1), is(2));
+
+    ResultSet results2 = statement.executeQuery();
+    assertThat(results.isClosed(), is(true));
+
+    results2.next();
+    assertThat(results2.getInt(1), is(1));
+    results2.next();
+    assertThat(results2.getInt(1), is(2));
+  }
+
+  @Test
+  public void differentStatementExecuteCleansUpResources() throws Exception {
+    statement = con.prepareStatement("select 1,* from generate_series(1,10)");
+    try (PreparedStatement statement2 = con.prepareStatement("select 2,* from generate_series(1,10)")) {
+      ResultSet results = statement.executeQuery();
+
+      // read few first items to get stream initialized
+      results.next();
+      assertThat(results.getInt(1), is(1));
+      assertThat(results.getInt(2), is(1));
+      results.next();
+      assertThat(results.getInt(1), is(1));
+      assertThat(results.getInt(2), is(2));
+
+      ResultSet results2 = statement2.executeQuery();
+      assertThat(results.isClosed(), is(false));
+
+      results2.next();
+      assertThat(results2.getInt(1), is(2));
+      assertThat(results2.getInt(2), is(1));
+      results2.next();
+      assertThat(results2.getInt(1), is(2));
+      assertThat(results2.getInt(2), is(2));
+    }
   }
 
   static class ResultReader {
@@ -155,27 +176,9 @@ public class StreamResultSetTest extends BaseTest4 {
         timeBetweenFirstAndLastRow = currentTimeMillis() - firstRowTime;
         timeToFirstRow = firstRowTime - startTime;
       }
-      assertThat(count, is(GENERATED_ROWS));
+      assertThat("count", count, is(GENERATED_ROWS));
       // verify that our query actually caused a delay between rows on the server side
-      assertThat(serverLastRowTime - serverFirstRowTime, greaterThan(MIN_EXPECTED_DURATION));
+      assertThat("serverSideDelay", serverLastRowTime - serverFirstRowTime, greaterThan(EXPECTED_DURATION / 2));
     }
-  }
-
-  @Test
-  public void streamingDisabledBuffersAllRows() throws Exception {
-    Properties props = new Properties();
-    updateProperties(props);
-    PGProperty.STREAM_RESULTS.set(props, false);
-    Connection noStreamConn = TestUtil.openDB(props);
-
-    ResultReader r = new ResultReader();
-    r.run(prepareTable(noStreamConn));
-
-    // verify that the rows could be processed without much delays (allow some random gc time)
-    assertThat(r.timeBetweenFirstAndLastRow, lessThan(200L));
-    // verify that the first row took at least the expected time
-    assertThat(r.timeToFirstRow, greaterThan(MIN_EXPECTED_DURATION));
-
-    assertThat(r.maxDeltaBetweenServerAndLocal, greaterThan(MIN_EXPECTED_DURATION));
   }
 }
