c9de8bdcd3bb345797e8fc629993876006f44990
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-03-29 21:34:45 +1100

    Attempt to load from the TCCL first
    This allows user specified classes to be loaded from
    the current Thread context ClassLoader, rather than
    from the ClassLoader of the driver itself.
    
    Fixes #2112

---
+++ pgjdbc/src/test/java/org/postgresql/test/socketfactory/Holder.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.test.socketfactory;
+
+import javax.net.SocketFactory;
+
+public class Holder {
+
+  public static SocketFactory customSocketFactory;
+
+}
