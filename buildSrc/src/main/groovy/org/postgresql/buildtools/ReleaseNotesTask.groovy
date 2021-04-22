06b5c2969976d4831946efb49df3ed38bebb8a80
Dave Cramer <davecramer@gmail.com>
2021-04-22 11:21:49 -0400

    cleaned up spacing and many other things

--- buildSrc/src/main/groovy/org/postgresql/buildtools/ReleaseNotesTask.groovy
+++ buildSrc/src/main/groovy/org/postgresql/buildtools/ReleaseNotesTask.groovy
@@ -6,10 +6,8 @@
 package org.postgresql.buildtools
 
 import groovy.io.FileType
-import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
@@ -67,21 +65,22 @@ class ReleaseNotesTask extends DefaultTask {
 
 
             // create the new post
-            def releaseNote = """---
-    title:  PostgreSQL JDBC Driver ${currentVersion} Released
-    date:   ${new java.text.SimpleDateFormat('yyyy-MM-dd HH:mm:ss Z').format(today)}
-    categories:
+            def releaseNote =
+"""---
+title:  PostgreSQL JDBC Driver ${currentVersion} Released
+date:   ${new java.text.SimpleDateFormat('yyyy-MM-dd HH:mm:ss Z').format(today)}
+categories:
     - new_release
-    version: ${currentVersion}
-    ---
-    **Notable changes**
+version: ${currentVersion}
+---
+**Notable changes**
 
-    ${notableChanges(repositoryDirectory, previousVersion)}
-    <!--more-->
+${notableChanges(repositoryDirectory, previousVersion)}
+<!--more-->
 
-    **Commits by author**
+**Commits by author**
 
-    ${getContributors(repositoryDirectory, previousVersion)}
+${getContributors(repositoryDirectory, previousVersion)}
     """
             println releaseNote
             // write release notes
@@ -89,21 +88,23 @@ class ReleaseNotesTask extends DefaultTask {
             releaseFile << releaseNote
 
             // update the changelog
-            updateChangeLog(repositoryDirectory, currentVersion)
+            updateChangeLog(repositoryDirectory, currentVersion, previousVersion.substring(3))
 
-        return
-    } catch( Exception e ) {
-        print e
+            return
+        } catch( Exception e ) {
+            print e
+        }
     }
-}
 
-def updateChangeLog( dir, currentVersion ) {
-    File tempFile = File.createTempFile("CHANGELOG", "MD")
-    File changeLog= new File("${dir.getAbsolutePath()}/CHANGELOG.md")
+    def updateChangeLog( dir, currentVersion, previousVersion ) {
+        Date today = Calendar.getInstance().time
 
-    changeLog.eachLine {
-        if ( it.contains("[Unreleased]") ) {
-            tempFile << """## [Unreleased]
+        File tempFile = File.createTempFile("CHANGELOG", "MD")
+        File changeLog= new File("${dir.getAbsolutePath()}/CHANGELOG.md")
+
+        changeLog.eachLine {
+            if ( it.endsWith("[Unreleased]") ) {
+                tempFile << """## [Unreleased]
 ### Changed
 
 ### Added
@@ -111,44 +112,47 @@ def updateChangeLog( dir, currentVersion ) {
 ### Fixed
 
 """
-            tempFile << "[$currentVersion]\n"
-        } else {
-            tempFile << it + '\n'
-        }
-    }
-    tempFile.renameTo(changeLog.getAbsolutePath())
-}
-def checkCurrentVersionExists (dir, currentVersion ) {
-    FileInputStream fis = new FileInputStream("${dir.getAbsolutePath()}/CHANGELOG.md")
-    fis.eachLine {
-        if ( it.contains("[${currentVersion.substring(3)}]") ) {
-            return true
-        }
-    }
-    return false
-}
-
-def notableChanges( dir, previousVersion ){
-    def currentChanges =""
-    FileInputStream fis = new FileInputStream("${dir.getAbsolutePath()}/CHANGELOG.md")
-    int state = 0
-
-    fis.eachLine {
-
-        if (state == 0 && it.contains('[Unreleased]')) {
-            state = 1
-        }
-        else if (state == 1 ) {
-
-            if (!it.contains("[${previousVersion.substring(3)}]")) {
-                currentChanges += it +'\n'
+                tempFile << "[$currentVersion] (${new java.text.SimpleDateFormat('yyyy-MM-dd HH:mm:ss Z').format(today)})\n"
+            } else if ( it.contains("$previousVersion...HEAD") ) {
+                tempFile << "[$previousVersion]: https://github.com/pgjdbc/pgjdbc/compare/REL$previousVersion...REL$currentVersion\n"
+                tempFile << "[Unreleased]: https://github.com/pgjdbc/pgjdbc/compare/REL$currentVersion...HEAD\n"
             } else {
-                state = 2
+                tempFile << it + '\n'
             }
         }
+        tempFile.renameTo(changeLog.getAbsolutePath())
     }
-    return currentChanges
-}
+    def checkCurrentVersionExists (dir, currentVersion ) {
+        FileInputStream fis = new FileInputStream("${dir.getAbsolutePath()}/CHANGELOG.md")
+        fis.eachLine {
+            if ( it.contains("[${currentVersion.substring(3)}]") ) {
+                return true
+            }
+        }
+        return false
+    }
+
+    def notableChanges( dir, previousVersion ){
+        def currentChanges =""
+        FileInputStream fis = new FileInputStream("${dir.getAbsolutePath()}/CHANGELOG.md")
+        int state = 0
+
+        fis.eachLine {
+
+            if (state == 0 && it.contains('[Unreleased]')) {
+                state = 1
+            }
+            else if (state == 1 ) {
+
+                if (!it.contains("[${previousVersion.substring(3)}]")) {
+                    currentChanges += it +'\n'
+                } else {
+                    state = 2
+                }
+            }
+        }
+        return currentChanges
+    }
 
 /**
  * We are going to change the contributors map here.
@@ -156,67 +160,70 @@ def notableChanges( dir, previousVersion ){
  * @param sha
  * @return
  */
-def getContributorForSha(Map contributors, sha){
+    def getContributorForSha(Map contributors, sha){
 
-    def url = new URL("https://api.github.com/repos/pgjdbc/pgjdbc/commits/$sha")
-    def jsonSlurper = new JsonSlurper()
-    def commit = jsonSlurper.parse(url)
-    def author = commit.commit.author
-    if (!contributors.containsKey(author.name)) {
-        if ( author.html_url) {
-            contributors.put(author.name, author.html_url)
-        } else {
-            contributors.put(author.name, author.email)
+        def url = new URL("https://api.github.com/repos/pgjdbc/pgjdbc/commits/$sha")
+        def jsonSlurper = new JsonSlurper()
+        def commit = jsonSlurper.parse(url)
+        def author = commit.commit.author
+        if (!contributors.containsKey(author.name)) {
+            if ( author.html_url) {
+                contributors.put(author.name, author.html_url)
+            } else {
+                contributors.put(author.name, author.email)
+            }
         }
     }
-}
 
-def getContributors(File repoPath, previousRelease) {
-    def authorCommits = ""
-    def jsonSlurper = new JsonSlurper()
-    File contributorFile = new File("$repoPath.path/contributors.json")
-    def contributors = jsonSlurper.parse(contributorFile)
+    def getContributors(File repoPath, previousRelease) {
+        def authorCommits = ""
+        def jsonSlurper = new JsonSlurper()
+        File contributorFile = new File("$repoPath.path/contributors.json")
+        def contributors = jsonSlurper.parse(contributorFile, "UTF-8")
 
-    String[] commands = ["git", "shortlog", "--format=%s@@@%H@@@%h@@@",
-                         "--grep=maven-release-plugin|update versions in readme.md",
-                         "--extended-regexp", "--invert-grep", "--no-merges", "$previousRelease..HEAD"]
+        String[] commands = ["git", "shortlog", "--format=%s@@@%H@@@%h@@@",
+                             "--grep=maven-release-plugin|update versions in readme.md",
+                             "--extended-regexp", "--invert-grep", "--no-merges", "$previousRelease..HEAD"]
 
-    def gitProcess = commands.execute(null, repoPath)
-    gitProcess.waitFor()
+        def gitProcess = commands.execute(null, repoPath)
+        gitProcess.waitFor()
 
-    if (gitProcess.exitValue() != 0) {
-        gitProcess.err.eachLine {
-            println it
-        }
-    } else {
-        gitProcess.in.eachLine {
-            if (it.contains('@@@')) {
-                String[] components = it.split('@@@')
-                String subject = components[0]
-                String sha = components[1]
-                String shortSha = components[2]
-                getContributorForSha(contributors, shortSha)
-                def pattern = /\(?#(\d+)\)?/
-                if (subject =~ pattern) {
-                    subject = subject.replaceFirst(pattern, "[PR ${1}](https://github.com/pgjdbc/pgjdbc/pull/${1})")
-                    //$matcher.group(1)"
+        if (gitProcess.exitValue() != 0) {
+            gitProcess.err.eachLine {
+                println it
+            }
+        } else {
+            gitProcess.in.eachLine {
+                if (it.contains('@@@')) {
+                    String[] components = it.split('@@@')
+                    String subject = components[0]
+                    String sha = components[1]
+                    String shortSha = components[2]
+                    getContributorForSha(contributors, shortSha)
+                    def pattern = /\(?#(\d+)\)?/
+                    def m = subject =~ pattern
+                    if (m){
+                        subject = subject.replaceFirst(pattern, "[PR ${m[0][1]}](https://github.com/pgjdbc/pgjdbc/pull/${m[0][1]})")
+                        authorCommits += subject + '\n'
+                        //$matcher.group(1)"
+                    }
+
                 } else {
-                    subject = ""
+                    authorCommits += it + '\n'
                 }
 
-                authorCommits += subject + '\n'
-            } else {
-                authorCommits += it + '\n'
             }
-
         }
-    }
 
-    //TODO: This doesn't write the braces before and after
-    contributorFile.write(new JsonBuilder(contributors).toPrettyString())
-    return authorCommits
+        int numContributors = contributors.size()
+        contributorFile.write("{\n", "UTF-8")
+        contributors.eachWithIndex { key, value, index ->
+            // don't write out the comma for the last line
+            contributorFile.append("   \"$key\" : \"${value?value:"N/A"}\"${(index+1) < numContributors?',':''}\n", "UTF-8")
+        }
+        contributorFile.append("}\n", "UTF-8")
+        return authorCommits
     }
 
 }
-
 
