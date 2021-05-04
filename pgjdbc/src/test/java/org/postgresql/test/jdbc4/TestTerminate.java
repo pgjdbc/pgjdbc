797bf6625ae399ecac9f68f84594617bf481b2b3
Dave Cramer <davecramer@gmail.com>
2021-05-04 08:37:29 -0400

    fix checkstyle

--- pgjdbc/src/test/java/org/postgresql/test/jdbc4/TestTerminate.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc4/TestTerminate.java
@@ -1,28 +1,39 @@
+/*
+ * Copyright (c) 2021, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
 package org.postgresql.test.jdbc4;
 
-import org.junit.After;
-import org.junit.Before;
-
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.postgresql.util.PSQLState.ERROR_CODE_CRASH_SHUTDOWN;
 
 import org.postgresql.test.TestUtil;
 import org.postgresql.test.jdbc2.BaseTest4;
-import org.postgresql.util.PSQLState;
 
-import java.sql.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static org.postgresql.util.PSQLState.ERROR_CODE_CRASH_SHUTDOWN;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.Statement;
 
 public class TestTerminate extends BaseTest4 {
   Connection con2;
+
   @Before
-  public void setup() throws Exception{
+  public void setup() throws Exception {
     super.setUp();
     TestUtil.createTempTable(con, "bigselect", "i int, t text");
     TestUtil.execute( "insert into bigselect (i, t) values (generate_series(1,10), 'a;kjdf;ajd;akj;kj;kj;j')", con);
     con2 = TestUtil.openDB();
   }
+
   @After
   public void tearDown() throws SQLException {
     super.tearDown();
@@ -30,25 +41,24 @@ public class TestTerminate extends BaseTest4 {
 
   @Test
   public void testTerminate() throws Exception {
-    try (Statement statement = con.createStatement()){
-      try ( ResultSet rs = statement.executeQuery( "select * from bigselect")){
+    try (Statement statement = con.createStatement()) {
+      try ( ResultSet rs = statement.executeQuery( "select * from bigselect")) {
         try {
           con2.createStatement().execute("COPY (SELECT pg_backend_pid()) TO PROGRAM 'xargs kill -SIGSEGV';");
         } catch (Exception ex ) {
           // ignore this
         }
-        while(rs.next()) {
+        while (rs.next()) {
           rs.getString(2);
         }
       }
       try (ResultSet rs = statement.executeQuery("select 1")) {
         fail("should not get here, above we have caused a fatal termination. This will close this connection");
-      } catch (SQLException ex ){
+      } catch (SQLException ex ) {
         SQLWarning sqlWarning = statement.getWarnings();
         assertNotNull(sqlWarning);
         assertEquals(sqlWarning.getSQLState(),ERROR_CODE_CRASH_SHUTDOWN.getState());
       }
-
     }
   }
 }
