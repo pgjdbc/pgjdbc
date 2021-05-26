7538d516956d19b4471b9b716d82b89f390b3ec1
Dave Cramer <davecramer@gmail.com>
2021-05-26 13:54:19 -0400

    fix: backpatch PR#507 which reworks OSGI bundle activator fixes ISSUE #2133

--- build.gradle.kts
+++ build.gradle.kts
@@ -716,8 +716,13 @@ subprojects {
                 } else {
                     "sspiImplementation"("com.github.waffle:waffle-jna")
                 }
-                "osgiImplementation"("org.osgi:org.osgi.core")
-                "osgiImplementation"("org.osgi:org.osgi.enterprise")
+                // The dependencies are provided by OSGi container,
+                // so they should not be exposed as transitive dependencies
+                "osgiCompileOnly"("org.osgi:org.osgi.core")
+                "osgiCompileOnly"("org.osgi:org.osgi.service.jdbc")
+                "testImplementation"("org.osgi:org.osgi.service.jdbc") {
+                    because("DataSourceFactory is needed for PGDataSourceFactoryTest")
+                }
             }
         }
     }
