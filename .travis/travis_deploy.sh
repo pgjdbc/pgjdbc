45a771fccd77c1211d8638111ee3e9934849b781
Sehrope Sarkuni <sehrope@jackdb.com>
2021-05-29 11:18:10 -0400

    test: Remove failing tests and scripts from Travis matrix
    Removes repeatedly failing tests from Travis matrix and old scripts that
    are now no longer being referenced. All the removed tests are covered by
    a matrix entry in the omni suite so there should be no loss of coerage.

--- .travis/travis_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x -e
-
-# Skip tests and checkstype to speed up snapshot deployment
-MVN_ARGS="clean deploy -B -V -DskipTests -Dcheckstyle.skip=true -Dskip.assembly=true --settings settings.xml"
-
-mvn ${MVN_ARGS} -P release-artifacts,release -Dskip.unzip-jdk-src=false
