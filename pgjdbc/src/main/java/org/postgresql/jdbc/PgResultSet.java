0077af21116f67112e03062424ee22424fe36e17
Dave Cramer <davecramer@gmail.com>
2021-07-02 08:49:07 -0400

    Fix: RefreshRow causes row to become readOnly Issue #2193 (#2195)
    * Fix: RefreshRow causes row to become readOnly Issue #2193

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
@@ -1385,7 +1385,12 @@ public class PgResultSet implements ResultSet, org.postgresql.PGRefCursorResultS
       PgResultSet rs = (PgResultSet) selectStatement.executeQuery();
 
       if (rs.next()) {
-        rowBuffer = rs.thisRow;
+        // we know that the row is updatable as it was tested above.
+        if ( rs.thisRow == null ) {
+          rowBuffer = null;
+        } else {
+          rowBuffer = castNonNull(rs.thisRow).updateableCopy();
+        }
       }
 
       castNonNull(rows).set(currentRow, castNonNull(rowBuffer));
