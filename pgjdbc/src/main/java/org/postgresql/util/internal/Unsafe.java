a7ee1cdf1ab74f1505dd56e33359a5ac3797b6d1
Severin Gehwolf <sgehwolf@redhat.com>
2021-11-23 16:53:39 +0100

    Use JDK provided API for looking at the KRB creds cache
    Closes #2023

--- pgjdbc/src/main/java/org/postgresql/util/internal/Unsafe.java
+++ pgjdbc/src/main/java/org/postgresql/util/internal/Unsafe.java
@@ -5,20 +5,85 @@
 
 package org.postgresql.util.internal;
 
-/**
- * This class is excluded from {@code forbidden-apis} check.
- * @see <a href="https://github.com/policeman-tools/forbidden-apis/issues/82">Extend
- * suppression configuration to be more fine grained</a>
- */
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag;
+import javax.security.auth.login.Configuration;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
 public class Unsafe {
+
+  static {
+    Configuration.setConfiguration(new CustomKrbConfig());
+  }
+
+  private static String CONFIG_ITEM_NAME = "ticketCache";
+  private static String KRBLOGIN_MODULE = "com.sun.security.auth.module.Krb5LoginModule";
+
+  /**
+   * Equivalent of:
+   *
+   * {@code
+   *
+   * ticketCache {
+   * com.sun.security.auth.module.Krb5LoginModule required
+   * refreshKrb5Config=false
+   * useTicketCache=true
+   * doNotPrompt=true
+   * useKeyTab=false
+   * renewTGT=false
+   * isInitiator=false debug=true; };
+   *
+   * }
+   *
+   */
+  static class CustomKrbConfig extends Configuration {
+
+    @Override
+    public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+        if (CONFIG_ITEM_NAME.equals(name)) {
+          Map<String, String> options = new HashMap<>();
+          options.put("refreshKrb5Config", Boolean.FALSE.toString());
+          options.put("useTicketCache", Boolean.TRUE.toString());
+          options.put("doNotPrompt", Boolean.TRUE.toString());
+          options.put("useKeyTab", Boolean.FALSE.toString());
+          options.put("isInitiator", Boolean.FALSE.toString());
+          options.put("renewTGT", Boolean.FALSE.toString());
+          options.put("debug", Boolean.FALSE.toString());
+          return new AppConfigurationEntry[] {
+            new AppConfigurationEntry(KRBLOGIN_MODULE,
+              LoginModuleControlFlag.REQUIRED, options) };
+        }
+        return null;
+      }
+
+  }
   public static boolean credentialCacheExists() {
+    LoginContext lc = null;
     try {
-      @SuppressWarnings({"nullness"})
-      sun.security.krb5.Credentials credentials =
-          sun.security.krb5.Credentials.acquireTGTFromCache(null, null);
-      return credentials != null;
-    } catch (Exception ex) {
+      lc = new LoginContext(CONFIG_ITEM_NAME, new CallbackHandler() {
+
+        @Override
+        public void handle(Callback[] callbacks)
+                throws IOException, UnsupportedCallbackException {
+            // config has doNotPrompt, so it should never happen
+            throw new RuntimeException("Should not happen!");
+        }
+
+      });
+      lc.login();
+    } catch (LoginException e) {
       return false;
     }
+    Subject sub = lc.getSubject();
+    return sub != null;
   }
 }
