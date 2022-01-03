4d9fc6239cb1f26121817113ca4609abc824ca09
Dave Cramer <davecramer@gmail.com>
2022-01-03 09:26:34 -0500

    rework test to only create the big LO once
    Modernize tests

--- pgjdbc/src/test/java/org/postgresql/test/jdbc2/BlobTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc2/BlobTest.java
@@ -16,7 +16,9 @@ import org.postgresql.largeobject.LargeObjectManager;
 import org.postgresql.test.TestUtil;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -42,115 +44,129 @@ public class BlobTest {
 
   private Connection con;
 
+  /*
+    Only do this once
+  */
+  @BeforeClass
+  public static void createLargeBlob() throws Exception {
+    try ( Connection con = TestUtil.openDB() ) {
+      TestUtil.createTable(con, "testblob", "id name,lo oid");
+      con.setAutoCommit(false);
+      LargeObjectManager lom = ((org.postgresql.PGConnection) con).getLargeObjectAPI();
+      long oid = lom.createLO(LargeObjectManager.READWRITE);
+      LargeObject blob = lom.open(oid);
+
+      byte[] buf = new byte[256];
+      for (int i = 0; i < buf.length; i++) {
+        buf[i] = (byte) i;
+      }
+      // I want to create a large object
+      int i = 1024 / buf.length;
+      for (int j = i; j > 0; j--) {
+        blob.write(buf, 0, buf.length);
+      }
+      assertEquals(1024, blob.size());
+      blob.close();
+      try (PreparedStatement pstmt = con.prepareStatement("INSERT INTO testblob(id, lo) VALUES(?,?)")) {
+        pstmt.setString(1, "l1");
+        pstmt.setLong(2, oid);
+        pstmt.executeUpdate();
+      }
+      con.commit();
+    }
+  }
+
+  @AfterClass
+  public static void cleanup() throws Exception {
+    try ( Connection con = TestUtil.openDB() ) {
+      try (Statement stmt = con.createStatement()) {
+        stmt.execute("SELECT lo_unlink(lo) FROM testblob where id = 'l1'");
+      } finally {
+        TestUtil.dropTable(con, "testblob");
+      }
+    }
+  }
+
   @Before
   public void setUp() throws Exception {
     con = TestUtil.openDB();
-    TestUtil.createTable(con, "testblob", "id name,lo oid");
-    con.setAutoCommit(false);
-    LargeObjectManager lom = ((org.postgresql.PGConnection) con).getLargeObjectAPI();
-    long oid = lom.createLO(LargeObjectManager.READWRITE);
-    LargeObject blob = lom.open(oid);
-
-    byte []buf = new byte[256];
-    for ( int i = 0; i < buf.length; i++ ) {
-      buf[i] = (byte)i;
-    }
-    // I want to create a large object
-    int i = 1024 / buf.length;
-    for ( int j = i; j > 0; j--)  {
-      blob.write(buf, 0, buf.length);
-    }
-    assertEquals(1024,blob.size());
-    blob.close();
-    try ( PreparedStatement pstmt = con.prepareStatement("INSERT INTO testblob(id, lo) VALUES(?,?)")) {
-      pstmt.setString(1, "l1");
-      pstmt.setLong(2, oid);
-      pstmt.executeUpdate();
-    }
-    con.commit();
     con.setAutoCommit(false);
   }
 
   @After
   public void tearDown() throws Exception {
     con.setAutoCommit(true);
-    try {
-      Statement stmt = con.createStatement();
-      try {
-        stmt.execute("SELECT lo_unlink(lo) FROM testblob");
-      } finally {
-        try {
-          stmt.close();
-        } catch (Exception e) {
-        }
-      }
+    try ( Statement stmt = con.createStatement() ) {
+      stmt.execute("SELECT lo_unlink(lo) FROM testblob where id != 'l1'");
+      stmt.execute("delete from testblob where id != 'l1'");
     } finally {
-      TestUtil.dropTable(con, "testblob");
       TestUtil.closeDB(con);
     }
   }
 
   @Test
   public void testSetNull() throws Exception {
-    PreparedStatement pstmt = con.prepareStatement("INSERT INTO testblob(lo) VALUES (?)");
+    try (PreparedStatement pstmt = con.prepareStatement("INSERT INTO testblob(lo) VALUES (?)")) {
 
-    pstmt.setBlob(1, (Blob) null);
-    pstmt.executeUpdate();
+      pstmt.setBlob(1, (Blob) null);
+      pstmt.executeUpdate();
 
-    pstmt.setNull(1, Types.BLOB);
-    pstmt.executeUpdate();
+      pstmt.setNull(1, Types.BLOB);
+      pstmt.executeUpdate();
 
-    pstmt.setObject(1, null, Types.BLOB);
-    pstmt.executeUpdate();
+      pstmt.setObject(1, null, Types.BLOB);
+      pstmt.executeUpdate();
 
-    pstmt.setClob(1, (Clob) null);
-    pstmt.executeUpdate();
+      pstmt.setClob(1, (Clob) null);
+      pstmt.executeUpdate();
 
-    pstmt.setNull(1, Types.CLOB);
-    pstmt.executeUpdate();
+      pstmt.setNull(1, Types.CLOB);
+      pstmt.executeUpdate();
 
-    pstmt.setObject(1, null, Types.CLOB);
-    pstmt.executeUpdate();
+      pstmt.setObject(1, null, Types.CLOB);
+      pstmt.executeUpdate();
+    }
   }
 
   @Test
   public void testSet() throws SQLException {
-    Statement stmt = con.createStatement();
-    stmt.execute("INSERT INTO testblob(id,lo) VALUES ('1', lo_creat(-1))");
-    ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '1'");
-    assertTrue(rs.next());
+    try ( Statement stmt = con.createStatement() ) {
+      stmt.execute("INSERT INTO testblob(id,lo) VALUES ('1', lo_creat(-1))");
+      ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '1'");
+      assertTrue(rs.next());
 
-    PreparedStatement pstmt = con.prepareStatement("INSERT INTO testblob(id, lo) VALUES(?,?)");
+      PreparedStatement pstmt = con.prepareStatement("INSERT INTO testblob(id, lo) VALUES(?,?)");
 
-    Blob blob = rs.getBlob(1);
-    pstmt.setString(1, "setObjectTypeBlob");
-    pstmt.setObject(2, blob, Types.BLOB);
-    assertEquals(1, pstmt.executeUpdate());
+      Blob blob = rs.getBlob(1);
+      pstmt.setString(1, "setObjectTypeBlob");
+      pstmt.setObject(2, blob, Types.BLOB);
+      assertEquals(1, pstmt.executeUpdate());
 
-    blob = rs.getBlob(1);
-    pstmt.setString(1, "setObjectBlob");
-    pstmt.setObject(2, blob);
-    assertEquals(1, pstmt.executeUpdate());
+      blob = rs.getBlob(1);
+      pstmt.setString(1, "setObjectBlob");
+      pstmt.setObject(2, blob);
+      assertEquals(1, pstmt.executeUpdate());
 
-    blob = rs.getBlob(1);
-    pstmt.setString(1, "setBlob");
-    pstmt.setBlob(2, blob);
-    assertEquals(1, pstmt.executeUpdate());
+      blob = rs.getBlob(1);
+      pstmt.setString(1, "setBlob");
+      pstmt.setBlob(2, blob);
+      assertEquals(1, pstmt.executeUpdate());
 
-    Clob clob = rs.getClob(1);
-    pstmt.setString(1, "setObjectTypeClob");
-    pstmt.setObject(2, clob, Types.CLOB);
-    assertEquals(1, pstmt.executeUpdate());
+      Clob clob = rs.getClob(1);
+      pstmt.setString(1, "setObjectTypeClob");
+      pstmt.setObject(2, clob, Types.CLOB);
+      assertEquals(1, pstmt.executeUpdate());
 
-    clob = rs.getClob(1);
-    pstmt.setString(1, "setObjectClob");
-    pstmt.setObject(2, clob);
-    assertEquals(1, pstmt.executeUpdate());
+      clob = rs.getClob(1);
+      pstmt.setString(1, "setObjectClob");
+      pstmt.setObject(2, clob);
+      assertEquals(1, pstmt.executeUpdate());
 
-    clob = rs.getClob(1);
-    pstmt.setString(1, "setClob");
-    pstmt.setClob(2, clob);
-    assertEquals(1, pstmt.executeUpdate());
+      clob = rs.getClob(1);
+      pstmt.setString(1, "setClob");
+      pstmt.setClob(2, clob);
+      assertEquals(1, pstmt.executeUpdate());
+    }
   }
 
   /*
@@ -183,90 +199,99 @@ public class BlobTest {
   public void testMarkResetStream() throws Exception {
     assertTrue(uploadFile(TEST_FILE, NATIVE_STREAM) > 0);
 
-    Statement stmt = con.createStatement();
-    ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '/test-file.xml'");
-    assertTrue(rs.next());
+    try ( Statement stmt = con.createStatement() ) {
+      try ( ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '/test-file.xml'") ) {
+        assertTrue(rs.next());
 
-    LargeObjectManager lom = ((org.postgresql.PGConnection) con).getLargeObjectAPI();
+        LargeObjectManager lom = ((org.postgresql.PGConnection) con).getLargeObjectAPI();
 
-    long oid = rs.getLong(1);
-    LargeObject blob = lom.open(oid);
-    InputStream bis = blob.getInputStream();
+        long oid = rs.getLong(1);
+        LargeObject blob = lom.open(oid);
+        InputStream bis = blob.getInputStream();
 
-    assertEquals('<', bis.read());
-    bis.mark(4);
-    assertEquals('?', bis.read());
-    assertEquals('x', bis.read());
-    assertEquals('m', bis.read());
-    assertEquals('l', bis.read());
-    bis.reset();
-    assertEquals('?', bis.read());
+        assertEquals('<', bis.read());
+        bis.mark(4);
+        assertEquals('?', bis.read());
+        assertEquals('x', bis.read());
+        assertEquals('m', bis.read());
+        assertEquals('l', bis.read());
+        bis.reset();
+        assertEquals('?', bis.read());
+      }
+    }
   }
 
   @Test
   public void testGetBytesOffset() throws Exception {
     assertTrue(uploadFile(TEST_FILE, NATIVE_STREAM) > 0);
 
-    Statement stmt = con.createStatement();
-    ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '/test-file.xml'");
-    assertTrue(rs.next());
+    try ( Statement stmt = con.createStatement() ) {
+      try (ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '/test-file.xml'")) {
 
-    Blob lob = rs.getBlob(1);
-    byte[] data = lob.getBytes(2, 4);
-    assertEquals(data.length, 4);
-    assertEquals(data[0], '?');
-    assertEquals(data[1], 'x');
-    assertEquals(data[2], 'm');
-    assertEquals(data[3], 'l');
+        assertTrue(rs.next());
+
+        Blob lob = rs.getBlob(1);
+        byte[] data = lob.getBytes(2, 4);
+        assertEquals(data.length, 4);
+        assertEquals(data[0], '?');
+        assertEquals(data[1], 'x');
+        assertEquals(data[2], 'm');
+        assertEquals(data[3], 'l');
+      }
+    }
   }
 
   @Test
   public void testMultipleStreams() throws Exception {
     assertTrue(uploadFile(TEST_FILE, NATIVE_STREAM) > 0);
 
-    Statement stmt = con.createStatement();
-    ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '/test-file.xml'");
-    assertTrue(rs.next());
+    try ( Statement stmt = con.createStatement() ) {
+      try (ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '/test-file.xml'")) {
+        assertTrue(rs.next());
 
-    Blob lob = rs.getBlob(1);
-    byte[] data = new byte[2];
+        Blob lob = rs.getBlob(1);
+        byte[] data = new byte[2];
 
-    InputStream is = lob.getBinaryStream();
-    assertEquals(data.length, is.read(data));
-    assertEquals(data[0], '<');
-    assertEquals(data[1], '?');
-    is.close();
+        InputStream is = lob.getBinaryStream();
+        assertEquals(data.length, is.read(data));
+        assertEquals(data[0], '<');
+        assertEquals(data[1], '?');
+        is.close();
 
-    is = lob.getBinaryStream();
-    assertEquals(data.length, is.read(data));
-    assertEquals(data[0], '<');
-    assertEquals(data[1], '?');
-    is.close();
+        is = lob.getBinaryStream();
+        assertEquals(data.length, is.read(data));
+        assertEquals(data[0], '<');
+        assertEquals(data[1], '?');
+        is.close();
+      }
+    }
   }
 
   @Test
   public void testParallelStreams() throws Exception {
     assertTrue(uploadFile(TEST_FILE, NATIVE_STREAM) > 0);
 
-    Statement stmt = con.createStatement();
-    ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id='/test-file.xml'");
-    assertTrue(rs.next());
+    try ( Statement stmt = con.createStatement() ) {
+      try (ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id = '/test-file.xml'")) {
+        assertTrue(rs.next());
 
-    Blob lob = rs.getBlob(1);
-    InputStream is1 = lob.getBinaryStream();
-    InputStream is2 = lob.getBinaryStream();
+        Blob lob = rs.getBlob(1);
+        InputStream is1 = lob.getBinaryStream();
+        InputStream is2 = lob.getBinaryStream();
 
-    while (true) {
-      int i1 = is1.read();
-      int i2 = is2.read();
-      assertEquals(i1, i2);
-      if (i1 == -1) {
-        break;
+        while (true) {
+          int i1 = is1.read();
+          int i2 = is2.read();
+          assertEquals(i1, i2);
+          if (i1 == -1) {
+            break;
+          }
+        }
+
+        is1.close();
+        is2.close();
       }
     }
-
-    is1.close();
-    is2.close();
   }
 
   @Test
@@ -275,15 +300,17 @@ public class BlobTest {
       return;
     }
 
-    Statement stmt = con.createStatement();
-    stmt.execute("INSERT INTO testblob(id,lo) VALUES ('1', lo_creat(-1))");
-    ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id ='1'");
-    assertTrue(rs.next());
+    try ( Statement stmt = con.createStatement() ) {
+      stmt.execute("INSERT INTO testblob(id,lo) VALUES ('1', lo_creat(-1))");
+      try ( ResultSet rs = stmt.executeQuery("SELECT lo FROM testblob where id ='1'") ) {
+        assertTrue(rs.next());
 
-    Blob lob = rs.getBlob(1);
-    long length = ((long) Integer.MAX_VALUE) + 1024;
-    lob.truncate(length);
-    assertEquals(length, lob.length());
+        Blob lob = rs.getBlob(1);
+        long length = ((long) Integer.MAX_VALUE) + 1024;
+        lob.truncate(length);
+        assertEquals(length, lob.length());
+      }
+    }
   }
 
   @Test
@@ -394,39 +421,38 @@ public class BlobTest {
 
     LargeObjectManager lom = ((org.postgresql.PGConnection) con).getLargeObjectAPI();
 
-    Statement st = con.createStatement();
-    ResultSet rs = st.executeQuery(TestUtil.selectSQL("testblob", "id,lo", "id = '" + id + "'"));
-    assertNotNull(rs);
+    try ( Statement st = con.createStatement() ) {
+      try ( ResultSet rs = st.executeQuery(TestUtil.selectSQL("testblob", "id,lo", "id = '" + id + "'")) ) {
+        assertNotNull(rs);
 
-    while (rs.next()) {
-      String file = rs.getString(1);
-      long oid = rs.getLong(2);
+        while (rs.next()) {
+          String file = rs.getString(1);
+          long oid = rs.getLong(2);
 
-      InputStream fis = getClass().getResourceAsStream(file);
-      LargeObject blob = lom.open(oid);
-      InputStream bis = blob.getInputStream();
+          InputStream fis = getClass().getResourceAsStream(file);
+          LargeObject blob = lom.open(oid);
+          InputStream bis = blob.getInputStream();
 
-      int f = fis.read();
-      int b = bis.read();
-      int c = 0;
-      while (f >= 0 && b >= 0 & result) {
-        result = (f == b);
-        f = fis.read();
-        b = bis.read();
-        c++;
+          int f = fis.read();
+          int b = bis.read();
+          int c = 0;
+          while (f >= 0 && b >= 0 & result) {
+            result = (f == b);
+            f = fis.read();
+            b = bis.read();
+            c++;
+          }
+          result = result && f == -1 && b == -1;
+
+          if (!result) {
+            fail("Large Object API Blob compare failed at " + c + " of " + blob.size());
+          }
+
+          blob.close();
+          fis.close();
+        }
       }
-      result = result && f == -1 && b == -1;
-
-      if (!result) {
-        fail("Large Object API Blob compare failed at " + c + " of " + blob.size());
-      }
-
-      blob.close();
-      fis.close();
     }
-    rs.close();
-    st.close();
-
     return result;
   }
 
@@ -436,38 +462,37 @@ public class BlobTest {
   private boolean compareBlobs(String id) throws Exception {
     boolean result = true;
 
-    Statement st = con.createStatement();
-    ResultSet rs = st.executeQuery(TestUtil.selectSQL("testblob", "id,lo", "id = '" + id + "'"));
-    assertNotNull(rs);
+    try ( Statement st = con.createStatement() ) {
+      try (ResultSet rs = st.executeQuery(TestUtil.selectSQL("testblob", "id,lo", "id = '" + id + "'"))) {
+        assertNotNull(rs);
 
-    while (rs.next()) {
-      String file = rs.getString(1);
-      Blob blob = rs.getBlob(2);
+        while (rs.next()) {
+          String file = rs.getString(1);
+          Blob blob = rs.getBlob(2);
 
-      InputStream fis = getClass().getResourceAsStream(file);
-      InputStream bis = blob.getBinaryStream();
+          InputStream fis = getClass().getResourceAsStream(file);
+          InputStream bis = blob.getBinaryStream();
 
-      int f = fis.read();
-      int b = bis.read();
-      int c = 0;
-      while (f >= 0 && b >= 0 & result) {
-        result = (f == b);
-        f = fis.read();
-        b = bis.read();
-        c++;
+          int f = fis.read();
+          int b = bis.read();
+          int c = 0;
+          while (f >= 0 && b >= 0 & result) {
+            result = (f == b);
+            f = fis.read();
+            b = bis.read();
+            c++;
+          }
+          result = result && f == -1 && b == -1;
+
+          if (!result) {
+            fail("JDBC API Blob compare failed at " + c + " of " + blob.length());
+          }
+
+          bis.close();
+          fis.close();
+        }
       }
-      result = result && f == -1 && b == -1;
-
-      if (!result) {
-        fail("JDBC API Blob compare failed at " + c + " of " + blob.length());
-      }
-
-      bis.close();
-      fis.close();
     }
-    rs.close();
-    st.close();
-
     return result;
   }
 
@@ -477,37 +502,37 @@ public class BlobTest {
   private boolean compareClobs(String id) throws Exception {
     boolean result = true;
 
-    Statement st = con.createStatement();
-    ResultSet rs = st.executeQuery(TestUtil.selectSQL("testblob", "id,lo", "id = '" + id + "'"));
-    assertNotNull(rs);
+    try ( Statement st = con.createStatement() ) {
+      try (ResultSet rs = st.executeQuery(TestUtil.selectSQL("testblob", "id,lo", "id = '" + id + "'"))) {
+        assertNotNull(rs);
 
-    while (rs.next()) {
-      String file = rs.getString(1);
-      Clob clob = rs.getClob(2);
+        while (rs.next()) {
+          String file = rs.getString(1);
+          Clob clob = rs.getClob(2);
 
-      InputStream fis = getClass().getResourceAsStream(file);
-      InputStream bis = clob.getAsciiStream();
+          InputStream fis = getClass().getResourceAsStream(file);
+          InputStream bis = clob.getAsciiStream();
 
-      int f = fis.read();
-      int b = bis.read();
-      int c = 0;
-      while (f >= 0 && b >= 0 & result) {
-        result = (f == b);
-        f = fis.read();
-        b = bis.read();
-        c++;
+          int f = fis.read();
+          int b = bis.read();
+          int c = 0;
+          while (f >= 0 && b >= 0 & result) {
+            result = (f == b);
+            f = fis.read();
+            b = bis.read();
+            c++;
+          }
+          result = result && f == -1 && b == -1;
+
+          if (!result) {
+            fail("Clob compare failed at " + c + " of " + clob.length());
+          }
+
+          bis.close();
+          fis.close();
+        }
       }
-      result = result && f == -1 && b == -1;
-
-      if (!result) {
-        fail("Clob compare failed at " + c + " of " + clob.length());
-      }
-
-      bis.close();
-      fis.close();
     }
-    rs.close();
-    st.close();
 
     return result;
   }
