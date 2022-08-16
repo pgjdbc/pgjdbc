93c5c80ba9a184284305a83e58a51e0890b14f1c
Dave Cramer <davecramer@gmail.com>
2022-08-16 10:01:21 -0400

    fix checkstyle, unused import

--- pgjdbc/src/main/java/org/postgresql/gss/MakeGSS.java
+++ pgjdbc/src/main/java/org/postgresql/gss/MakeGSS.java
@@ -22,7 +22,6 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.security.PrivilegedAction;
-import java.security.PrivilegedExceptionAction;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.logging.Level;
