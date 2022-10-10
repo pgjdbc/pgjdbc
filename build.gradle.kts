8ec1004ef70af01233b4489d7cbb8c18b500e77b
Dave Cramer <davecramer@gmail.com>
2022-10-10 17:04:47 -0400

    remove javadoc links for java 17 and above

--- build.gradle.kts
+++ build.gradle.kts
@@ -348,7 +348,9 @@ allprojects {
                 header = "<b>PostgreSQL JDBC</b>"
                 bottom =
                     "Copyright &copy; 1997-$lastEditYear PostgreSQL Global Development Group. All Rights Reserved."
-                if (JavaVersion.current() >= JavaVersion.VERSION_1_9) {
+                if  (JavaVersion.current() >= JavaVersion.VERSION_17 ) {
+                    addBooleanOption("html5", true)
+                } else if (JavaVersion.current() >= JavaVersion.VERSION_1_9 ) {
                     addBooleanOption("html5", true)
                     links("https://docs.oracle.com/javase/9/docs/api/")
                 } else {
