8ab22a463592c818ebef086d5781c77d91d3fc29
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-02-11 14:58:07 +0300

    chore: skip autostyle markdown task to workaround failure
    The failure is
    
        Execution failed for task ':benchmarks:autostyleMarkdownProcess':
            org.gradle.internal.execution.fingerprint.InputFingerprinter$InputFileFingerprintingException: Cannot fingerprint input file property 'sourceFiles': Could not stat file D:\a\pgjdbc\pgjdbc\benchmarks\**\*.md
                at org.gradle.internal.execution.fingerprint.impl.DefaultInputFingerprinter$InputCollectingVisitor.visitInputFileProperty(DefaultInputFingerprinter.java:141)
                at org.gradle.internal.execution.steps.SkipEmptyWorkStep$1.visitInputFileProperty(SkipEmptyWorkStep.java:135)
    ..
                Caused by: net.rubygrapefruit.platform.NativeException: Could not get file details of D:\a\pgjdbc\pgjdbc\benchmarks\**\*.md: could not file attributes (errno 123)
                    at net.rubygrapefruit.platform.internal.DefaultWindowsFiles.stat(DefaultWindowsFiles.java:38)
                    at net.rubygrapefruit.platform.internal.DefaultWindowsFiles.stat(DefaultWindowsFiles.java:28)

--- build-logic/verification/src/main/kotlin/build-logic.autostyle.gradle.kts
+++ build-logic/verification/src/main/kotlin/build-logic.autostyle.gradle.kts
@@ -1,3 +1,5 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
 plugins {
     id("com.github.autostyle")
 }
@@ -8,9 +10,13 @@ autostyle {
         trimTrailingWhitespace()
         endWithNewline()
     }
-    format("markdown") {
-        target("**/*.md")
-        endWithNewline()
+    // TODO: the task fails on Windows as follows:
+    // Cannot fingerprint input file property 'sourceFiles': Could not stat file D:\a\pgjdbc\pgjdbc\benchmarks\**\*.md
+    if (!Os.isFamily(Os.FAMILY_WINDOWS)) {
+        format("markdown") {
+            target("**/*.md")
+            endWithNewline()
+        }
     }
 }
 
