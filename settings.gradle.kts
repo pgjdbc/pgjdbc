94534b94ccbea751e5b41b4192dab9a335172d15
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-03-06 19:40:02 +0300

    chore: bump com.github.lburgazzoli.karaf to 0.5.6 to fix deprecations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         id("biz.aQute.bnd.builder") version "6.4.0"
         id("com.github.burrunan.s3-build-cache") version "1.5"
         id("com.github.johnrengelman.shadow") version "7.1.2"
-        id("com.github.lburgazzoli.karaf") version "0.5.4"
+        id("com.github.lburgazzoli.karaf") version "0.5.6"
         id("com.github.vlsi.crlf") version "1.86"
         id("com.github.vlsi.gettext") version "1.86"
         id("com.github.vlsi.gradle-extensions") version "1.86"
