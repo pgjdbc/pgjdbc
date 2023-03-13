4f524679faaf3a253f9e496bc8afa4d8aa7971c0
Renzo Dani <arons7@gmail.com>
2023-03-13 21:37:42 +0100

    java.time does not look proper handled, especially ZonedDateTime

--- pgjdbc/src/test/java/org/postgresql/test/jdbc2/TimestamptzTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc2/TimestamptzTest.java
@@ -9,22 +9,25 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.postgresql.test.TestUtil;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.Types;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.postgresql.test.TestUtil;
 
 /**
  * Tests both db and java side that correct type are passed.
@@ -34,7 +37,7 @@ public class TimestamptzTest extends BaseTest4 {
 
   public TimestamptzTest(BinaryMode binaryMode) {
     setBinaryMode(binaryMode);
-    setTimestamptzAlways(TimestamptzAlways.YES);
+//    setTimestamptzAlways(TimestamptzAlways.YES);
   }
 
   @Parameterized.Parameters(name = "timestamptzAlways = {1}")
@@ -166,4 +169,72 @@ public class TimestamptzTest extends BaseTest4 {
       assertEquals(java.sql.Types.TIMESTAMP_WITH_TIMEZONE, md.getColumnType(1)); // currently give instead TIMESTAMP
     }
   }
+  
+  
+  
+  @Test
+  public void testJavaTime_ZonedDateTime() throws SQLException {
+    GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("Europe/Berlin"));
+    cal.set(2023, Calendar.MARCH, 12, 9, 30);
+    cal.set(Calendar.SECOND, 0);
+    cal.set(Calendar.MILLISECOND, 0);
+    
+    //Problem 
+    // java.lang.AssertionError: Can't infer the SQL type to use for an instance of java.time.ZonedDateTime. Use setObject() with an explicit Types value to specify the type to use.
+    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
+      ps.setObject(1, cal.toZonedDateTime());
+    } catch (SQLException e) {
+      fail(e.getMessage());
+    }
+    
+    //Problem 
+    // java.lang.AssertionError: Bad value for type timestamp/date/time: 2023-03-12T09:30+01:00[Europe/Berlin]
+    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
+      ps.setObject(1, cal.toZonedDateTime(), Types.TIMESTAMP);
+    } catch (SQLException e) {
+      fail(e.getMessage());
+    }
+  }
+  
+  
+  @Test
+  public void testJavaTime_Instant() throws SQLException {
+    //Problem
+    // java.lang.AssertionError: Can't infer the SQL type to use for an instance of java.time.ZonedDateTime. Use setObject() with an explicit Types value to specify the type to use.
+    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
+      ps.setObject(1, Instant.now());
+    } catch (SQLException e) {
+      fail(e.getMessage());
+    }
+    
+    //Problem 
+    // java.lang.AssertionError: Bad value for type timestamp/date/time: 2023-03-13T20:34:58.711330339Z
+    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
+      ps.setObject(1, Instant.now(), Types.TIMESTAMP);
+    } catch (SQLException e) {
+      fail(e.getMessage());
+    }
+  }
+  
+  
+  @Test
+  public void testJavaTime_LocalTime() throws SQLException {
+    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
+      ps.setObject(1, LocalTime.now());
+    } catch (SQLException e) {
+      fail(e.getMessage());
+    }
+  }
+  
+  
+  @Test
+  public void testJavaTime_LocalDateTime() throws SQLException {
+    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
+      ps.setObject(1, LocalDateTime.now());
+    } catch (SQLException e) {
+      fail(e.getMessage());
+    }
+  }
+  
+  
 }
