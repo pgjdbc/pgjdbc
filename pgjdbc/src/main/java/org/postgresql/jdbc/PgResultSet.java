2b85da8efc442a940643b7142e19bf1854779cd3
Sergey Zhuravlev <sergey.zhuravlev@gmail.com>
2023-04-21 17:11:44 +0200

    feat: method getFastLong should be able to parse all longs

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
@@ -2558,9 +2558,12 @@ public class PgResultSet implements ResultSet, org.postgresql.PGRefCursorResultS
     }
   };
 
+  private static final long MAX_LONG_DIV_TEN = Long.MAX_VALUE / 10;
+
   /**
    * Optimised byte[] to number parser. This code does not handle null values, so the caller must do
-   * checkResultSet and handle null values prior to calling this function.
+   * checkResultSet and handle null values prior to calling this function. Fraction part is
+   * discarded.
    *
    * @param bytes integer represented as a sequence of ASCII bytes
    * @return The parsed number.
@@ -2568,50 +2571,53 @@ public class PgResultSet implements ResultSet, org.postgresql.PGRefCursorResultS
    *         The value must then be parsed by {@link #toLong(String)}.
    */
   private long getFastLong(byte[] bytes, long minVal, long maxVal) throws NumberFormatException {
-    if (bytes.length == 0) {
+    int len = bytes.length;
+    if (len == 0) {
       throw FAST_NUMBER_FAILED;
     }
 
-    long val = 0;
-    int start;
-    boolean neg;
-    if (bytes[0] == '-') {
-      neg = true;
-      start = 1;
-      if (bytes.length == 1 || bytes.length > 19) {
-        throw FAST_NUMBER_FAILED;
-      }
-    } else {
-      start = 0;
-      neg = false;
-      if (bytes.length > 18) {
-        throw FAST_NUMBER_FAILED;
-      }
-    }
+    boolean neg = bytes[0] == '-';
 
-    int periodsSeen = 0;
-    while (start < bytes.length) {
+    long val = 0;
+    int start = neg ? 1 : 0;
+    while (start < len) {
       byte b = bytes[start++];
       if (b < '0' || b > '9') {
-        if (b == '.' && periodsSeen == 0) {
-          periodsSeen++;
-          continue;
+        if (b == '.') {
+          if (neg && len == 2 || !neg && len == 1) {
+            // we have to check that string is not "." or "-."
+            throw FAST_NUMBER_FAILED;
+          }
+          while (start < len) {
+            b = bytes[start++];
+            if (b < '0' || b > '9') {
+              throw FAST_NUMBER_FAILED;
+            }
+          }
+          break;
         } else {
           throw FAST_NUMBER_FAILED;
         }
       }
-      if (periodsSeen == 0) {
+
+      if (val <= MAX_LONG_DIV_TEN) {
         val *= 10;
         val += b - '0';
+      } else {
+        throw FAST_NUMBER_FAILED;
       }
     }
 
-    int numNonSignChars = neg ? bytes.length - 1 : bytes.length;
-    if (periodsSeen > 1 || periodsSeen == numNonSignChars) {
-      throw FAST_NUMBER_FAILED;
-    }
-
-    if (neg) {
+    if (val < 0) {
+      // It is possible to get overflow in two situations:
+      // 1. for MIN_VALUE, because abs(MIN_VALUE)=MAX_VALUE+1. In this situation thanks to
+      //    complement arithmetic we got correct result and shouldn't do anything with it.
+      // 2. for incorrect string, representing a number greater than MAX_VALUE, for example
+      //    "9223372036854775809", it this case we have to throw exception
+      if (!(neg && val == Long.MIN_VALUE)) {
+        throw FAST_NUMBER_FAILED;
+      }
+    } else if (neg) {
       val = -val;
     }
 
