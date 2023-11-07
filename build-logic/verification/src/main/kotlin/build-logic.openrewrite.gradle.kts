b5ebb51fbbf739cc854831f4316ca5e7375b5ba4
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2023-11-07 14:11:18 +0300

    chore: exclude generated messages_*.java files from openrewrite processing

--- build-logic/verification/src/main/kotlin/build-logic.openrewrite.gradle.kts
+++ build-logic/verification/src/main/kotlin/build-logic.openrewrite.gradle.kts
@@ -5,3 +5,8 @@ plugins {
 dependencies {
     rewrite(platform("org.openrewrite.recipe:rewrite-recipe-bom:latest.integration"))
 }
+
+rewrite {
+    // This is auto-generated code, so there's no need to clean it up
+    exclusion("pgjdbc/src/main/java/org/postgresql/translation/messages_*.java")
+}
