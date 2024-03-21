cec5292c9754bda36cbc957248deb447f4d5cd42
Vishal Raj <vishalraj1802@gmail.com>
2024-03-21 15:34:07 -0400

    fixes checker framework errors

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgArray.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgArray.java
@@ -509,7 +509,7 @@ public class PgArray implements Array {
   }
 
   @Override
-  public boolean equals(Object obj) {
+  public boolean equals(@Nullable Object obj) {
     if (this == obj) {
       return true;
     }
