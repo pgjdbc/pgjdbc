eeb37dda71a76f70db220e26ac9c51573d4b539e
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2024-05-16 21:30:38 +0300

    perf: replace BufferedOutputStream with unsynchronized PgBufferedOutputStream, increase the send buffer size
    This fixes the following issues:
    1) Java 21 uses ReentrantLock for BufferedOutputStream, however, we do not need synchronization
    as we synchronize at the level of QueryExecutor.execute
    
    2) The default buffer size of 8192 was small for the modern connections
    
    3) The previous implementation of GSSOutputStream could produce oversize packets
    that would be rejected by the backend.
    
    
    Fixes https://github.com/pgjdbc/pgjdbc/pull/3245
    
    See https://github.com/postgres/postgres/blob/acecd6746cdc2df5ba8dcc2c2307c6560c7c2492/src/backend/libpq/be-secure-gssapi.c#L348

--- pgjdbc/src/main/java/org/postgresql/gss/GSSOutputStream.java
+++ pgjdbc/src/main/java/org/postgresql/gss/GSSOutputStream.java
@@ -5,81 +5,83 @@
 
 package org.postgresql.gss;
 
+import org.postgresql.util.internal.PgBufferedOutputStream;
+
 import org.ietf.jgss.GSSContext;
 import org.ietf.jgss.GSSException;
 import org.ietf.jgss.MessageProp;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
-public class GSSOutputStream extends OutputStream {
+/**
+ * Output stream that wraps each packed with GSS encryption.
+ */
+public class GSSOutputStream extends PgBufferedOutputStream {
+  private final PgBufferedOutputStream pgOut;
   private final GSSContext gssContext;
   private final MessageProp messageProp;
-  private final byte[] buffer;
-  private final byte[] int4Buf = new byte[4];
-  private int index;
-  private final OutputStream wrapped;
 
-  public GSSOutputStream(OutputStream out, GSSContext gssContext, MessageProp messageProp, int bufferSize) {
-    wrapped = out;
+  /**
+   * Creates GSS output stream.
+   * @param out output stream for the encrypted data
+   * @param gssContext gss context
+   * @param messageProp message properties
+   * @param maxTokenSize maximum length of the encrypted messages
+   */
+  public GSSOutputStream(PgBufferedOutputStream out, GSSContext gssContext, MessageProp messageProp, int maxTokenSize) throws GSSException {
+    super(out, getBufferSize(gssContext, messageProp, maxTokenSize));
+    this.pgOut = out;
     this.gssContext = gssContext;
     this.messageProp = messageProp;
-    buffer = new byte[bufferSize];
+  }
+
+  private static int getBufferSize(GSSContext gssContext, MessageProp messageProp, int maxTokenSize) throws GSSException {
+    return gssContext.getWrapSizeLimit(messageProp.getQOP(), messageProp.getPrivacy(), maxTokenSize);
   }
 
   @Override
-  public void write(int b) throws IOException {
-    buffer[index++] = (byte) b;
-    if (index >= buffer.length) {
-      flush();
+  protected void flushBuffer() throws IOException {
+    if (count > 0) {
+      writeWrapped(buf, 0, count);
+      count = 0;
     }
   }
 
-  @Override
-  public void write(byte[] buf) throws IOException {
-    write(buf, 0, buf.length);
-  }
-
-  @Override
-  public void write(byte[] b, int pos, int len) throws IOException {
-    int max;
-
-    while ( len > 0 ) {
-      int roomToWrite = buffer.length - index;
-      if ( len < roomToWrite ) {
-        System.arraycopy(b, pos, buffer, index, len);
-        index += len;
-        len -= roomToWrite;
-      } else {
-        System.arraycopy(b, pos, buffer, index, roomToWrite);
-        index += roomToWrite;
-        len -= roomToWrite;
-      }
-      if (roomToWrite == 0) {
-        flush();
-      }
-    }
-  }
-
-  @Override
-  public void flush() throws IOException {
+  private void writeWrapped(byte[] b, int off, int len) throws IOException {
     try {
-      byte[] token = gssContext.wrap(buffer, 0, index, messageProp);
-      sendInteger4Raw(token.length);
-      wrapped.write(token, 0, token.length);
-      index = 0;
-    } catch ( GSSException ex ) {
+      byte[] token = gssContext.wrap(buf, off, len, messageProp);
+      pgOut.writeInt4(token.length);
+      pgOut.write(token, 0, token.length);
+    } catch (GSSException ex) {
       throw new IOException(ex);
     }
-    wrapped.flush();
   }
 
-  private void sendInteger4Raw(int val) throws IOException {
-    int4Buf[0] = (byte) (val >>> 24);
-    int4Buf[1] = (byte) (val >>> 16);
-    int4Buf[2] = (byte) (val >>> 8);
-    int4Buf[3] = (byte) (val);
-    wrapped.write(int4Buf);
+  @Override
+  public void write(byte[] b, int off, int len) throws IOException {
+    if (count > 0) {
+      // If there's some data in the buffer, combine both
+      int avail = buf.length - count;
+      int prefixLength = Math.min(len, avail);
+      System.arraycopy(b, off, buf, count, prefixLength);
+      count += prefixLength;
+      off += prefixLength;
+      len -= prefixLength;
+      if (count == buf.length) {
+        flushBuffer();
+      }
+    }
+    // Write out the rest, chunk the writes, so we do not exceed the maximum encrypted message size
+    while (len >= buf.length) {
+      writeWrapped(b, off, buf.length);
+      off += buf.length;
+      len -= buf.length;
+    }
+    if (len == 0) {
+      return;
+    }
+    System.arraycopy(b, off, buf, 0, len);
+    count += len;
   }
 
 }
