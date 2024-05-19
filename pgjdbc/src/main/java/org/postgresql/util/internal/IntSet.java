5e3190d5f3ad667a42210bee881b04404332cf30
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2024-05-19 20:55:10 +0300

    perf: optimize Set<Integer> which are used for checking if oid should be transferred with binary or text
    It looks like BitSet is faster than HashSet, so let's use it.
    
    Unfortunately, BitSet would consume memory in case the Oid value is high,
    and Oids can be up to 32bit, so we limit the max OID stored in a BitSet with
    8192*8, so we effectively limit the extra per-connection memory by
    16KiB (8KiB for send and 8KiB for receive)

---
+++ pgjdbc/src/main/java/org/postgresql/util/internal/IntSet.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2024, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.util.internal;
+
+import org.postgresql.core.Oid;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Read-optimized {@code Set} for storing {@link Oid} values.
+ * Note: the set does not support nullable values.
+ */
+public final class IntSet {
+  /**
+   * Maximal Oid that will bs stored in {@link BitSet}.
+   * If Oid exceeds this value, then it will be stored in {@code Set<Int>} only.
+   * In theory, Oids can be up to 32bit, so we want to limit per-connection memory utilization.
+   * Allow {@code BitSet} to consume up to 8KiB (one for send and one for receive).
+   */
+  private static final int MAX_OID_TO_STORE_IN_BITSET = 8192 * 8;
+
+  /**
+   * Contains values outside [0..MAX_OID_TO_STORE_IN_BITSET] range.
+   * This field is null if bitSet contains all the values.
+   */
+  private @Nullable Set<Integer> set;
+
+  /**
+   * Contains values in range of [0..MAX_OID_TO_STORE_IN_BITSET].
+   */
+  private final BitSet bitSet = new BitSet();
+
+  /**
+   * Clears the contents of the set.
+   */
+  public void clear() {
+    set = null;
+    bitSet.clear();
+  }
+
+  /**
+   * Adds all the values to the set.
+   * @param values set of values to add
+   */
+  public void addAll(Collection<? extends Integer> values) {
+    for (Integer value : values) {
+      add(value);
+    }
+  }
+
+  /**
+   * Adds a single value to the set.
+   *
+   * @param value value to add
+   * @return true if the set did not already contain the specified value
+   */
+  public boolean add(int value) {
+    if (value >= 0 && value <= MAX_OID_TO_STORE_IN_BITSET) {
+      boolean contains = bitSet.get(value);
+      if (!contains) {
+        bitSet.set(value);
+        return true;
+      }
+      return false;
+    }
+    Set<Integer> set = this.set;
+    if (set == null) {
+      this.set = set = new HashSet<>();
+    }
+    return set.add(value);
+  }
+
+  /**
+   * Removes a value from the set.
+   * @param value value to remove
+   * @return true if the element was
+   */
+  public boolean remove(int value) {
+    if (value >= 0 && value <= MAX_OID_TO_STORE_IN_BITSET) {
+      boolean contains = bitSet.get(value);
+      if (contains) {
+        bitSet.clear(value);
+        return true;
+      }
+      return false;
+    }
+    Set<Integer> set = this.set;
+    return set != null && set.remove(value);
+  }
+
+  /**
+   * Checks if a given value belongs to the set.
+   * @param value value to check
+   * @return true if the value belons to the set
+   */
+  public boolean contains(int value) {
+    if (value >= 0 && value <= MAX_OID_TO_STORE_IN_BITSET) {
+      return bitSet.get(value);
+    }
+    Set<Integer> set = this.set;
+    return set != null && set.contains(value);
+  }
+
+  /**
+   * Returns a mutable snapshot of the values stored in the current set.
+   * @return a mutable snapshot of the values stored in the current set
+   */
+  public Set<Integer> toMutableSet() {
+    Set<Integer> set = this.set;
+    Set<Integer> result = new HashSet<>(
+        (int) ((bitSet.cardinality() + (set != null ? set.size() : 0)) / 0.75f));
+    if (set != null) {
+      result.addAll(set);
+    }
+    bitSet.stream().forEach(result::add);
+    return result;
+  }
+}
---
+++ pgjdbc/src/test/java/org/postgresql/util/internal/IntSetTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2024, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.util.internal;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.postgresql.core.Oid;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntSetTest {
+  private final IntSet intSet = new IntSet();
+
+  @ParameterizedTest
+  @ValueSource(ints = {Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR, Integer.MIN_VALUE,
+      Integer.MAX_VALUE})
+  void empty_contains_no_values(int oid) {
+    assertFalse(intSet.contains(oid), () -> "empty intset should not contain oid " + oid);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR, Integer.MIN_VALUE,
+      Integer.MAX_VALUE})
+  void contains_added_value(int oid) {
+    intSet.add(oid);
+    assertTrue(intSet.contains(oid),
+        () -> "intset should contain the oid that was just added: " + oid);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR, Integer.MIN_VALUE,
+      Integer.MAX_VALUE})
+  void clear_removes_elements(int oid) {
+    intSet.add(oid);
+    intSet.clear();
+    assertFalse(intSet.contains(oid),
+        () -> "intset should contain oid " + oid + " after .clear()");
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR, Integer.MIN_VALUE,
+      Integer.MAX_VALUE})
+  void compact_does_not_contain_other_values(int oid) {
+    intSet.add(Oid.INT8);
+    intSet.add(Oid.BIT);
+    assertFalse(intSet.contains(oid),
+        () -> "intset contains only INT8 and BIT, so it should not contain oid " + oid);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR, Integer.MIN_VALUE,
+      Integer.MAX_VALUE})
+  void non_compact_does_not_contain_other_values(int oid) {
+    intSet.add(Oid.INT8);
+    intSet.add(Oid.BIT);
+    intSet.add(23465234);
+    assertFalse(intSet.contains(oid),
+        () -> "intset contains only INT8, BIT, and 23465234, so it should not contain oid " + oid);
+  }
+
+  @Test
+  void addAll_contains_all_values() {
+    List<Integer> values = Arrays.asList(Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR);
+    intSet.addAll(values);
+    for (Integer value : values) {
+      assertTrue(intSet.contains(value),
+          () -> "intset should contain oid " + value + " after addAdd(" + values + ")");
+    }
+  }
+
+  @Test
+  void addAll_does_not_contains_other_values() {
+    List<Integer> values = Arrays.asList(Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR,
+        Integer.MIN_VALUE, Integer.MAX_VALUE);
+    intSet.addAll(values);
+    assertFalse(intSet.contains(Oid.BIT),
+        () -> "intset should not contain BIT after addAll(" + values + ")");
+
+    assertFalse(intSet.contains(2736453),
+        () -> "intset should not contain 2736453 after addAll(" + values + ")");
+
+    assertFalse(intSet.contains(-2736453),
+        () -> "intset should not contain -2736453 after addAll(" + values + ")");
+  }
+
+  @Test
+  void toMutableSet_contains_all_values() {
+    List<Integer> values = Arrays.asList(Oid.UNSPECIFIED, Oid.INT4, Oid.BYTEA, Oid.VARCHAR, 24337);
+    intSet.addAll(values);
+  }
+}
