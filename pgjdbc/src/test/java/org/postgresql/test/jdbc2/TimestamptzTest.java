37c3e18813693de6995c4a774d41f61c7cb36649
Renzo Dani <arons7@gmail.com>
2024-07-03 18:34:43 +0200

    do not use pattern fail(e.getMessage())

--- pgjdbc/src/test/java/org/postgresql/test/jdbc2/TimestamptzTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc2/TimestamptzTest.java
@@ -87,21 +87,17 @@ public class TimestamptzTest extends BaseTest4 {
       ps.setString(1, "Some data");
       ps.setTimestamp(2, new Timestamp(cal.getTimeInMillis()));
 
-      try {
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-          String resultFunction = rs.getString("res");
-          System.out.printf("result:%s \n", resultFunction);
+      ResultSet rs = ps.executeQuery();
+      if (rs.next()) {
+        String resultFunction = rs.getString("res");
+        System.out.printf("result:%s \n", resultFunction);
 
-          if (resultFunction.contains("wrongInfo")) {
-            fail("choose wrong overloading");
-          }
-
-        } else {
-          fail("no result");
+        if (resultFunction.contains("wrongInfo")) {
+          fail("choose wrong overloading");
         }
-      } catch (SQLException e) {
-        fail(e.getMessage());
+
+      } else {
+        fail("no result");
       }
     }
   }
@@ -128,15 +124,11 @@ public class TimestamptzTest extends BaseTest4 {
     try (PreparedStatement ps = con.prepareStatement(" SELECT * from testtimestamp.tbtesttimestamp where ts < ?  +  interval '1 hour' ")) {
       ps.setTimestamp(1, new Timestamp(cal.getTimeInMillis()));
 
-      try {
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-          System.out.printf("id:%d ts:%s \n", rs.getInt("id"), rs.getTimestamp("ts"));
-        } else {
-          fail("no result");
-        }
-      } catch (SQLException e) {
-        fail(e.getMessage());
+      ResultSet rs = ps.executeQuery();
+      if (rs.next()) {
+        System.out.printf("id:%d ts:%s \n", rs.getInt("id"), rs.getTimestamp("ts"));
+      } else {
+        fail("no result");
       }
     }
   }
@@ -146,17 +138,13 @@ public class TimestamptzTest extends BaseTest4 {
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
 
-      try {
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-          String typeFromDb = rs.getString(1);
-          System.out.printf("db type: %s\n",typeFromDb);
-          assertTrue("timestamp with time zone".equalsIgnoreCase(typeFromDb));
-        } else {
-          fail("no result");
-        }
-      } catch (SQLException e) {
-        fail(e.getMessage());
+      ResultSet rs = ps.executeQuery();
+      if (rs.next()) {
+        String typeFromDb = rs.getString(1);
+        System.out.printf("db type: %s\n",typeFromDb);
+        assertTrue("timestamp with time zone".equalsIgnoreCase(typeFromDb));
+      } else {
+        fail("no result");
       }
     }
   }
@@ -184,16 +172,12 @@ public class TimestamptzTest extends BaseTest4 {
     // java.lang.AssertionError: Can't infer the SQL type to use for an instance of java.time.ZonedDateTime. Use setObject() with an explicit Types value to specify the type to use.
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setObject(1, cal.toZonedDateTime());
-    } catch (SQLException e) {
-      fail(e.getMessage());
     }
 
     //Problem
     // java.lang.AssertionError: Bad value for type timestamp/date/time: 2023-03-12T09:30+01:00[Europe/Berlin]
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setObject(1, cal.toZonedDateTime(), Types.TIMESTAMP_WITH_TIMEZONE);
-    } catch (SQLException e) {
-      fail(e.getMessage());
     }
   }
 
@@ -203,43 +187,33 @@ public class TimestamptzTest extends BaseTest4 {
     // java.lang.AssertionError: Can't infer the SQL type to use for an instance of java.time.ZonedDateTime. Use setObject() with an explicit Types value to specify the type to use.
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setObject(1, Instant.now());
-    } catch (SQLException e) {
-      fail(e.getMessage());
-    }
+    } 
 
     //Problem
     // java.lang.AssertionError: Bad value for type timestamp/date/time: 2023-03-13T20:34:58.711330339Z
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setObject(1, Instant.now(), Types.TIMESTAMP);
-    } catch (SQLException e) {
-      fail(e.getMessage());
-    }
+    } 
 
     //Problem
     // java.lang.AssertionError: Cannot cast an instance of java.time.Instant to type Types.TIMESTAMP_WITH_TIMEZONE
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setObject(1, Instant.now(), Types.TIMESTAMP_WITH_TIMEZONE);
-    } catch (SQLException e) {
-      fail(e.getMessage());
-    }
+    } 
   }
 
   @Test
   public void testJavaTime_LocalTime() throws SQLException {
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setObject(1, LocalTime.now());
-    } catch (SQLException e) {
-      fail(e.getMessage());
-    }
+    } 
   }
 
   @Test
   public void testJavaTime_LocalDateTime() throws SQLException {
     try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
       ps.setObject(1, LocalDateTime.now());
-    } catch (SQLException e) {
-      fail(e.getMessage());
-    }
+    } 
   }
 
 }
