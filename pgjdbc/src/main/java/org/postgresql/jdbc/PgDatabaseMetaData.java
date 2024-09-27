cf49a34299e740fefde3fe12a630af2e20d1ea5e
Sophiah Ho <sophiah.ho@precisely.com>
2024-09-27 10:46:56 -0400

    fix(jdbc): populate TABLE_CATALOG with current_database()::information_schema.sql_identifier in getSchemas

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgDatabaseMetaData.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgDatabaseMetaData.java
@@ -1599,7 +1599,7 @@ public class PgDatabaseMetaData implements DatabaseMetaData {
   public ResultSet getSchemas(@Nullable String catalog, @Nullable String schemaPattern)
       throws SQLException {
     String sql;
-    sql = "SELECT nspname AS \"TABLE_SCHEM\", NULL AS \"TABLE_CATALOG\" FROM pg_catalog.pg_namespace "
+    sql = "SELECT nspname AS \"TABLE_SCHEM\", current_database()::information_schema.sql_identifier AS \"TABLE_CATALOG\" FROM pg_catalog.pg_namespace "
           + " WHERE nspname <> 'pg_toast' AND (nspname !~ '^pg_temp_' "
           + " OR nspname = (pg_catalog.current_schemas(true))[1]) AND (nspname !~ '^pg_toast_temp_' "
           + " OR nspname = replace((pg_catalog.current_schemas(true))[1], 'pg_temp_', 'pg_toast_temp_')) ";
