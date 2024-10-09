6520d3705c50f42b0dc0685f9363433c79080773
Ken Southerland <crowmagnumb@gmail.com>
2024-10-09 13:17:47 -0700

    I probably also need this

--- pgjdbc/src/main/java/org/postgresql/jdbc/SQLDataReader.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/SQLDataReader.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SQLDataReader {
-  public <T> T read(@Nullable Object obj, Class<T> type, TimestampUtils timestampUtils) throws SQLException {
+  public @Nullable  <T> T read(@Nullable Object obj, Class<T> type, TimestampUtils timestampUtils) throws SQLException {
     if (obj == null) {
       return null;
     }
