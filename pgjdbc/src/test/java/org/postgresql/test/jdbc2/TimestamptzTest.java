bc3e1c667b734453d4598079c3306942ce628a4a
Renzo Dani <arons7@gmail.com>
2025-01-14 17:07:56 +0100

    revert of commit 4051e2ab145785b14bc1b4a806e2ad3f131eaa8f
    ZonedDateTime and Instant will be moved to a different PR.

--- pgjdbc/src/test/java/org/postgresql/test/jdbc2/TimestamptzTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/jdbc2/TimestamptzTest.java
@@ -20,8 +20,6 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.sql.Types;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -194,46 +192,6 @@ public class TimestamptzTest extends BaseTest4 {
     }
   }
 
-  @Test
-  public void testJavaTime_ZonedDateTime() throws SQLException {
-    GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("Europe/Berlin"));
-    cal.set(2023, Calendar.MARCH, 12, 9, 30);
-    cal.set(Calendar.SECOND, 0);
-    cal.set(Calendar.MILLISECOND, 0);
-
-    //Problem
-    // java.lang.AssertionError: Can't infer the SQL type to use for an instance of java.time.ZonedDateTime. Use setObject() with an explicit Types value to specify the type to use.
-    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
-      ps.setObject(1, cal.toZonedDateTime());
-    }
-
-    //Problem
-    // java.lang.AssertionError: Bad value for type timestamp/date/time: 2023-03-12T09:30+01:00[Europe/Berlin]
-    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
-      ps.setObject(1, cal.toZonedDateTime(), Types.TIMESTAMP_WITH_TIMEZONE);
-    }
-  }
-
-  @Test
-  public void testJavaTime_Instant() throws SQLException {
-    //Problem
-    // java.lang.AssertionError: Can't infer the SQL type to use for an instance of java.time.ZonedDateTime. Use setObject() with an explicit Types value to specify the type to use.
-    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
-      ps.setObject(1, Instant.now());
-    }
-
-    //Problem
-    // java.lang.AssertionError: Bad value for type timestamp/date/time: 2023-03-13T20:34:58.711330339Z
-    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
-      ps.setObject(1, Instant.now(), Types.TIMESTAMP);
-    }
-
-    //Problem
-    // java.lang.AssertionError: Cannot cast an instance of java.time.Instant to type Types.TIMESTAMP_WITH_TIMEZONE
-    try (PreparedStatement ps = con.prepareStatement(" SELECT pg_typeof(?) ")) {
-      ps.setObject(1, Instant.now(), Types.TIMESTAMP_WITH_TIMEZONE);
-    }
-  }
 
   @Test
   public void testJavaTime_LocalTime() throws SQLException {
