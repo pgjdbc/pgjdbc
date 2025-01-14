5f10632be09b98901dc188593046fd9910ebaabf
Renzo Dani <arons7@gmail.com>
2025-01-14 08:17:37 +0100

    keep data consistency

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
@@ -249,6 +249,8 @@ public class PgResultSet implements ResultSet, PGRefCursorResultSet {
         return getTime(columnIndex);
       case Types.TIMESTAMP:
         return getTimestamp(columnIndex, null);
+      case Types.TIMESTAMP_WITH_TIMEZONE:
+          return getTimestamp(columnIndex, null);   
       case Types.BINARY:
       case Types.VARBINARY:
       case Types.LONGVARBINARY:
@@ -2128,6 +2130,12 @@ public class PgResultSet implements ResultSet, PGRefCursorResultSet {
               getTimestampUtils().toString(
                   getDefaultCalendar(), (Timestamp) valueObject)));
           break;
+          
+        case Types.TIMESTAMP_WITH_TIMEZONE:
+            rowBuffer.set(columnIndex, connection.encodeString(
+                getTimestampUtils().toString(
+                    getDefaultCalendar(), (Timestamp) valueObject)));
+            break;  
 
         case Types.NULL:
           // Should never happen?
