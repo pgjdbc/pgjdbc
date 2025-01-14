07f7fa210d0c3ed7a1d4aa1a5c55cb0045a7aca0
Renzo Dani <arons7@gmail.com>
2025-01-14 16:39:05 +0100

    explode to same logica as before but using now the correct oid type

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgResultSet.java
@@ -250,7 +250,7 @@ public class PgResultSet implements ResultSet, PGRefCursorResultSet {
       case Types.TIMESTAMP:
         return getTimestamp(columnIndex, null);
       case Types.TIMESTAMP_WITH_TIMEZONE:
-          return getTimestamp(columnIndex, null);   
+        return getTimestamp(columnIndex, null);
       case Types.BINARY:
       case Types.VARBINARY:
       case Types.LONGVARBINARY:
@@ -2130,12 +2130,12 @@ public class PgResultSet implements ResultSet, PGRefCursorResultSet {
               getTimestampUtils().toString(
                   getDefaultCalendar(), (Timestamp) valueObject)));
           break;
-          
+
         case Types.TIMESTAMP_WITH_TIMEZONE:
-            rowBuffer.set(columnIndex, connection.encodeString(
-                getTimestampUtils().toString(
-                    getDefaultCalendar(), (Timestamp) valueObject)));
-            break;  
+          rowBuffer.set(columnIndex, connection.encodeString(
+              getTimestampUtils().toString(
+                  getDefaultCalendar(), (Timestamp) valueObject)));
+          break;
 
         case Types.NULL:
           // Should never happen?
