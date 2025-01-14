07e4f58cb855c4cbc6d9cc5c1fab64da9be0129f
Renzo Dani <arons7@gmail.com>
2025-01-14 17:07:56 +0100

    revert of commit 4051e2ab145785b14bc1b4a806e2ad3f131eaa8f
    ZonedDateTime and Instant will be moved to a different PR.

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgPreparedStatement.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgPreparedStatement.java
@@ -68,13 +68,11 @@ import java.sql.SQLXML;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.sql.Types;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.OffsetTime;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -689,10 +687,6 @@ class PgPreparedStatement extends PgStatement implements PreparedStatement {
           } else if (in instanceof LocalDateTime) {
             setTimestamp(parameterIndex, (LocalDateTime) in);
             break;
-          } else if (in instanceof Instant) {
-            tmpts = Timestamp.from( (Instant) in );
-          } else if (in instanceof ZonedDateTime) {
-            tmpts = Timestamp.valueOf( ((ZonedDateTime) in).toLocalDateTime());
           } else {
             Charset connectionCharset = Charset.forName(connection.getEncoding().name());
             tmpts = getTimestampUtils().toTimestamp(getDefaultCalendar(), in.toString().getBytes(connectionCharset));
@@ -705,10 +699,6 @@ class PgPreparedStatement extends PgStatement implements PreparedStatement {
           setTimestamp(parameterIndex, (OffsetDateTime) in);
         } else if (in instanceof PGTimestamp) {
           setObject(parameterIndex, in);
-        } else if (in instanceof Instant) {
-          setTimestamp(parameterIndex, Timestamp.from( (Instant) in ));
-        } else if (in instanceof ZonedDateTime) {
-          setTimestamp(parameterIndex, Timestamp.valueOf(((ZonedDateTime) in).toLocalDateTime()));
         } else {
           throw new PSQLException(
               GT.tr("Cannot cast an instance of {0} to type {1}",
@@ -1063,10 +1053,6 @@ class PgPreparedStatement extends PgStatement implements PreparedStatement {
       setTime(parameterIndex, (Time) x);
     } else if (x instanceof Timestamp) {
       setTimestamp(parameterIndex, (Timestamp) x);
-    } else if (x instanceof Instant ) {
-      setTimestamp(parameterIndex, Timestamp.from((Instant) x ));
-    } else if (x instanceof ZonedDateTime ) {
-      setTimestamp(parameterIndex, Timestamp.valueOf(((ZonedDateTime) x).toLocalDateTime()));
     } else if (x instanceof Boolean) {
       setBoolean(parameterIndex, (Boolean) x);
     } else if (x instanceof Byte) {
