b3f64f49ddcc435950cdcee1bb5370924b371c75
Marek Läll <marek.lall@fairown.com>
2025-05-23 12:33:07 +0300

    after rebase fix: tests sent invalid properties to Driver

--- pgjdbc/src/main/java/org/postgresql/PGProperty.java
+++ pgjdbc/src/main/java/org/postgresql/PGProperty.java
@@ -1153,4 +1153,23 @@ public enum PGProperty {
     }
     return null;
   }
+
+  /**
+   * Return valid properties. Not existing properties are not returned.
+   *
+   * @param properties properties bundle
+   * @return properties bundle with valid properties
+   */
+  public static Properties removeNonExistingProperties(Properties properties) {
+    Properties props = new Properties();
+    properties.stringPropertyNames().stream()
+        .filter(name -> PGProperty.forName(name) != null)
+        .forEach(name -> {
+          String value = properties.getProperty(name);
+          if (value != null) {
+            props.setProperty(name, value);
+          }
+        });
+    return props;
+  }
 }
