55b61f5f910ff7c73c09b8004d6e7d1c6021cb73
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-05-28 23:22:29 +0300

    fix: style

--- pgjdbc/src/main/java/org/postgresql/jdbc/PgDatabaseMetaData.java
+++ pgjdbc/src/main/java/org/postgresql/jdbc/PgDatabaseMetaData.java
@@ -44,7 +44,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class PgDatabaseMetaData implements DatabaseMetaData {
