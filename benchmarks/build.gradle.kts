322c044b8f227dc9e705ffbc9d7e91a3b71621b7
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-06-14 20:13:38 +0300

    test: extract pgjdbc/testFixtures to testkit project
    It enables using shadowed artifacts for benchmarks and osgi-test

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -3,21 +3,19 @@
  * See the LICENSE file in the project root for more information.
  */
 plugins {
-    id("java-test-fixtures")
     id("build-logic.java-library")
     id("me.champeau.jmh")
 }
 
 dependencies {
-    // Make jmhCompileClasspath resolvable
-    jmhImplementation(project(":postgresql"))
-    jmhRuntimeOnly("com.ongres.scram:scram-client:3.1") {
-        because("We refer non-shaded :postgresql, so we add explicit scram-client dependency")
+    jmhImplementation(projects.postgresql) {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.SHADOWED))
+        }
     }
-    jmhImplementation(testFixtures(project(":postgresql")))
+    jmhImplementation(projects.testkit)
     jmhImplementation("org.roaringbitmap:RoaringBitmap:1.3.0")
     jmhImplementation("it.unimi.dsi:fastutil:8.5.15")
-    jmhRuntimeOnly("com.ongres.scram:scram-client:3.1")
     jmhImplementation("org.openjdk.jmh:jmh-core:1.37")
     jmhImplementation("org.openjdk.jmh:jmh-generator-annprocess:1.37")
 }
---
+++ testkit/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("build-logic.java-library")
+    id("build-logic.without-type-annotations")
+}
+
+dependencies {
+    api(platform("org.junit:junit-bom:5.13.1"))
+    api("org.junit.jupiter:junit-jupiter-api")
+
+    // We want testkit to be compatible with both regular and shadowed variants,
+    // so we use compileOnly.
+    compileOnly(projects.postgresql)
+    implementation("org.checkerframework:checker-qual:3.49.4")
+}
