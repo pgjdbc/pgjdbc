7cd0fc03711724ec9edf82266ee82e9a894cc9c9
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2025-06-16 14:40:48 +0300

    fix(deps): update dependency uk.org.webcompere:system-stubs-jupiter to v2.1.8

--- build-logic/jvm/src/main/kotlin/build-logic.test-junit5.gradle.kts
+++ build-logic/jvm/src/main/kotlin/build-logic.test-junit5.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     if (buildParameters.testJdkVersion >= 11) {
         // system-stubs 2.0+ requires Java 11+
-        testImplementation("uk.org.webcompere:system-stubs-jupiter:2.1.7")
+        testImplementation("uk.org.webcompere:system-stubs-jupiter:2.1.8")
         // system-stubs-jupiter might come with an outdated byte-buddy version, so we mention
         // the latest one so it would be updated by the bots even if system-stubs-jupiter does not update
         testImplementation(platform("net.bytebuddy:byte-buddy-parent:1.17.5"))
