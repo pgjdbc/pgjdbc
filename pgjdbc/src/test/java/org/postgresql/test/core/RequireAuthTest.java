26c799348e20a9be3e899ddd2c9e59c72244fb29
Dave Cramer <davecramer@gmail.com>
2025-12-19 09:38:20 -0500

    check to see if we have scram or md5 before running tests

--- pgjdbc/src/test/java/org/postgresql/test/core/RequireAuthTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/core/RequireAuthTest.java
@@ -22,14 +22,17 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 class RequireAuthTest {
+  private static boolean hasScram = true;
 
   @BeforeAll
   static void beforeAll() {
     try (Connection conn = TestUtil.openPrivilegedDB()) {
-      conn.createStatement().execute("create user nobody with password 'none'");
-      conn.createStatement().execute("create user pword with password 'password'");
-      conn.createStatement().execute("create user scram with password 'scram'");
-      conn.createStatement().execute("create database authtest owner nobody");
+      TestUtil.executeQuery(conn, "create user nobody with password 'none'");
+      TestUtil.executeQuery(conn, "create user pword with password 'password'");
+      TestUtil.executeQuery(conn, "create user scram with password 'scram'");
+      TestUtil.executeQuery(conn, "create user md51 with password 'pword'");
+      TestUtil.executeQuery(conn, "create database authtest owner nobody");
+      hasScram = TestUtil.queryForString(conn, "show password_encryption").equals("scram-sha-256");
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
@@ -38,10 +41,11 @@ class RequireAuthTest {
   @AfterAll
   static void afterAll() {
     try (Connection conn = TestUtil.openPrivilegedDB()) {
-      conn.createStatement().execute("drop database authtest");
-      conn.createStatement().execute("drop user scram");
-      conn.createStatement().execute("drop user pword");
-      conn.createStatement().execute("drop user nobody");
+      TestUtil.executeQuery(conn, "drop database authtest");
+      TestUtil.executeQuery(conn, "drop user md51");
+      TestUtil.executeQuery(conn, "drop user scram");
+      TestUtil.executeQuery(conn, "drop user pword");
+      TestUtil.executeQuery(conn, "drop user nobody");
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
@@ -111,10 +115,25 @@ class RequireAuthTest {
   }
 
   @Test
+  void testRequireAuthAllowMd5orScram() throws SQLException {
+    Properties props = new Properties();
+    props.setProperty("user", hasScram ? "scram" : "md51");
+    props.setProperty("password", hasScram ? "scram" : "pword");
+    props.setProperty(TEST_URL_PROPERTY_PREFIX + PGProperty.PG_DBNAME.getName(), "authtest");
+
+    PGProperty.REQUIRE_AUTH.set(props, hasScram ? "scram-sha-256" : "md5");
+
+    // This should succeed if server uses md5 auth
+    try (Connection conn = TestUtil.openDB(props)) {
+      assertTrue(conn.isValid(5));
+    }
+  }
+
+  @Test
   void testRequireAuthMultipleAllowed() throws SQLException {
     Properties props = new Properties();
-    props.setProperty("user", "scram");
-    props.setProperty("password", "scram");
+    props.setProperty("user", hasScram ? "scram" : "md51");
+    props.setProperty("password", hasScram ? "scram" : "pword");
     props.setProperty(TEST_URL_PROPERTY_PREFIX + PGProperty.PG_DBNAME.getName(), "authtest");
 
     PGProperty.REQUIRE_AUTH.set(props, "password,md5,scram-sha-256");
