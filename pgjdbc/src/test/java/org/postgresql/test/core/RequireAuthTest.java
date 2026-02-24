5b4fca14f45d820d981fceef6b333514393421b1
Dave Cramer <davecramer@gmail.com>
2026-02-24 08:55:20 -0500

    remote assumption that we have scram

--- pgjdbc/src/test/java/org/postgresql/test/core/RequireAuthTest.java
+++ pgjdbc/src/test/java/org/postgresql/test/core/RequireAuthTest.java
@@ -21,7 +21,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 class RequireAuthTest {
-  private static boolean hasScram = true;
+  private static boolean hasScram;
 
   @BeforeAll
   static void beforeAll() throws SQLException {
