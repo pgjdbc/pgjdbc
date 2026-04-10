bc03c9e41dec975bb20f48f8dca77470f1a77f40
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2026-04-10 11:41:09 +0300

    fix: avoid creating transient ResultSet for describe statement purposes
    Previously the code created temporary ResultSet which might interfere with
    statement.closeOnCompletion().

--- pgjdbc/src/main/java/org/postgresql/core/ResultHandlerBase.java
+++ pgjdbc/src/main/java/org/postgresql/core/ResultHandlerBase.java
@@ -11,7 +11,6 @@ import static org.postgresql.util.internal.Nullness.castNonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.sql.SQLException;
-import java.sql.SQLWarning;
 import java.util.List;
 
 /**
@@ -19,14 +18,11 @@ import java.util.List;
  * {@link SQLException#setNextException(SQLException)} has {@code O(N)} complexity,
  * so this class tracks the last exception object to speedup {@code setNextException}.
  */
-public class ResultHandlerBase implements ResultHandler {
+public abstract class ResultHandlerBase implements ResultHandler {
   // Last exception is tracked to avoid O(N) SQLException#setNextException just in case there
   // will be lots of exceptions (e.g. all batch rows fail with constraint violation or so)
   private @Nullable SQLException firstException;
   private @Nullable SQLException lastException;
-
-  private @Nullable SQLWarning firstWarning;
-  private @Nullable SQLWarning lastWarning;
 
   @Override
   public void handleResultRows(Query fromQuery, Field[] fields, List<Tuple> tuples,
@@ -39,17 +35,6 @@ public class ResultHandlerBase implements ResultHandler {
 
   @Override
   public void secureProgress() {
-  }
-
-  @Override
-  public void handleWarning(SQLWarning warning) {
-    if (firstWarning == null) {
-      firstWarning = lastWarning = warning;
-      return;
-    }
-    SQLWarning lastWarning = castNonNull(this.lastWarning);
-    lastWarning.setNextException(warning);
-    this.lastWarning = warning;
   }
 
   @Override
@@ -73,10 +58,5 @@ public class ResultHandlerBase implements ResultHandler {
   @Override
   public @Nullable SQLException getException() {
     return firstException;
-  }
-
-  @Override
-  public @Nullable SQLWarning getWarning() {
-    return firstWarning;
   }
 }
