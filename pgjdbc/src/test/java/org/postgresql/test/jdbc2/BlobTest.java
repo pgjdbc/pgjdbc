pgjdbc/pgjdbc

commit 84b144f122ac48e2739ab32b0f3beda340900a66

remove old comment about finalize, guard closed flag with a reentrant lock to protect against concurrent close. (#3905)
* remove old comment about finalize, guard closed flag with a reentrant lock to protect against concurrent close. Fixes #Issue 3904

* Remove ResourceLock, simplify checking for closed and add check for close in all functions of LargeObject

pgjdbc/src/main/java/org/postgresql/largeobject/LargeObject.java
@@ -9,6 +9,7 @@ import org.postgresql.core.BaseConnection;
 import org.postgresql.fastpath.Fastpath;
 import org.postgresql.fastpath.FastpathArg;
 import org.postgresql.util.ByteStreamWriter;
+import org.postgresql.util.GT;
 import org.postgresql.util.PSQLException;
 import org.postgresql.util.PSQLState;
 
@@ -75,6 +76,18 @@ public class LargeObject
   private final boolean commitOnClose; // Only initialized when open a LOB with CommitOnClose
 
   /**
+   * Checks if this LargeObject is closed and throws an exception if it is.
+   *
+   * @throws SQLException if this LargeObject has been closed
+   */
+  private void checkClosed() throws SQLException {
+    if (closed) {
+      throw new PSQLException(GT.tr("This large object has been closed."),
+          PSQLState.OBJECT_NOT_IN_STATE);
+    }
+  }
+
+  /**
    * This opens a large object.
    *
    * <p>If the object does not exist, then an SQLException is thrown.</p>
@@ -123,19 +136,9 @@ public class LargeObject
   }
 
   public LargeObject copy() throws SQLException {
+    checkClosed();
     return new LargeObject(fp, oid, mode);
   }
-
-  /*
-   * Release large object resources during garbage cleanup.
-   *
-   * This code used to call close() however that was problematic because the scope of the fd is a
-   * transaction, thus if commit or rollback was called before garbage collection ran then the call
-   * to close would error out with an invalid large object handle. So this method now does nothing
-   * and lets the server handle cleanup when it ends the transaction.
-   *
-   * protected void finalize() throws SQLException { }
-   */
 
   /**
    * @return the OID of this LargeObject
@@ -160,28 +163,29 @@ public class LargeObject
    */
   @Override
   public void close() throws SQLException {
-    if (!closed) {
-      // flush any open output streams
-      if (os != null) {
-        try {
-          // we can't call os.close() otherwise we go into an infinite loop!
-          os.flush();
-        } catch (IOException ioe) {
-          throw new PSQLException("Exception flushing output stream", PSQLState.DATA_ERROR, ioe);
-        } finally {
-          os = null;
-        }
+    if (closed) {
+      return;
+    }
+    closed = true;
+    // flush any open output streams
+    if (os != null) {
+      try {
+        // we can't call os.close() otherwise we go into an infinite loop!
+        os.flush();
+      } catch (IOException ioe) {
+        throw new PSQLException("Exception flushing output stream", PSQLState.DATA_ERROR, ioe);
+      } finally {
+        os = null;
       }
+    }
 
-      // finally close
-      FastpathArg[] args = new FastpathArg[1];
-      args[0] = new FastpathArg(fd);
-      fp.fastpath("lo_close", args); // true here as we dont care!!
-      closed = true;
-      BaseConnection conn = this.conn;
-      if (this.commitOnClose && conn != null) {
-        conn.commit();
-      }
+    // finally close
+    FastpathArg[] args = new FastpathArg[1];
+    args[0] = new FastpathArg(fd);
+    fp.fastpath("lo_close", args); // true here as we dont care!!
+    BaseConnection conn = this.conn;
+    if (this.commitOnClose && conn != null) {
+      conn.commit();
     }
   }
 
@@ -193,6 +197,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public byte[] read(int len) throws SQLException {
+    checkClosed();
     // This is the original method, where the entire block (len bytes)
     // is retrieved in one go.
     FastpathArg[] args = new FastpathArg[2];
@@ -215,6 +220,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public int read(byte[] buf, int off, int len) throws SQLException {
+    checkClosed();
     byte[] b = read(len);
     if (b.length == 0) {
       return 0;
@@ -231,6 +237,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public void write(byte[] buf) throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[2];
     args[0] = new FastpathArg(fd);
     args[1] = new FastpathArg(buf);
@@ -246,6 +253,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public void write(byte[] buf, int off, int len) throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[2];
     args[0] = new FastpathArg(fd);
     args[1] = new FastpathArg(buf, off, len);
@@ -259,6 +267,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public void write(ByteStreamWriter writer) throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[2];
     args[0] = new FastpathArg(fd);
     args[1] = FastpathArg.of(writer);
@@ -276,6 +285,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public void seek(int pos, int ref) throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[3];
     args[0] = new FastpathArg(fd);
     args[1] = new FastpathArg(pos);
@@ -291,6 +301,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public void seek64(long pos, int ref) throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[3];
     args[0] = new FastpathArg(fd);
     args[1] = new FastpathArg(pos);
@@ -308,6 +319,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public void seek(int pos) throws SQLException {
+    checkClosed();
     seek(pos, SEEK_SET);
   }
 
@@ -316,6 +328,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public int tell() throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[1];
     args[0] = new FastpathArg(fd);
     return fp.getInteger("lo_tell", args);
@@ -326,6 +339,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public long tell64() throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[1];
     args[0] = new FastpathArg(fd);
     return fp.getLong("lo_tell64", args);
@@ -341,6 +355,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public int size() throws SQLException {
+    checkClosed();
     int cp = tell();
     seek(0, SEEK_END);
     int sz = tell();
@@ -355,6 +370,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public long size64() throws SQLException {
+    checkClosed();
     long cp = tell64();
     seek64(0, SEEK_END);
     long sz = tell64();
@@ -371,6 +387,7 @@ public class LargeObject
    * @throws SQLException if something goes wrong
    */
   public void truncate(int len) throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[2];
     args[0] = new FastpathArg(fd);
     args[1] = new FastpathArg(len);
@@ -386,6 +403,7 @@ public class LargeObject
    * @throws SQLException if something goes wrong
    */
   public void truncate64(long len) throws SQLException {
+    checkClosed();
     FastpathArg[] args = new FastpathArg[2];
     args[0] = new FastpathArg(fd);
     args[1] = new FastpathArg(len);
@@ -401,6 +419,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public InputStream getInputStream() throws SQLException {
+    checkClosed();
     return new BlobInputStream(this);
   }
 
@@ -413,6 +432,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public InputStream getInputStream(long limit) throws SQLException {
+    checkClosed();
     return new BlobInputStream(this, BlobInputStream.DEFAULT_MAX_BUFFER_SIZE, limit);
   }
 
@@ -427,6 +447,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public InputStream getInputStream(int bufferSize, long limit) throws SQLException {
+    checkClosed();
     return new BlobInputStream(this, bufferSize, limit);
   }
 
@@ -439,6 +460,7 @@ public class LargeObject
    * @throws SQLException if a database-access error occurs.
    */
   public OutputStream getOutputStream() throws SQLException {
+    checkClosed();
     if (os == null) {
       os = new BlobOutputStream(this);
     }

pgjdbc/src/test/java/org/postgresql/jdbc/LargeObjectManagerTest.java
@@ -177,4 +177,80 @@ class LargeObjectManagerTest {
       lom.delete(loId);
     }
   }
+
+  @Test
+  void closedLargeObjectThrowsException() throws Exception {
+    try (PgConnection con = (PgConnection) TestUtil.openDB()) {
+      assumeTrue(TestUtil.haveMinimumServerVersion(con, ServerVersion.v9_0));
+      con.setAutoCommit(false);
+
+      LargeObjectManager lom = con.getLargeObjectAPI();
+      long loId = lom.createLO();
+      LargeObject lo = lom.open(loId, LargeObjectManager.WRITE);
+
+      // Close the large object
+      lo.close();
+
+      // Now try to use methods on the closed object - they should all throw PSQLException
+      try {
+        lo.read(10);
+        fail("Expected PSQLException when calling read() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      try {
+        lo.write(new byte[]{1, 2, 3});
+        fail("Expected PSQLException when calling write() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      try {
+        lo.seek(0);
+        fail("Expected PSQLException when calling seek() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      try {
+        lo.tell();
+        fail("Expected PSQLException when calling tell() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      try {
+        lo.size();
+        fail("Expected PSQLException when calling size() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      try {
+        lo.truncate(0);
+        fail("Expected PSQLException when calling truncate() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      try {
+        lo.getInputStream();
+        fail("Expected PSQLException when calling getInputStream() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      try {
+        lo.getOutputStream();
+        fail("Expected PSQLException when calling getOutputStream() on closed LargeObject");
+      } catch (PSQLException e) {
+        assertEquals(PSQLState.OBJECT_NOT_IN_STATE.getState(), e.getSQLState());
+      }
+
+      // Clean up
+      lom.delete(loId);
+      con.commit();
+    }
+  }
 }

commit a95b8d0f3b3d9708a9708d2e53adab93cac2e14c

fix(largeobject): flush output stream before marking LargeObject closed
LargeObject.close() set the closed flag before flushing the buffered
output stream. The implicit os.flush() then wrote through the closed
object, so LargeObject.write() -> checkClosed() threw "This large
object has been closed." and close() failed.

Restore the pre-42.7.11 ordering: flush first, then mark the object
closed. The regression was introduced in #3905, which moved
closed = true ahead of the flush.

Add BlobTest.closeFlushesBufferedOutputStream, which writes a small,
still-buffered payload, closes the LargeObject directly, then reopens
and verifies the bytes were persisted. It fails before this change and
passes after.

Fixes #4247

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

pgjdbc/src/main/java/org/postgresql/largeobject/LargeObject.java
@@ -166,8 +166,8 @@ public class LargeObject
     if (closed) {
       return;
     }
-    closed = true;
-    // flush any open output streams
+    // flush any open output streams before marking the object closed, otherwise the flush
+    // would write through a closed LargeObject and fail its checkClosed() guard
     if (os != null) {
       try {
         // we can't call os.close() otherwise we go into an infinite loop!
@@ -178,6 +178,7 @@ public class LargeObject
         os = null;
       }
     }
+    closed = true;
 
     // finally close
     FastpathArg[] args = new FastpathArg[1];

pgjdbc/src/test/java/org/postgresql/test/jdbc2/BlobTest.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.sql.Blob;
 import java.sql.Clob;
 import java.sql.Connection;
@@ -133,6 +134,35 @@ class BlobTest {
 
       pstmt.setObject(1, null, Types.CLOB);
       pstmt.executeUpdate();
+    }
+  }
+
+  /**
+   * Closing a LargeObject must flush its still-buffered output stream before marking the object
+   * closed. See <a href="https://github.com/pgjdbc/pgjdbc/issues/4247">issue 4247</a>: a regression
+   * in 42.7.11 set the closed flag first, so the implicit flush failed with
+   * "This large object has been closed.".
+   */
+  @Test
+  void closeFlushesBufferedOutputStream() throws Exception {
+    LargeObjectManager lom = ((PGConnection) con).getLargeObjectAPI();
+    long oid = lom.createLO(LargeObjectManager.READWRITE);
+
+    byte[] data = "pgjdbc-4247".getBytes(StandardCharsets.US_ASCII);
+
+    LargeObject blob = lom.open(oid, LargeObjectManager.WRITE);
+    OutputStream os = blob.getOutputStream();
+    // Less than the buffer size, so the bytes stay buffered until close() flushes them
+    os.write(data);
+    // Close the LargeObject directly without flushing the stream first: this must not throw
+    blob.close();
+
+    blob = lom.open(oid, LargeObjectManager.READ);
+    try {
+      assertArrayEquals(data, blob.read(data.length));
+    } finally {
+      blob.close();
+      lom.delete(oid);
     }
   }
 